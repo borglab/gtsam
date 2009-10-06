7e77ae5f53050a171f060b1bb0d214fad2c570dc
Frank Dellaert <dellaert@gmail.com>
2009-10-06 18:01:40 +0000

    formatting only, clear by default

--- cpp/Ordering.h
+++ cpp/Ordering.h
@@ -4,39 +4,41 @@
  * @author  Frank Dellaert
  */
 
-
 #pragma once
 
 #include <vector>
 #include <string>
 
-// \namespace
-
 namespace gtsam {
 
-/**
- * @class Ordering
- * @brief ordering of indices for eliminating a factor graph
- */
-class Ordering : public std::vector<std::string>
-{
-public:
-	/** Constructor  */
-	Ordering(){clear();}
-
-	Ordering(std::vector<std::string> strings_in) : std::vector<std::string> (strings_in) {}
-
-	/** Destructor */
-	~Ordering(){}
-
-	void print() const;
-
 	/**
-	 * check if two orderings are the same
-	 * @param ordering
-	 * @return bool
+	 * @class Ordering
+	 * @brief ordering of indices for eliminating a factor graph
 	 */
-	bool equals(Ordering &ord);
-};
+	class Ordering: public std::vector<std::string> {
+	public:
+		/**
+		 * Default constructor creates empty ordering
+		 */
+		Ordering() {
+		}
+
+		/**
+		 * Copy constructor from string vector
+		 * TODO: should take reference?
+		 */
+		Ordering(std::vector<std::string> strings_in) :
+			std::vector<std::string>(strings_in) {
+		}
+
+		void print() const;
+
+		/**
+		 * check if two orderings are the same
+		 * @param ordering
+		 * @return bool
+		 */
+		bool equals(Ordering &ord);
+	};
 
 }
