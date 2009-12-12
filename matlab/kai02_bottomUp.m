7725a9caebc6494a21f279bbc4d13ed578d5db48
Kai Ni <nikai97@gmail.com>
2009-12-12 22:34:36 +0000

    find a bottom to up ordering given the tree structure {pred} returned by matlab's graphminspantree

--- matlab/kai02_bottomUp.m
+++ matlab/kai02_bottomUp.m
@@ -1,5 +1,25 @@
-% test ordering
+% find a bottom to up ordering given the tree structure {pred} returned by matlab's graphminspantree
+function [ordering] = kai02_bottomUp(pred)
 
-input = 6;
-ordering = bottom_up_ordering(6);
+%% compute the levels of the nodes
+parents = [0];
+node_levels = zeros(length(pred), 1);
+current_level = 1;
+while ~isempty(parents)
+    parents = find(ismember(pred, parents));
+    node_levels(parents) = current_level;
+    current_level = current_level + 1;
+end
+max_level = current_level - 1;
+
+ordering = Ordering();
+for level = max_level:-1:1
+    ids = find(node_levels==level);
+    for i = 1:length(ids)
+        ordering.push_back(sprintf('x%d', ids(i)));
+    end
+end
+
 ordering.print('ordering')
+
+end
