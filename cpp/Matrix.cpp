a7a5e5e816ea77db856f99fe10c1e9121a2360ea
Chris Beall <chrisbeall@gmail.com>
2009-12-14 16:56:50 +0000

    small performance improvement by using a pointer to address r

--- cpp/Matrix.cpp
+++ cpp/Matrix.cpp
@@ -346,10 +346,12 @@ weighted_eliminate(Matrix& A, Vector& b, const Vector& sigmas) {
 			double ai = a(i);
 			b(i) -= ai*d;
 			double *Aptr = A.data().begin()+i*n+j+1;
+			double *rptr = r.data().begin()+j+1;
 			for (int j2=j+1;j2<n;++j2){ // limit to only columns in separator
 				//A(i,j2) -= ai*r(j2);
-				*Aptr -= ai*r(j2);
+				*Aptr -= ai* *rptr;
 				Aptr++;
+				rptr++;
 			}
 		}
 		if (verbose) print(sub(A,0,m,j+1,n), "updated A");
