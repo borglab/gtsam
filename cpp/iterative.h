07cc95e4c420e03247d614e20ca5615fbb03778b
Frank Dellaert <dellaert@gmail.com>
2009-12-31 12:55:16 +0000

    Comments and formatting

--- cpp/iterative.h
+++ cpp/iterative.h
@@ -6,12 +6,28 @@
  */
 
 #include "Matrix.h"
+
 namespace gtsam {
 
-	class GaussianFactorGraph;
-	class VectorConfig;
+	/**
+	 * Method of conjugate gradients (CG) template
+	 * "System" class S needs gradient(S,v), e=S*v, v=S^e
+	 * "Vector" class V needs dot(v,v), -v, v+v, s*v
+	 * "Vector" class E needs dot(v,v)
+	 * @param Ab, the "system" that needs to be solved, examples below
+	 * @param x is the initial estimate
+	 * @param epsilon determines the convergence criterion: norm(g)<epsilon*norm(g0)
+	 * @param maxIterations, if 0 will be set to |x|
+	 * @param steepest flag, if true does steepest descent, not CG
+	 * */
+	template<class S, class V, class E>
+	V conjugateGradients(const S& Ab, V x, bool verbose, double epsilon,
+			size_t maxIterations, bool steepest = false);
 
-	/** combined system |Ax-b_|^2 */
+	/**
+	 * Helper class encapsulating the combined system |Ax-b_|^2
+	 * Needed to run Conjugate Gradients on matrices
+	 * */
 	class System {
 	private:
 		const Matrix& A_;
@@ -41,25 +57,18 @@ namespace gtsam {
 	};
 
 	/**
-	 * Method of conjugate gradients (CG) template
-	 * "System" class S needs gradient(S,v), e=S*v, v=S^e
-	 * "Vector" class V needs dot(v,v), -v, v+v, s*v
-	 * "Vector" class E needs dot(v,v)
-	 * @param Ab, the "system" that needs to be solved, examples below
-	 * @param x is the initial estimate
-	 * @param epsilon determines the convergence criterion: norm(g)<epsilon*norm(g0)
-	 * @param maxIterations, if 0 will be set to |x|
-	 * @param steepest flag, if true does steepest descent, not CG
-	 * */
-	template<class S, class V, class E>
-	V conjugateGradients(const S& Ab, V x, bool verbose, double epsilon,
-			size_t maxIterations, bool steepest = false);
-
-	/**
 	 * Method of steepest gradients, System version
 	 */
 	Vector steepestDescent(const System& Ab, const Vector& x, bool verbose =
 			false, double epsilon = 1e-3, size_t maxIterations = 0);
+
+	/**
+	 * Method of conjugate gradients (CG), System version
+	 */
+	Vector conjugateGradientDescent(const System& Ab, const Vector& x,
+			bool verbose = false, double epsilon = 1e-3, size_t maxIterations = 0);
+
+	/** convenience calls using matrices, will create System class internally: */
 
 	/**
 	 * Method of steepest gradients, Matrix version
@@ -68,23 +77,20 @@ namespace gtsam {
 			bool verbose = false, double epsilon = 1e-3, size_t maxIterations = 0);
 
 	/**
-	 * Method of steepest gradients, Gaussian Factor Graph version
-	 * */
-	VectorConfig steepestDescent(const GaussianFactorGraph& fg,
-			const VectorConfig& x, bool verbose = false, double epsilon = 1e-3,
-			size_t maxIterations = 0);
-
-	/**
-	 * Method of conjugate gradients (CG), System version
-	 */
-	Vector conjugateGradientDescent(const System& Ab, const Vector& x,
-			bool verbose = false, double epsilon = 1e-3, size_t maxIterations = 0);
-
-	/**
 	 * Method of conjugate gradients (CG), Matrix version
 	 */
 	Vector conjugateGradientDescent(const Matrix& A, const Vector& b,
 			const Vector& x, bool verbose = false, double epsilon = 1e-3,
+			size_t maxIterations = 0);
+
+	class GaussianFactorGraph;
+	class VectorConfig;
+
+	/**
+	 * Method of steepest gradients, Gaussian Factor Graph version
+	 * */
+	VectorConfig steepestDescent(const GaussianFactorGraph& fg,
+			const VectorConfig& x, bool verbose = false, double epsilon = 1e-3,
 			size_t maxIterations = 0);
 
 	/**
