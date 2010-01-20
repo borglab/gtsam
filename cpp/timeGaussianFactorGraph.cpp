b4f91632964008fdbee5b9f08a675edf824d2dbe
Alex Cunningham <alexgcunningham@gmail.com>
2010-01-20 14:31:24 +0000

    Added more labels and results (with higher numbers of repetitions) for timeGaussianFactorGraph

--- cpp/timeGaussianFactorGraph.cpp
+++ cpp/timeGaussianFactorGraph.cpp
@@ -45,23 +45,34 @@ double timePlanarSmoother(int N) {
 /* ************************************************************************* */
 TEST(timeGaussianFactorGraph, linearTime)
 {
-	int T = 1000;
-	double time1 = timeKalmanSmoother(  T);  cout << time1 << endl;
-	double time2 = timeKalmanSmoother(2*T);  cout << time2 << endl;
+	// Original T = 1000;
+
+	// Alex's Results
+	// T = 100000
+	// 1907: T - 1.65, 2T = 3.28
+
+	int T = 100000;
+	double time1 = timeKalmanSmoother(  T);  cout << "timeKalmanSmoother( T): " << time1;
+	double time2 = timeKalmanSmoother(2*T);  cout << "  (2*T): " << time2 << endl;
 	DOUBLES_EQUAL(2*time1,time2,0.1);
 }
 
 /* ************************************************************************* */
 TEST(timeGaussianFactorGraph, planar)
 {
+	// Timing with N = 30
 	// 1741: 8.12, 8.12, 8.12, 8.14, 8.16
 	// 1742: 5.97, 5.97, 5.97, 5.99, 6.02
 	// 1746: 5.96, 5.96, 5.97, 6.00, 6.04
 	// 1748: 5.91, 5.92, 5.93, 5.95, 5.96
 	// 1839: 0.206956 0.206939 0.206213 0.206092 0.206780 // colamd !!!!
-	int N = 30;
-	double time = timePlanarSmoother(N); cout << time << endl;
-	DOUBLES_EQUAL(5.97,time,0.1);
+
+	// Alex's Machine
+	// 1907 (N = 30) :  0.14
+	//      (N = 100): 16.36
+	int N = 100;
+	double time = timePlanarSmoother(N); cout << "timeGaussianFactorGraph: " << time << endl;
+	// DOUBLES_EQUAL(5.97,time,0.1);
 }
 
 /* ************************************************************************* */
