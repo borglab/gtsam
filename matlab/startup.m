3120d6842f148184cf6b4db53c61f22c264ad86e
Viorela Ila <iviorela@gmail.com>
2010-04-07 21:47:21 +0000

    startup

---
+++ matlab/startup.m
@@ -0,0 +1,14 @@
+% Matlab code to test gtsam toolbox
+%
+%
+%   
+%   
+clc;
+disp(' ');
+disp('                    Matlab scripts to test GTSAM toolbox ');
+disp(' ');
+
+path(path,genpath(pwd));
+toolboxpath = '~/toolbox/gtsam'
+addpath(toolboxpath);
+
