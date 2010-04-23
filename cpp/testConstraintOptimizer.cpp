e35e7a98d15dfae758d8fed4ded1368a3f6ddcb5
Alex Cunningham <alexgcunningham@gmail.com>
2010-04-23 02:57:37 +0000

    Added analytic hessian SQP example, removed binary

--- cpp/testConstraintOptimizer.cpp
+++ cpp/testConstraintOptimizer.cpp
@@ -6,6 +6,7 @@
 
 #include <CppUnitLite/TestHarness.h>
 #include <boost/tuple/tuple.hpp>
+#include <boost/optional.hpp>
 
 #include <ConstraintOptimizer.h>
 
@@ -64,6 +65,90 @@ TEST( matrix, CQP_example_automatic ) {
 
 	CHECK(assert_equal(expected, actState));
 	CHECK(actLam.size() == 2);
+}
+
+/* ************************************************************************* */
+
+/** SQP example from SQP tutorial */
+namespace sqp_example1 {
+
+	/**
+	 * objective function with gradient and hessian
+	 * fx = (x2-2)^2 + x1^2;
+	 */
+	Vector objective(const Vector& x, boost::optional<Vector&> g = boost::none,
+			  boost::optional<Matrix&> B = boost::none) {
+		double x1 = x(0), x2 = x(1);
+		if (g) *g = Vector_(2, 2.0*x1, 2.0*(x2-2.0));
+		if (B) *B = 2.0 * eye(2,2);
+		return Vector_(1, (x2-2)*(x2-2) + x1*x1);
+	}
+
+	/**
+	 * constraint function with gradient and hessian
+	 * cx = 4*x1^2 + x2^2 - 1;
+	 */
+	Vector constraint(const Vector& x, boost::optional<Matrix&> A = boost::none,
+			  boost::optional<Matrix&> B = boost::none) {
+		double x1 = x(0), x2 = x(1);
+		if (A) *A = Matrix_(2,1, 8.0*x1, 2.0*x2);
+		if (B) *B = Matrix_(2,2,
+				8.0, 0.0,
+				0.0, 2.0);
+		return Vector_(1, 4.0*x1*x1 + x2*x2 - 1.0);
+	}
+
+
+}
+
+/* ************************************************************************* */
+TEST( matrix, SQP_simple_analytic ) {
+	using namespace sqp_example1;
+
+//	 Problem:
+//	  min(x) f(x) = (x2-2)^2 + x1^2
+//	    s.t. c(x) = 4x1^2 + x2^2 - 1 =0
+//	 state is x = [x1 x2]' , with dim(state) = 2
+//	 constraint has dim p = 1
+
+	// parameters
+	double stepsize = 0.25;
+	size_t maxIt = 50;
+
+	// initial conditions
+	Vector x0 = Vector_(2, 2.0, 4.0),
+		   lam0 = Vector_(1, -0.5);
+
+	// current state
+	Vector x = x0, lam = lam0;
+
+	for (size_t i =0; i<maxIt; ++i) {
+
+		// evaluate functions
+		Vector dfx;
+		Matrix dcx, ddfx, ddcx;
+		Vector fx = objective(x, dfx, ddfx);
+		Vector cx = constraint(x, dcx, ddcx);
+
+		// use analytic hessian
+		Matrix B = ddfx - lam(0)*ddcx;
+
+		// solve subproblem
+		Vector delta, lambda;
+		boost::tie(delta, lambda) = solveCQP(B, -dcx, dfx, -cx);
+
+		// update
+		Vector step = stepsize * delta;
+		x = x + step;
+		lam = lambda;
+	}
+
+	// verify
+	Vector expX = Vector_(2, 0.0, 1.0),
+		   expLambda = Vector_(1, -1.0);
+
+	CHECK(assert_equal(expX, x, 1e-4));
+	CHECK(assert_equal(expLambda, lam, 1e-4));
 }
 	
 
