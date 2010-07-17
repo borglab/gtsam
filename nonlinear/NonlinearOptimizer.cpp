864b66ea93f89becd10bda9a5ae4d0b4b655ba39
Frank Dellaert <dellaert@gmail.com>
2010-07-17 15:04:49 +0000

    Added dummy c++ file to make library compile even if no enable_ldl

---
+++ nonlinear/NonlinearOptimizer.cpp
@@ -0,0 +1,15 @@
+/**
+ * NonlinearOptimizer.cpp
+ * @brief: Dummy c++ file to have *something* in lin-nonlinear if USE_LDL not defined.
+ * @Author: Frank Dellaert
+ * Created on: Jul 17, 2010
+ */
+
+#pragma once
+
+#include "NonlinearOptimizer.h"
+
+namespace gtsam {
+
+
+}
