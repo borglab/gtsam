e718306fd040e0769dc7c77fab8a357cf69a49c1
Kai Ni <nikai97@gmail.com>
2010-10-18 16:15:06 +0000

    fix test wrap problem

--- wrap/geometry.h
+++ wrap/geometry.h
@@ -1,14 +1,3 @@
-/* ----------------------------------------------------------------------------
-
- * GTSAM Copyright 2010, Georgia Tech Research Corporation, 
- * Atlanta, Georgia 30332-0415
- * All Rights Reserved
- * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
-
- * See LICENSE for the license information
-
- * -------------------------------------------------------------------------- */
-
 
 class Point2 {
  Point2();
