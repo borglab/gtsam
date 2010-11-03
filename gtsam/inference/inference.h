d9d2cb7dda44a64a1912898526dd17a26ec7db64
Richard Roberts <richard.jw.roberts@gmail.com>
2010-11-03 21:34:07 +0000

    Removed commented code

--- gtsam/inference/inference.h
+++ gtsam/inference/inference.h
@@ -52,45 +52,4 @@ namespace gtsam {
 
 	};
 
-	// ELIMINATE: FACTOR GRAPH -> BAYES NET
-
-//	/**
-//   * Eliminate a single node yielding a Conditional
-//   * Eliminates the factors from the factor graph through findAndRemoveFactors
-//   * and adds a new factor on the separator to the factor graph
-//   */
-//	template<class Factor, class Conditional>
-//	boost::shared_ptr<Conditional>
-//	eliminateOne(FactorGraph<Factor>& factorGraph, Index key);
-//
-//	/**
-//	 * eliminate factor graph using the given (not necessarily complete)
-//	 * ordering, yielding a chordal Bayes net and (partially eliminated) FG
-//	 */
-//	template<class Factor, class Conditional>
-//	BayesNet<Conditional> eliminate(FactorGraph<Factor>& factorGraph, const Ordering& ordering);
-
-	// FACTOR/MARGINALIZE: BAYES NET -> FACTOR GRAPH
-
-//	/**
-//	 * Factor P(X) as P(not keys|keys) P(keys)
-//	 * @return P(not keys|keys) as an incomplete BayesNet, and P(keys) as a factor graph
-//	 */
-//	template<class Factor, class Conditional>
-//	std::pair< BayesNet<Conditional>, FactorGraph<Factor> >
-//	factor(const BayesNet<Conditional>& bn, const Ordering& keys);
-//
-//	/**
-//	 * integrate out all except ordering, might be inefficient as the ordering
-//	 * will simply be the current ordering with the keys put in the back
-//	 */
-//	template<class Factor, class Conditional>
-//	FactorGraph<Factor> marginalize(const BayesNet<Conditional>& bn, const Ordering& keys);
-
-	/**
-	 * Hacked-together function to compute a Gaussian marginal for the given variable.
-	 * todo: This is inefficient!
-	 */
-	//std::pair<Vector,Matrix> marginalGaussian(const GaussianFactorGraph& fg, const Symbol& key);
-
 } /// namespace gtsam
