3e1761b81bd3002bf3d9348037dff73980dff7ae
Yong-Dian Jian <ydjian@gmail.com>
2010-11-05 05:35:04 +0000

    update for spcg

--- gtsam/linear/IterativeOptimizationParameters.h
+++ gtsam/linear/IterativeOptimizationParameters.h
@@ -7,6 +7,9 @@
 
 #pragma once
 
+#include <vector>
+#include <boost/shared_ptr.hpp>
+
 namespace gtsam {
 
 	// a container for all related parameters
@@ -19,12 +22,19 @@ namespace gtsam {
 			ERROR,
 		} verbosityLevel;
 
-	protected:
+	public:
 		int maxIterations_;
 		int reset_ ; // number of iterations before reset, for cg and gmres
 		double epsilon_; // relative error
 		double epsilon_abs_; // absolute error
 		verbosityLevel verbosity_;
+
+        // specialize for some solvers
+		typedef size_t Index;
+		typedef std::vector<Index> Spec ;
+		typedef boost::shared_ptr<Spec> sharedSpec ;
+        sharedSpec reduce_spec_ ;
+        sharedSpec skeleton_spec_ ;
 
 	public:
 		IterativeOptimizationParameters():
@@ -32,14 +42,18 @@ namespace gtsam {
 			reset_(101),
 			epsilon_(1e-5),
 			epsilon_abs_(1e-5),
-			verbosity_(ERROR) {}
+			verbosity_(ERROR),
+			reduce_spec_(), skeleton_spec_() {}
 
 		IterativeOptimizationParameters(const IterativeOptimizationParameters &parameters):
 			maxIterations_(parameters.maxIterations_),
 			reset_(parameters.reset_),
 			epsilon_(parameters.epsilon_),
 			epsilon_abs_(parameters.epsilon_abs_),
-			verbosity_(parameters.verbosity_) {}
+			verbosity_(parameters.verbosity_),
+			reduce_spec_(parameters.reduce_spec_),
+			skeleton_spec_(parameters.skeleton_spec_)
+			{}
 
 
 		IterativeOptimizationParameters
@@ -54,6 +68,5 @@ namespace gtsam {
 		double epsilon() const { return epsilon_ ;}
 		double epsilon_abs() const { return epsilon_abs_ ; }
 		verbosityLevel verbosity() const { return verbosity_ ; }
-
 	};
 }
