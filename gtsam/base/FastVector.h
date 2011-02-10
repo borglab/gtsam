7404f78bc1c6a7b7050d56527da675667c7b776f
Richard Roberts <richard.jw.roberts@gmail.com>
2011-02-10 01:52:01 +0000

    Added "FastVector", an std::vector using a boost pool_allocator

---
+++ gtsam/base/FastVector.h
@@ -0,0 +1,55 @@
+/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation,
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file    FastSet.h
+ * @brief   A thin wrapper around std::vector that uses boost's pool_allocator.
+ * @author  Richard Roberts
+ * @created Feb 9, 2011
+ */
+
+#pragma once
+
+#include <vector>
+#include <boost/pool/pool_alloc.hpp>
+
+namespace gtsam {
+
+/**
+ * FastVector is a thin wrapper around std::vector that uses the boost
+ * pool_allocator instead of the default STL allocator.  This is just a
+ * convenience to avoid having lengthy types in the code.  Through timing,
+ * we've seen that the pool_allocator can lead to speedups of several
+ * percent.
+ */
+template<typename VALUE>
+class FastVector: public std::vector<VALUE, boost::pool_allocator<VALUE> > {
+
+public:
+
+  typedef std::vector<VALUE, boost::pool_allocator<VALUE> > Base;
+
+  /** Default constructor */
+  FastVector() {}
+
+  /** Constructor from a range, passes through to base class */
+  template<typename INPUTITERATOR>
+  FastVector(INPUTITERATOR first, INPUTITERATOR last) : Base(first, last) {}
+
+  /** Copy constructor from another FastSet */
+  FastVector(const FastVector<VALUE>& x) : Base(x) {}
+
+  /** Copy constructor from the base map class */
+  FastVector(const Base& x) : Base(x) {}
+
+};
+
+}
