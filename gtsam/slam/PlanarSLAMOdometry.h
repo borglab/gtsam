9db83d5dad2dfb4daf442ffbbb0f96076e14d21c
Frank Dellaert <dellaert@gmail.com>
2011-11-04 04:26:37 +0000

    added PlanarSLAMOdometry for wrapping

---
+++ gtsam/slam/PlanarSLAMOdometry.h
@@ -0,0 +1,28 @@
+/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation, 
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file PlanarSLAMOdometry.h
+ * @brief Re-created on Feb 22, 2010 for compatibility with MATLAB
+ * @author Frank Dellaert
+ */
+
+#pragma once
+
+#include <gtsam/slam/planarSLAM.h>
+#include <gtsam/slam/planarSLAMValues.h>
+
+namespace gtsam {
+
+	typedef gtsam::planarSLAM::Odometry PlanarSLAMOdometry;
+
+}
+
