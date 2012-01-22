852bb149ce0a3d6c861df021ff13b442bdde42da
Frank Dellaert <dellaert@gmail.com>
2012-01-22 06:28:11 +0000

    documentation

--- tests/matlab/CHECK.m
+++ tests/matlab/CHECK.m
@@ -1,4 +1,9 @@
 function CHECK(name,assertion)
+% CHECK throw an error if an assertion fails
+%
+% CHECK(name,assertion)
+%  - name of test
+%  - assertion
 
 if (assertion~=1)
   error(['CHECK ' name ' fails']);
