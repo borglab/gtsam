0e0680d3c967982bacc925e866fb8e5a7ce46873
Richard Roberts <richard.jw.roberts@gmail.com>
2012-05-15 14:35:40 +0000

    Templated version of skewSymmetric to work with fixed-size matrices

--- gtsam/base/Matrix.h
+++ gtsam/base/Matrix.h
@@ -398,7 +398,8 @@ Matrix vector_scale(const Matrix& A, const Vector& v, bool inf_mask = false); //
  * @return a 3*3 skew symmetric matrix
 */
 Matrix3 skewSymmetric(double wx, double wy, double wz);
-inline Matrix3 skewSymmetric(const Vector& w) { return skewSymmetric(w(0),w(1),w(2));}
+template<class Derived>
+inline Matrix3 skewSymmetric(const Eigen::MatrixBase<Derived>& w) { return skewSymmetric(w(0),w(1),w(2));}
 
 /** Use SVD to calculate inverse square root of a matrix */
 Matrix inverse_square_root(const Matrix& A);
