da480b45eb4196e76210bdb3cd58003c01f47ebb
Stephen Williams <stephen.vincent.williams@gmail.com>
2012-05-15 15:28:24 +0000

    Updated license

--- gtsam/geometry/tests/timePose3.cpp
+++ gtsam/geometry/tests/timePose3.cpp
@@ -1,8 +1,8 @@
 /* ----------------------------------------------------------------------------
 
- * GTSAM Copyright 2010, Georgia Tech Tesearch Corporation,
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation,
  * Atlanta, Georgia 30332-0415
- * All Tights Teserved
+ * All Rights Reserved
  * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
 
  * See LICENSE for the license information
