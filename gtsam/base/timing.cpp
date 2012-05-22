2060f1dd22b9e960bb83076b1a60f07e871802e4
Richard Roberts <richard.jw.roberts@gmail.com>
2012-05-22 20:37:13 +0000

    Initial changes to compile on windows

--- gtsam/base/timing.cpp
+++ gtsam/base/timing.cpp
@@ -20,7 +20,6 @@
 #include <stdio.h>
 #include <iostream>
 #include <iomanip>
-#include <sys/time.h>
 #include <stdlib.h>
 #include <boost/foreach.hpp>
 #include <boost/format.hpp>
@@ -153,15 +152,13 @@ const boost::shared_ptr<TimingOutline>& TimingOutline::child(size_t child, const
 void TimingOutline::tic() {
 	assert(!timerActive_);
 	timerActive_ = true;
-	gettimeofday(&t0_, NULL);
+	t0_ = clock_t::now();
 }
 
 /* ************************************************************************* */
 void TimingOutline::toc() {
-	struct timeval t;
-	gettimeofday(&t, NULL);
 	assert(timerActive_);
-	add(t.tv_sec*1000000 + t.tv_usec - (t0_.tv_sec*1000000 + t0_.tv_usec));
+	add(boost::chrono::duration_cast<duration_t>(clock_t::now() - t0_).count());
 	timerActive_ = false;
 }
 
