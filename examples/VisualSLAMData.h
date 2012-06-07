e0686484b68e112b6b8cde0fa7e03b5642e49850
Chris Beall <chrisbeall@gmail.com>
2012-06-07 14:53:39 +0000

    fixed compile errors

--- examples/VisualSLAMData.h
+++ examples/VisualSLAMData.h
@@ -66,8 +66,8 @@ struct VisualSLAMExampleData {
 		double theta = 0.0;
 		double r = 30.0;
 		for (int i=0; i<n; ++i, theta += 2*M_PI/n) {
-			Point3 C = gtsam::Point3(r*cos(theta), r*sin(theta), 0.0);
-			SimpleCamera camera = SimpleCamera::lookat(C, Point3(), Point3(0,0,1));
+			gtsam::Point3 C = gtsam::Point3(r*cos(theta), r*sin(theta), 0.0);
+			gtsam::SimpleCamera camera = gtsam::SimpleCamera::lookat(C, gtsam::Point3(), gtsam::Point3(0,0,1));
 			data.poses.push_back(camera.pose());
 		}
 		data.odometry = data.poses[0].between(data.poses[1]);
