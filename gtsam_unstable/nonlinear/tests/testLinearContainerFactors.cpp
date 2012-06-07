111ef8a0f001787f55292b8011da912dd061bff3
Alex Cunningham <alexgcunningham@gmail.com>
2012-06-07 18:16:37 +0000

    Moved LinearizedFactor from MastSLAM, started on a test for LinearContainerFactor

---
+++ gtsam_unstable/nonlinear/tests/testLinearContainerFactors.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file testLinearContainerFactors.cpp
+ *
+ * @brief Tests the use of nonlinear containers for simple integration of linear factors into nonlinear graphs
+ *
+ * @date Jun 7, 2012
+ * @author Alex Cunningham
+ */
+
+#include <CppUnitLite/TestHarness.h>
+
+
+/* ************************************************************************* */
+TEST( testLinearContainerFactors, jacobian_factor ) {
+
+//	LinearContainerFactor actualLinFactor();
+
+}
+
+/* ************************************************************************* */
+int main() { TestResult tr; return TestRegistry::runAllTests(tr); }
+/* ************************************************************************* */
