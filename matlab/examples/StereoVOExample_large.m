642e180ff72523a69c1efb1ca8bbd7a61c9677fa
Chris Beall <chrisbeall@gmail.com>
2012-06-14 04:51:42 +0000

    renamed StereoVO data files

--- matlab/examples/StereoVOExample_large.m
+++ matlab/examples/StereoVOExample_large.m
@@ -24,7 +24,7 @@ initial = visualSLAMValues;
 %% load the initial poses from VO
 % row format: camera_id 4x4 pose (row, major)
 fprintf(1,'Reading data\n');
-cameras = dlmread('../../examples/Data/VO_camera_poses.txt');
+cameras = dlmread('../../examples/Data/VO_camera_poses_large.txt');
 for i=1:size(cameras,1)
     pose = gtsamPose3(reshape(cameras(i,2:17),4,4)');
     initial.insertPose(symbol('x',cameras(i,1)),pose);
@@ -32,7 +32,7 @@ end
 
 %% load stereo measurements and initialize landmarks
 % camera_id landmark_id uL uR v X Y Z
-measurements = dlmread('../../examples/Data/VO_stereo_factors.txt');
+measurements = dlmread('../../examples/Data/VO_stereo_factors_large.txt');
 
 fprintf(1,'Creating Graph\n'); tic
 for i=1:size(measurements,1)
