e337ab8b1d6dc5262d04ae87f4cc468a4060ac5c
Alex Cunningham <alexgcunningham@gmail.com>
2012-07-10 18:36:21 +0000

    Merge branch 'master' into wrap_mods_local

---
+++ gtsam_unstable/nonlinear/LinearContainerFactor.cpp
@@ -0,0 +1,148 @@
+/**
+ * @file LinearContainerFactor.cpp
+ *
+ * @date Jul 6, 2012
+ * @author Alex Cunningham
+ */
+
+#include <gtsam_unstable/nonlinear/LinearContainerFactor.h>
+
+#include <boost/foreach.hpp>
+
+namespace gtsam {
+
+/* ************************************************************************* */
+void LinearContainerFactor::rekeyFactor(const Ordering& ordering) {
+	Ordering::InvertedMap invOrdering = ordering.invert(); // TODO: inefficient - make more selective ordering invert
+	rekeyFactor(invOrdering);
+}
+
+/* ************************************************************************* */
+void LinearContainerFactor::rekeyFactor(const Ordering::InvertedMap& invOrdering) {
+	BOOST_FOREACH(Index& idx, factor_->keys()) {
+		Key fullKey = invOrdering.find(idx)->second;
+		idx = fullKey;
+		keys_.push_back(fullKey);
+	}
+}
+
+/* ************************************************************************* */
+LinearContainerFactor::LinearContainerFactor(
+		const JacobianFactor& factor, const Ordering& ordering)
+: factor_(factor.clone()) {
+	rekeyFactor(ordering);
+}
+
+/* ************************************************************************* */
+LinearContainerFactor::LinearContainerFactor(
+		const HessianFactor& factor, const Ordering& ordering)
+: factor_(factor.clone()) {
+	rekeyFactor(ordering);
+}
+
+/* ************************************************************************* */
+LinearContainerFactor::LinearContainerFactor(
+		const GaussianFactor::shared_ptr& factor, const Ordering& ordering)
+: factor_(factor->clone()) {
+	rekeyFactor(ordering);
+}
+
+/* ************************************************************************* */
+LinearContainerFactor::LinearContainerFactor(
+		const GaussianFactor::shared_ptr& factor)
+: factor_(factor->clone())
+{
+}
+
+/* ************************************************************************* */
+LinearContainerFactor::LinearContainerFactor(const JacobianFactor& factor,
+		const Ordering::InvertedMap& inverted_ordering)
+: factor_(factor.clone()) {
+	rekeyFactor(inverted_ordering);
+}
+
+/* ************************************************************************* */
+LinearContainerFactor::LinearContainerFactor(const HessianFactor& factor,
+		const Ordering::InvertedMap& inverted_ordering)
+: factor_(factor.clone()) {
+	rekeyFactor(inverted_ordering);
+}
+
+/* ************************************************************************* */
+LinearContainerFactor::LinearContainerFactor(
+		const GaussianFactor::shared_ptr& factor,
+		const Ordering::InvertedMap& ordering)
+: factor_(factor->clone()) {
+	rekeyFactor(ordering);
+}
+
+/* ************************************************************************* */
+void LinearContainerFactor::print(const std::string& s, const KeyFormatter& keyFormatter) const {
+	Base::print(s+"LinearContainerFactor", keyFormatter);
+	if (factor_)
+		factor_->print("   Stored Factor", keyFormatter);
+}
+
+/* ************************************************************************* */
+bool LinearContainerFactor::equals(const NonlinearFactor& f, double tol) const {
+	const LinearContainerFactor* jcf = dynamic_cast<const LinearContainerFactor*>(&f);
+	return jcf && factor_->equals(*jcf->factor_, tol) && NonlinearFactor::equals(f);
+}
+
+/* ************************************************************************* */
+double LinearContainerFactor::error(const Values& c) const {
+	//	VectorValues vecvalues;
+	//	// FIXME: add values correctly here
+	//	return factor_.error(vecvalues);
+		return 0; // FIXME: placeholder
+}
+
+/* ************************************************************************* */
+size_t LinearContainerFactor::dim() const {
+	if (isJacobian())
+		return toJacobian()->get_model()->dim();
+	else
+		return 1; // Hessians don't have true dimension
+}
+
+/* ************************************************************************* */
+boost::shared_ptr<GaussianFactor>
+LinearContainerFactor::linearize(const Values& c, const Ordering& ordering) const {
+	// clone factor
+	boost::shared_ptr<GaussianFactor> result = factor_->clone();
+
+	// rekey
+	BOOST_FOREACH(Index& key, result->keys())
+		key = ordering[key];
+
+	return result;
+}
+
+/* ************************************************************************* */
+bool LinearContainerFactor::isJacobian() const {
+	return boost::shared_dynamic_cast<JacobianFactor>(factor_);
+}
+
+/* ************************************************************************* */
+JacobianFactor::shared_ptr LinearContainerFactor::toJacobian() const {
+	return boost::shared_dynamic_cast<JacobianFactor>(factor_);
+}
+
+/* ************************************************************************* */
+HessianFactor::shared_ptr LinearContainerFactor::toHessian() const {
+	return boost::shared_dynamic_cast<HessianFactor>(factor_);
+}
+
+/* ************************************************************************* */
+GaussianFactor::shared_ptr LinearContainerFactor::negate(const Ordering& ordering) const {
+	GaussianFactor::shared_ptr result = factor_->negate();
+	BOOST_FOREACH(Key& key, result->keys())
+		key = ordering[key];
+	return result;
+}
+
+/* ************************************************************************* */
+} // \namespace gtsam
+
+
+
---
+++ gtsam_unstable/nonlinear/LinearContainerFactor.h
@@ -0,0 +1,113 @@
+/**
+ * @file LinearContainerFactor.h
+ *
+ * @brief Wrap Jacobian and Hessian linear factors to allow simple injection into a nonlinear graph
+ * 
+ * @date Jul 6, 2012
+ * @author Alex Cunningham
+ */
+
+#pragma once
+
+#include <gtsam/linear/HessianFactor.h>
+#include <gtsam/nonlinear/NonlinearFactor.h>
+
+namespace gtsam {
+
+/**
+ * Dummy version of a generic linear factor to be injected into a nonlinear factor graph
+ */
+class LinearContainerFactor : public NonlinearFactor {
+protected:
+
+	GaussianFactor::shared_ptr factor_;
+
+public:
+
+	/** Primary constructor: store a linear factor and decode the ordering */
+	LinearContainerFactor(const JacobianFactor& factor, const Ordering& ordering);
+
+	/** Primary constructor: store a linear factor and decode the ordering */
+	LinearContainerFactor(const HessianFactor& factor, const Ordering& ordering);
+
+	/** Constructor from shared_ptr */
+	LinearContainerFactor(const GaussianFactor::shared_ptr& factor, const Ordering& ordering);
+
+	/** Constructor from re-keyed factor: all indices assumed replaced with Key */
+	LinearContainerFactor(const GaussianFactor::shared_ptr& factor);
+
+	/** Alternate constructor: store a linear factor and decode keys with inverted ordering*/
+	LinearContainerFactor(const JacobianFactor& factor,
+			const Ordering::InvertedMap& inverted_ordering);
+
+	/** Alternate constructor: store a linear factor and decode keys with inverted ordering*/
+	LinearContainerFactor(const HessianFactor& factor,
+			const Ordering::InvertedMap& inverted_ordering);
+
+	/** Constructor from shared_ptr with inverted ordering*/
+	LinearContainerFactor(const GaussianFactor::shared_ptr& factor,
+			const Ordering::InvertedMap& ordering);
+
+	// Access
+
+	const GaussianFactor::shared_ptr& factor() const { return factor_; }
+
+	// Testable
+
+  /** print */
+  void print(const std::string& s = "", const KeyFormatter& keyFormatter = gtsam::DefaultKeyFormatter) const;
+
+  /** Check if two factors are equal */
+  bool equals(const NonlinearFactor& f, double tol = 1e-9) const;
+
+	// NonlinearFactor
+
+  /**
+   * Calculate the error of the factor: uses the underlying linear factor to compute ordering
+   */
+  double error(const Values& c) const;
+
+  /** get the dimension of the factor: rows of linear factor */
+  size_t dim() const;
+
+  /** linearize to a GaussianFactor: values has no effect, just clones/rekeys underlying factor */
+  boost::shared_ptr<GaussianFactor>
+  linearize(const Values& c, const Ordering& ordering) const;
+
+  /**
+   * Creates an anti-factor directly and performs rekeying due to ordering
+   */
+  GaussianFactor::shared_ptr negate(const Ordering& ordering) const;
+
+  /**
+   * Creates a shared_ptr clone of the factor - needs to be specialized to allow
+   * for subclasses
+   *
+   * Clones the underlying linear factor
+   */
+  NonlinearFactor::shared_ptr clone() const {
+  	return NonlinearFactor::shared_ptr(new LinearContainerFactor(factor_));
+  }
+
+  // casting syntactic sugar
+
+  /**
+   * Simple check whether this is a Jacobian or Hessian factor
+   */
+  bool isJacobian() const;
+
+  /** Casts to JacobianFactor */
+  JacobianFactor::shared_ptr toJacobian() const;
+
+  /** Casts to HessianFactor */
+  HessianFactor::shared_ptr toHessian() const;
+
+protected:
+  void rekeyFactor(const Ordering& ordering);
+  void rekeyFactor(const Ordering::InvertedMap& invOrdering);
+
+}; // \class LinearContainerFactor
+
+} // \namespace gtsam
+
+
