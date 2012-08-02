b211c1070dd6262d63c266b1be8a630e9eb47c28
Alex Cunningham <alexgcunningham@gmail.com>
2012-08-02 20:47:16 +0000

    Rearranged the inverse depth factor/camera, created a gtsam_unstable/geometry folder - run cmake to find the new/moved files

--- gtsam_unstable/slam/tests/testInvDepthFactor3.cpp
+++ gtsam_unstable/slam/tests/testInvDepthFactor3.cpp
@@ -5,156 +5,25 @@
  *      Author: cbeall3
  */
 
-#include <gtsam_unstable/slam/InvDepthFactor3.h>
-#include <gtsam/geometry/SimpleCamera.h>
 #include <CppUnitLite/TestHarness.h>
+
+#include <gtsam/geometry/SimpleCamera.h>
 #include <gtsam/nonlinear/NonlinearEquality.h>
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
 #include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
 #include <gtsam/nonlinear/Symbol.h>
 
+#include <gtsam_unstable/slam/InvDepthFactor3.h>
+
 using namespace std;
 using namespace gtsam;
 
-/* ************************************************************************* */
 static Cal3_S2::shared_ptr K(new Cal3_S2(1500, 1200, 0, 640, 480));
+static SharedNoiseModel sigma(noiseModel::Unit::Create(2));
+
 Pose3 level_pose = Pose3(Rot3::ypr(-M_PI/2, 0., -M_PI/2), gtsam::Point3(0,0,1));
 SimpleCamera level_camera(level_pose, *K);
 
-/* ************************************************************************* */
-TEST( InvDepthFactor, Project1) {
-
-  // landmark 5 meters infront of camera
-  Point3 landmark(5, 0, 1);
-
-  Point2 expected_uv = level_camera.project(landmark);
-
-  InvDepthCamera3<Cal3_S2> inv_camera(level_pose, K);
-  LieVector inv_landmark(5, 1., 0., 1., 0., 0.);
-  LieScalar inv_depth(1./4);
-  Point2 actual_uv = inv_camera.project(inv_landmark, inv_depth);
-  CHECK(assert_equal(expected_uv, actual_uv));
-  CHECK(assert_equal(Point2(640,480), actual_uv));
-
-}
-
-/* ************************************************************************* */
-TEST( InvDepthFactor, Project2) {
-
-  // landmark 1m to the left and 1m up from camera
-  // inv landmark xyz is same as camera xyz, so depth  actually doesn't matter
-  Point3 landmark(1, 1, 2);
-  Point2 expected = level_camera.project(landmark);
-
-  InvDepthCamera3<Cal3_S2> inv_camera(level_pose, K);
-  LieVector diag_landmark(5, 0., 0., 1., M_PI/4., atan(1/sqrt(2)));
-  LieScalar inv_depth(1/sqrt(3));
-  Point2 actual = inv_camera.project(diag_landmark, inv_depth);
-  CHECK(assert_equal(expected, actual));
-}
-
-/* ************************************************************************* */
-TEST( InvDepthFactor, Project3) {
-
-  // landmark 1m to the left and 1m up from camera
-  // inv depth landmark xyz at origion
-  Point3 landmark(1, 1, 2);
-  Point2 expected = level_camera.project(landmark);
-
-  InvDepthCamera3<Cal3_S2> inv_camera(level_pose, K);
-  LieVector diag_landmark(5, 0., 0., 0., M_PI/4., atan(2./sqrt(2)));
-  LieScalar inv_depth( 1./sqrt(1+1+4));
-  Point2 actual = inv_camera.project(diag_landmark, inv_depth);
-  CHECK(assert_equal(expected, actual));
-}
-
-/* ************************************************************************* */
-TEST( InvDepthFactor, Project4) {
-
-  // landmark 4m to the left and 1m up from camera
-  // inv depth landmark xyz at origion
-  Point3 landmark(1, 4, 2);
-  Point2 expected = level_camera.project(landmark);
-
-  InvDepthCamera3<Cal3_S2> inv_camera(level_pose, K);
-  LieVector diag_landmark(5, 0., 0., 0., atan(4/1), atan(2./sqrt(1+16)));
-  LieScalar inv_depth(1./sqrt(1+16+4));
-  Point2 actual = inv_camera.project(diag_landmark, inv_depth);
-  CHECK(assert_equal(expected, actual));
-}
-
-
-/* ************************************************************************* */
-Point2 project_(const Pose3& pose, const LieVector& landmark, const LieScalar& inv_depth) {
-  return InvDepthCamera3<Cal3_S2>(pose,K).project(landmark, inv_depth); }
-
-TEST( InvDepthFactor, Dproject_pose)
-{
-  LieVector landmark(6,0.1,0.2,0.3, 0.1,0.2);
-  LieScalar inv_depth(1./4);
-  Matrix expected = numericalDerivative31<Point2,Pose3,LieVector>(project_,level_pose, landmark, inv_depth);
-  InvDepthCamera3<Cal3_S2> inv_camera(level_pose,K);
-  Matrix actual;
-  Point2 uv = inv_camera.project(landmark, inv_depth, actual, boost::none, boost::none);
-  CHECK(assert_equal(expected,actual,1e-6));
-}
-
-/* ************************************************************************* */
-TEST( InvDepthFactor, Dproject_landmark)
-{
-  LieVector landmark(5,0.1,0.2,0.3, 0.1,0.2);
-  LieScalar inv_depth(1./4);
-  Matrix expected = numericalDerivative32<Point2,Pose3,LieVector>(project_,level_pose, landmark, inv_depth);
-  InvDepthCamera3<Cal3_S2> inv_camera(level_pose,K);
-  Matrix actual;
-  Point2 uv = inv_camera.project(landmark, inv_depth, boost::none, actual, boost::none);
-  CHECK(assert_equal(expected,actual,1e-7));
-}
-
-/* ************************************************************************* */
-TEST( InvDepthFactor, Dproject_inv_depth)
-{
-  LieVector landmark(5,0.1,0.2,0.3, 0.1,0.2);
-  LieScalar inv_depth(1./4);
-  Matrix expected = numericalDerivative33<Point2,Pose3,LieVector>(project_,level_pose, landmark, inv_depth);
-  InvDepthCamera3<Cal3_S2> inv_camera(level_pose,K);
-  Matrix actual;
-  Point2 uv = inv_camera.project(landmark, inv_depth, boost::none, boost::none, actual);
-  CHECK(assert_equal(expected,actual,1e-7));
-}
-
-/* ************************************************************************* */
-TEST(InvDepthFactor, backproject)
-{
-  LieVector expected(5,0.,0.,1., 0.1,0.2);
-  LieScalar inv_depth(1./4);
-  InvDepthCamera3<Cal3_S2> inv_camera(level_pose,K);
-  Point2 z = inv_camera.project(expected, inv_depth);
-
-  LieVector actual_vec;
-  LieScalar actual_inv;
-  boost::tie(actual_vec, actual_inv) = inv_camera.backproject(z, 4);
-  CHECK(assert_equal(expected,actual_vec,1e-7));
-  CHECK(assert_equal(inv_depth,actual_inv,1e-7));
-}
-
-/* ************************************************************************* */
-TEST(InvDepthFactor, backproject2)
-{
-  // backwards facing camera
-  LieVector expected(5,-5.,-5.,2., 3., -0.1);
-  LieScalar inv_depth(1./10);
-  InvDepthCamera3<Cal3_S2> inv_camera(Pose3(Rot3::ypr(1.5,0.1, -1.5), Point3(-5, -5, 2)),K);
-  Point2 z = inv_camera.project(expected, inv_depth);
-
-  LieVector actual_vec;
-  LieScalar actual_inv;
-  boost::tie(actual_vec, actual_inv) = inv_camera.backproject(z, 10);
-  CHECK(assert_equal(expected,actual_vec,1e-7));
-  CHECK(assert_equal(inv_depth,actual_inv,1e-7));
-}
-
-static SharedNoiseModel sigma(noiseModel::Unit::Create(2));
 typedef InvDepthFactor3<Pose3, LieVector, LieScalar> InverseDepthFactor;
 typedef NonlinearEquality<Pose3> PoseConstraint;
 
@@ -183,7 +52,7 @@ TEST( InvDepthFactor, optimize) {
 
   LevenbergMarquardtParams lmParams;
   Values result = LevenbergMarquardtOptimizer(graph, initial, lmParams).optimize();
-  CHECK(assert_equal(initial, result, 1e-9));
+  EXPECT(assert_equal(initial, result, 1e-9));
 
   /// Add a second camera
 
@@ -214,7 +83,7 @@ TEST( InvDepthFactor, optimize) {
       result2.at<LieVector>(Symbol('l',1)),
       result2.at<LieScalar>(Symbol('d',1)));
 
-  CHECK(assert_equal(landmark1, l1_result2, 1e-9));
+  EXPECT(assert_equal(landmark1, l1_result2, 1e-9));
 }
 
 
