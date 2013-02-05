87f2755ec6b654850ecf8587535ed171bc64815c
Richard Roberts <richard.jw.roberts@gmail.com>
2013-02-05 21:52:49 +0000

    Fixed and reenabled broken timing scripts

--- gtsam/base/tests/timeTest.cpp
+++ gtsam/base/tests/timeTest.cpp
@@ -17,44 +17,56 @@
 
 #include <gtsam/base/timing.h>
 
+using namespace gtsam;
+
 int main(int argc, char *argv[]) {
 
   // FIXME: ticPush_ does not exist
-  ticPush_("1", "top 1");
-  ticPush_("1", "sub 1");
-  gttic_("sub sub a");
-  gttoc_("sub sub a");
-  ticPop_("1", "sub 1");
-  ticPush_("2", "sub 2");
-  gttic_("sub sub b");
-  gttoc_("sub sub b");
-  ticPop_("2", "sub 2");
-  ticPop_("1", "top 1");
-
-  ticPush_("2", "top 2");
-  ticPush_("1", "sub 1");
-  gttic_("sub sub a");
-  gttoc_("sub sub a");
-  ticPop_("1", "sub 1");
-  ticPush_("2", "sub 2");
-  gttic_("sub sub b");
-  gttoc_("sub sub b");
-  ticPop_("2", "sub 2");
-  ticPop_("2", "top 2");
-
-  for(size_t i=0; i<1000000; ++i) {
-    ticPush_("3", "overhead");
-    ticPush_("1", "overhead");
-    ticPop_("1", "overhead");
-    ticPop_("3", "overhead");
+  {
+    gttic_(top1);
+    gttic_(sub1);
+    gttic_(sub_sub_a);
+    gttoc_(sub_sub_a);
+    gttoc_(sub1);
+    gttic_(sub2);
+    gttic_(sub_sub_b);
+    gttoc_(sub_sub_b);
+    gttoc_(sub2);
+    gttoc_(top1);
   }
 
+  {
+    gttic_(top2);
+    gttic_(sub1);
+    gttic_(sub_sub_a);
+    gttoc_(sub_sub_a);
+    gttoc_(sub1);
+    gttic_(sub2);
+    gttic_(sub_sub_b);
+    gttoc_(sub_sub_b);
+    gttoc_(sub2);
+    gttoc_(top2);
+  }
+
+  gttic_(top3);
+  for(size_t i=0; i<1000000; ++i) {
+    gttic_(overhead);
+    gttic_(sub_overhead);
+    gttoc_(sub_overhead);
+    gttoc_(overhead);
+    tictoc_finishedIteration_();
+  }
+  gttoc_(top3);
+
+  gttic_(top4);
   for(size_t i=0; i<1000000; ++i) {
     gttic(overhead_a);
     gttic(overhead_b);
     gttoc(overhead_b);
     gttoc(overhead_a);
+    tictoc_finishedIteration();
   }
+  gttoc_(top4);
 
   tictoc_print_();
 
