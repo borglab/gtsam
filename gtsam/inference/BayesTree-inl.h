88496b33ab0a60906013ca998841e639716ac408
Alex Cunningham <alexgcunningham@gmail.com>
2013-03-05 15:11:43 +0000

    Fixed compile error due to missing typename

--- gtsam/inference/BayesTree-inl.h
+++ gtsam/inference/BayesTree-inl.h
@@ -255,7 +255,7 @@ namespace gtsam {
       root_.reset();
     else { // detach clique from parent
       sharedClique parent = clique->parent_.lock();
-      std::list<typename CLIQUE::shared_ptr>::iterator child = std::find(parent->children().begin(), parent->children().end(), clique);
+      typename std::list<typename CLIQUE::shared_ptr>::iterator child = std::find(parent->children().begin(), parent->children().end(), clique);
       assert(child != parent->children().end());
       parent->children().erase(child);
     }
