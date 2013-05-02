bc5c1c8bdd95dfeb526b7273f131dff44f7059d1
Luca Carlone <lucacar85@gmail.com>
2013-05-02 22:11:02 +0000

    First implementation of the IMU factors (MODEL1: no biases and no Coriolis effect)

---
+++ gtsam_unstable/slam/ImuFactor.h
@@ -0,0 +1,183 @@
+/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation, 
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ *  @file  ImuFactor.h
+ *  @author Luca Carlone, Stephen Williams
+ **/
+
+#pragma once
+
+/* GTSAM includes */
+#include <gtsam/nonlinear/NonlinearFactor.h>
+#include <gtsam/linear/GaussianFactor.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/base/LieVector.h>
+
+/* External or standard includes */
+#include <ostream>
+
+
+namespace gtsam {
+
+  /**
+   * A class for a measurement predicted by "between(config[key1],config[key2])"
+   * @addtogroup SLAM
+   */
+  class ImuFactor: public NoiseModelFactor4<Pose3,LieVector,Pose3,LieVector> {
+
+  public:
+
+    /** Struct to store results of preintegrating IMU measurements.  Can be build
+     * incrementally so as to avoid costly integration at time of factor
+     * construction. */
+    class PreintegratedMeasurements {
+    public:
+      Vector3 deltaPij; ///< Preintegrated relative position (in frame i)
+      Vector3 deltaVij; ///< Preintegrated relative velocity (in global frame)
+      Rot3 deltaRij; ///< Preintegrated relative orientation (in frame i)
+      double deltaTij; ///< Time interval from i to j
+
+      /** Default constructor, initialize with no IMU measurements */
+      PreintegratedMeasurements() : deltaPij(Vector3::Zero()), deltaVij(Vector3::Zero()), deltaTij(0.0) {}
+
+      /** Construct preintegrated measurements from a single IMU measurement. */
+      PreintegratedMeasurements(
+          const Vector3& measuredAcc, ///< Measured linear acceleration (in body frame)
+          const Vector3& measuredOmega, ///< Measured angular velocity (in body frame)
+          double deltaT ///< Time step
+      ) : deltaPij(Vector3::Zero()), deltaVij(Vector3::Zero()), deltaTij(0.0)
+      { integrateMeasurement(measuredAcc, measuredOmega, deltaT); }
+
+      /** Add a single IMU measurement to the preintegration. */
+      void integrateMeasurement(
+          const Vector3& measuredAcc, ///< Measured linear acceleration (in body frame)
+          const Vector3& measuredOmega, ///< Measured angular velocity (in body frame)
+          double deltaT ///< Time step
+      ) {
+        // NOTE: order is important here because each update uses old values.
+        deltaPij += deltaVij * deltaT + 0.5 * deltaRij.matrix() * measuredAcc * deltaT*deltaT;
+        deltaVij += deltaRij.matrix() * measuredAcc * deltaT;
+        deltaRij = deltaRij * Rot3::Expmap(measuredOmega * deltaT);
+        deltaTij += deltaT;
+      }
+    };
+
+  private:
+
+    typedef ImuFactor This;
+    typedef NoiseModelFactor4<Pose3,LieVector,Pose3,LieVector> Base;
+
+    PreintegratedMeasurements preintegratedMeasurements_;
+    Vector3 gravity_;
+
+  public:
+
+    /** Shorthand for a smart pointer to a factor */
+    typedef typename boost::shared_ptr<ImuFactor> shared_ptr;
+
+    /** Default constructor - only use for serialization */
+    ImuFactor() {}
+
+    /** Constructor */
+    ImuFactor(Key pose_i, Key vel_i, Key pose_j, Key vel_j,
+        const PreintegratedMeasurements& preintegratedMeasurements, const Vector3& gravity,
+        const SharedNoiseModel& model) :
+      Base(model, pose_i, vel_i, pose_j, vel_j),
+      preintegratedMeasurements_(preintegratedMeasurements),
+      gravity_(gravity) {
+    }
+
+    virtual ~ImuFactor() {}
+
+    /// @return a deep copy of this factor
+    virtual gtsam::NonlinearFactor::shared_ptr clone() const {
+      return boost::static_pointer_cast<gtsam::NonlinearFactor>(
+          gtsam::NonlinearFactor::shared_ptr(new This(*this))); }
+
+    /** implement functions needed for Testable */
+
+    /** print */
+    virtual void print(const std::string& s, const KeyFormatter& keyFormatter = DefaultKeyFormatter) const {
+      std::cout << s << "ImuFactor("
+          << keyFormatter(this->key1()) << ","
+          << keyFormatter(this->key2()) << ","
+          << keyFormatter(this->key3()) << ","
+          << keyFormatter(this->key4()) << ")\n";
+      this->noiseModel_->print("  noise model: ");
+    }
+
+    /** equals */
+    virtual bool equals(const NonlinearFactor& expected, double tol=1e-9) const {
+      const This *e =  dynamic_cast<const This*> (&expected);
+      return e != NULL && Base::equals(*e, tol);
+    }
+
+    /** implement functions needed to derive from Factor */
+
+    /** vector of errors */
+    Vector evaluateError(const Pose3& pose_i, const LieVector& vel_i, const Pose3& pose_j, const LieVector& vel_j,
+        boost::optional<Matrix&> H1 = boost::none,
+        boost::optional<Matrix&> H2 = boost::none,
+        boost::optional<Matrix&> H3 = boost::none,
+        boost::optional<Matrix&> H4 = boost::none) const {
+
+      if(H1) {
+        H1->resize(9,6);
+        (*H1) << pose_i.rotation().matrix() * skewSymmetric(preintegratedMeasurements_.deltaPij), - pose_i.rotation().matrix(),
+            pose_i.rotation().matrix() * skewSymmetric(preintegratedMeasurements_.deltaVij), Matrix3::Zero(),
+            -pose_j.rotation().between(pose_i.rotation()).matrix() , Matrix3::Zero();
+      }
+      if(H2) {
+        H2->resize(9,3);
+        (*H2) <<  - Matrix3::Identity() * preintegratedMeasurements_.deltaTij,
+            - Matrix3::Identity(),
+            Matrix3::Zero();
+
+      }
+      if(H3) {
+        H3->resize(9,6);
+        (*H3) << Matrix3::Zero(), pose_j.rotation().matrix(),
+            Matrix::Zero(3,6),
+            Matrix3::Identity() , Matrix3::Zero();
+      }
+      if(H4) {
+        H4->resize(9,3);
+        (*H4) << Matrix3::Zero(),
+            Matrix3::Identity(),
+            Matrix3::Zero();
+      }
+
+      const Vector3 fp = pose_j.translation().vector() - pose_i.translation().vector() - pose_i.rotation().matrix() * preintegratedMeasurements_.deltaPij -
+                    vel_i * preintegratedMeasurements_.deltaTij - 0.5 * gravity_ * preintegratedMeasurements_.deltaTij*preintegratedMeasurements_.deltaTij;
+
+      const Vector3 fv = vel_j- vel_i - pose_i.rotation().matrix() * preintegratedMeasurements_.deltaVij
+          - gravity_ * preintegratedMeasurements_.deltaTij;
+
+      const Vector3 fR = preintegratedMeasurements_.deltaRij.localCoordinates(pose_i.rotation().between(pose_j.rotation()) );
+
+      Vector r(9); r << fp, fv, fR;
+
+      return r;
+    }
+
+  private:
+
+    /** Serialization function */
+    friend class boost::serialization::access;
+    template<class ARCHIVE>
+    void serialize(ARCHIVE & ar, const unsigned int version) {
+      ar & boost::serialization::make_nvp("NoiseModelFactor4",
+          boost::serialization::base_object<Base>(*this));
+    }
+  }; // \class ImuFactor
+
+} /// namespace gtsam
---
+++ gtsam_unstable/slam/tests/testImuFactor.cpp
@@ -0,0 +1,280 @@
+/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation,
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file    testImuFactor.cpp
+ * @brief   Unit test for StereoFactor
+ * @author  Chris Beall
+ */
+
+#include <gtsam_unstable/slam/ImuFactor.h>
+//#include <gtsam/nonlinear/NonlinearEquality.h>
+//#include <gtsam/nonlinear/GaussNewtonOptimizer.h>
+//#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+//#include <gtsam/nonlinear/Values.h>
+#include <gtsam/nonlinear/Symbol.h>
+//#include <gtsam/geometry/StereoCamera.h>
+//#include <gtsam/geometry/Cal3_S2Stereo.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/base/LieVector.h>
+#include <gtsam/base/TestableAssertions.h>
+#include <gtsam/base/numericalDerivative.h>
+#include <CppUnitLite/TestHarness.h>
+
+#include <boost/bind.hpp>
+
+using namespace std;
+using namespace gtsam;
+
+// Convenience for named keys
+using symbol_shorthand::X;
+using symbol_shorthand::V;
+
+/* ************************************************************************* */
+TEST( ImuFactor, Constructor) {
+  Vector3 gravity; gravity << 0, 0, 9.81;
+  noiseModel::Diagonal::shared_ptr model = noiseModel::Diagonal::Sigmas(Vector_(9, 0.15, 0.15, 0.15, 1.5, 1.5, 1.5, 0.5, 0.5, 0.5));
+  ImuFactor factor(X(1), V(1), X(2), V(2), ImuFactor::PreintegratedMeasurements(),gravity, model);
+}
+
+///* ************************************************************************* */
+//TEST( ImuFactor, ConstructorWithTransform) {
+//  StereoPoint2 measurement(323, 318-50, 241);
+//  Pose3 body_P_sensor(Rot3::RzRyRx(-M_PI_2, 0.0, -M_PI_2), Point3(0.25, -0.10, 1.0));
+//
+//  TestStereoFactor factor(measurement, model, X(1), L(1), K, body_P_sensor);
+//}
+//
+///* ************************************************************************* */
+//TEST( StereoFactor, Equals ) {
+//  // Create two identical factors and make sure they're equal
+//  StereoPoint2 measurement(323, 318-50, 241);
+//
+//  TestStereoFactor factor1(measurement, model, X(1), L(1), K);
+//  TestStereoFactor factor2(measurement, model, X(1), L(1), K);
+//
+//  CHECK(assert_equal(factor1, factor2));
+//}
+//
+///* ************************************************************************* */
+//TEST( StereoFactor, EqualsWithTransform ) {
+//  // Create two identical factors and make sure they're equal
+//  StereoPoint2 measurement(323, 318-50, 241);
+//  Pose3 body_P_sensor(Rot3::RzRyRx(-M_PI_2, 0.0, -M_PI_2), Point3(0.25, -0.10, 1.0));
+//
+//  TestStereoFactor factor1(measurement, model, X(1), L(1), K, body_P_sensor);
+//  TestStereoFactor factor2(measurement, model, X(1), L(1), K, body_P_sensor);
+//
+//  CHECK(assert_equal(factor1, factor2));
+//}
+
+/* ************************************************************************* */
+TEST( ImuFactor, PreintegratedMeasurements )
+{
+  // Measurements
+  Vector3 measuredAcc(0.1, 0.0, 0.0);
+  Vector3 measuredOmega(M_PI/100.0, 0.0, 0.0);
+  double deltaT = 0.5;
+
+  // Expected preintegrated values
+  Vector3 expectedDeltaP1; expectedDeltaP1 << 0.5*0.1*0.5*0.5, 0, 0;
+  Vector3 expectedDeltaV1(0.05, 0.0, 0.0);
+  Rot3 expectedDeltaR1 = Rot3::RzRyRx(0.5 * M_PI/100.0, 0.0, 0.0);
+  double expectedDeltaT1(0.5);
+
+  // Actual preintegrated values
+  ImuFactor::PreintegratedMeasurements actual1(measuredAcc, measuredOmega, deltaT);
+
+  EXPECT(assert_equal(Vector(expectedDeltaP1), Vector(actual1.deltaPij), 1e-6));
+  EXPECT(assert_equal(Vector(expectedDeltaV1), Vector(actual1.deltaVij), 1e-6));
+  EXPECT(assert_equal(expectedDeltaR1, actual1.deltaRij, 1e-6));
+  DOUBLES_EQUAL(expectedDeltaT1, actual1.deltaTij, 1e-6);
+
+  // Integrate again
+  Vector3 expectedDeltaP2; expectedDeltaP2 << 0.025 + expectedDeltaP1(0) + 0.5*0.1*0.5*0.5, 0, 0;
+  Vector3 expectedDeltaV2 = Vector3(0.05, 0.0, 0.0) + expectedDeltaR1.matrix() * measuredAcc * 0.5;
+  Rot3 expectedDeltaR2 = Rot3::RzRyRx(2.0 * 0.5 * M_PI/100.0, 0.0, 0.0);
+  double expectedDeltaT2(1);
+
+  // Actual preintegrated values
+  ImuFactor::PreintegratedMeasurements actual2 = actual1;
+  actual2.integrateMeasurement(measuredAcc, measuredOmega, deltaT);
+
+  EXPECT(assert_equal(Vector(expectedDeltaP2), Vector(actual2.deltaPij), 1e-6));
+  EXPECT(assert_equal(Vector(expectedDeltaV2), Vector(actual2.deltaVij), 1e-6));
+  EXPECT(assert_equal(expectedDeltaR2, actual2.deltaRij, 1e-6));
+  DOUBLES_EQUAL(expectedDeltaT2, actual2.deltaTij, 1e-6);
+}
+
+/* ************************************************************************* */
+TEST( ImuFactor, Error )
+{
+  // Linearization point
+  Values values;
+  Pose3 x1(Rot3::RzRyRx(M_PI/12.0, M_PI/6.0, M_PI/4.0), Point3(5.0, 1.0, -50.0));
+  LieVector v1(3, 0.5, 0.0, 0.0);
+  Pose3 x2(Rot3::RzRyRx(M_PI/12.0 + M_PI/100.0, M_PI/6.0, M_PI/4.0), Point3(5.5, 1.0, -50.0));
+  LieVector v2(3, 0.5, 0.0, 0.0);
+
+  // Measurements
+  Vector3 gravity; gravity << 0, 0, 9.81;
+  Vector3 measuredOmega; measuredOmega << M_PI/100, 0, 0;
+  Vector3 measuredAcc = x1.rotation().unrotate(-Point3(gravity)).vector();
+  double deltaT = 1.0;
+  ImuFactor::PreintegratedMeasurements pre_int_data;
+  pre_int_data.integrateMeasurement(measuredAcc, measuredOmega, deltaT);
+
+  // Create factor
+  noiseModel::Diagonal::shared_ptr model = noiseModel::Diagonal::Sigmas(Vector_(9, 0.15, 0.15, 0.15, 1.5, 1.5, 1.5, 0.5, 0.5, 0.5));
+  ImuFactor factor(X(1), V(1), X(2), V(2), pre_int_data, gravity, model);
+
+  Vector errorActual = factor.evaluateError(x1, v1, x2, v2);
+
+  // Expected error
+  Vector errorExpected(9); errorExpected << 0, 0, 0, 0, 0, 0, 0, 0, 0;
+  EXPECT(assert_equal(errorExpected, errorActual, 1e-6));
+
+  // Expected Jacobians
+  Matrix H1e = numericalDerivative11<Pose3>(
+      boost::bind(&ImuFactor::evaluateError, &factor, _1, v1, x2, v2,
+          boost::none, boost::none, boost::none, boost::none), x1);
+  Matrix H2e = numericalDerivative11<LieVector>(
+      boost::bind(&ImuFactor::evaluateError, &factor, x1, _1, x2, v2,
+          boost::none, boost::none, boost::none, boost::none), v1);
+  Matrix H3e = numericalDerivative11<Pose3>(
+      boost::bind(&ImuFactor::evaluateError, &factor, x1, v1, _1, v2,
+          boost::none, boost::none, boost::none, boost::none), x2);
+  Matrix H4e = numericalDerivative11<LieVector>(
+      boost::bind(&ImuFactor::evaluateError, &factor, x1, v1, x2, _1,
+          boost::none, boost::none, boost::none, boost::none), v2);
+
+  // Actual Jacobians
+  Matrix H1a, H2a, H3a, H4a;
+  (void) factor.evaluateError(x1, v1, x2, v2, H1a, H2a, H3a, H4a);
+
+  EXPECT(assert_equal(H1e, H1a));
+  EXPECT(assert_equal(H2e, H2a));
+  EXPECT(assert_equal(H3e, H3a));
+  EXPECT(assert_equal(H4e, H4a));
+}
+
+///* ************************************************************************* */
+//TEST( StereoFactor, ErrorWithTransform ) {
+//  // Create the factor with a measurement that is 3 pixels off in x
+//  StereoPoint2 measurement(323, 318-50, 241);
+//  Pose3 body_P_sensor(Rot3::RzRyRx(-M_PI_2, 0.0, -M_PI_2), Point3(0.25, -0.10, 1.0));
+//  TestStereoFactor factor(measurement, model, X(1), L(1), K, body_P_sensor);
+//
+//  // Set the linearization point. The vehicle pose has been selected to put the camera at (-6, 0, 0)
+//  Pose3 pose(Rot3(), Point3(-6.50, 0.10 , -1.0));
+//  Point3 point(0.0, 0.0, 0.0);
+//
+//  // Use the factor to calculate the error
+//  Vector actualError(factor.evaluateError(pose, point));
+//
+//  // The expected error is (-3.0, +2.0, -1.0) pixels / UnitCovariance
+//  Vector expectedError = Vector_(3, -3.0, +2.0, -1.0);
+//
+//  // Verify we get the expected error
+//  CHECK(assert_equal(expectedError, actualError, 1e-9));
+//}
+//
+///* ************************************************************************* */
+//TEST( StereoFactor, Jacobian ) {
+//  // Create the factor with a measurement that is 3 pixels off in x
+//  StereoPoint2 measurement(323, 318-50, 241);
+//  TestStereoFactor factor(measurement, model, X(1), L(1), K);
+//
+//  // Set the linearization point
+//  Pose3 pose(Rot3(), Point3(0.0, 0.0, -6.25));
+//  Point3 point(0.0, 0.0, 0.0);
+//
+//  // Use the factor to calculate the Jacobians
+//  Matrix H1Actual, H2Actual;
+//  factor.evaluateError(pose, point, H1Actual, H2Actual);
+//
+//  // The expected Jacobians
+//  Matrix H1Expected = Matrix_(3, 6, 0.0,  -625.0, 0.0, -100.0,    0.0,  0.0,
+//                                    0.0,  -625.0, 0.0, -100.0,    0.0, -8.0,
+//                                    625.0,   0.0, 0.0,    0.0, -100.0,  0.0);
+//  Matrix H2Expected = Matrix_(3, 3, 100.0,   0.0, 0.0,
+//                                    100.0,   0.0, 8.0,
+//                                    0.0,   100.0, 0.0);
+//
+//  // Verify the Jacobians are correct
+//  CHECK(assert_equal(H1Expected, H1Actual, 1e-3));
+//  CHECK(assert_equal(H2Expected, H2Actual, 1e-3));
+//}
+//
+///* ************************************************************************* */
+//TEST( StereoFactor, JacobianWithTransform ) {
+//  // Create the factor with a measurement that is 3 pixels off in x
+//  StereoPoint2 measurement(323, 318-50, 241);
+//  Pose3 body_P_sensor(Rot3::RzRyRx(-M_PI_2, 0.0, -M_PI_2), Point3(0.25, -0.10, 1.0));
+//  TestStereoFactor factor(measurement, model, X(1), L(1), K, body_P_sensor);
+//
+//  // Set the linearization point. The vehicle pose has been selected to put the camera at (-6, 0, 0)
+//  Pose3 pose(Rot3(), Point3(-6.50, 0.10 , -1.0));
+//  Point3 point(0.0, 0.0, 0.0);
+//
+//  // Use the factor to calculate the Jacobians
+//  Matrix H1Actual, H2Actual;
+//  factor.evaluateError(pose, point, H1Actual, H2Actual);
+//
+//  // The expected Jacobians
+//  Matrix H1Expected = Matrix_(3, 6, -100.0,    0.0,  650.0,   0.0,  100.0,    0.0,
+//                                    -100.0,   -8.0,  649.2,  -8.0,  100.0,    0.0,
+//                                     -10.0, -650.0,    0.0,   0.0,    0.0,  100.0);
+//  Matrix H2Expected = Matrix_(3, 3,    0.0, -100.0,    0.0,
+//                                       8.0, -100.0,    0.0,
+//                                       0.0,    0.0, -100.0);
+//
+//  // Verify the Jacobians are correct
+//  CHECK(assert_equal(H1Expected, H1Actual, 1e-3));
+//  CHECK(assert_equal(H2Expected, H2Actual, 1e-3));
+//}
+//
+///* ************************************************************************* */
+//TEST( StereoFactor, singlePoint)
+//{
+//  NonlinearFactorGraph graph;
+//
+//  graph.add(NonlinearEquality<Pose3>(X(1), camera1));
+//
+//  StereoPoint2 measurement(320, 320.0-50, 240);
+//  // arguments: measurement, sigma, cam#, measurement #, K, baseline (m)
+//  graph.add(GenericStereoFactor<Pose3, Point3>(measurement, model, X(1), L(1), K));
+//
+//  // Create a configuration corresponding to the ground truth
+//  Values values;
+//  values.insert(X(1), camera1); // add camera at z=6.25m looking towards origin
+//
+//  Point3 l1(0, 0, 0);
+//  values.insert(L(1), l1);   // add point at origin;
+//
+//  GaussNewtonOptimizer optimizer(graph, values);
+//
+//  // We expect the initial to be zero because config is the ground truth
+//  DOUBLES_EQUAL(0.0, optimizer.error(), 1e-9);
+//
+//  // Iterate once, and the config should not have changed
+//  optimizer.iterate();
+//  DOUBLES_EQUAL(0.0, optimizer.error(), 1e-9);
+//
+//  // Complete solution
+//  optimizer.optimize();
+//
+//  DOUBLES_EQUAL(0.0, optimizer.error(), 1e-6);
+//}
+
+/* ************************************************************************* */
+  int main() { TestResult tr; return TestRegistry::runAllTests(tr);}
+/* ************************************************************************* */
