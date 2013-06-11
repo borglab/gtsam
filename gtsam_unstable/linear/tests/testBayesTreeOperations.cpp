361682c485fb2a45556def95528447d9bbdf0a82
Alex Cunningham <alexgcunningham@gmail.com>
2013-06-11 14:36:51 +0000

    Working on a test for liquefy

--- gtsam_unstable/linear/tests/testBayesTreeOperations.cpp
+++ gtsam_unstable/linear/tests/testBayesTreeOperations.cpp
@@ -10,6 +10,9 @@
 #include <CppUnitLite/TestHarness.h>
 
 #include <gtsam_unstable/linear/bayesTreeOperations.h>
+#include <gtsam/linear/GaussianMultifrontalSolver.h>
+
+#include <tests/smallExample.h>
 
 using namespace gtsam;
 
@@ -17,8 +20,15 @@ SharedDiagonal model2 = noiseModel::Diagonal::Sigmas(ones(2));
 SharedDiagonal model4 = noiseModel::Diagonal::Sigmas(ones(4));
 SharedDiagonal model6 = noiseModel::Diagonal::Sigmas(ones(6));
 
+using namespace std;
+
+using symbol_shorthand::X;
+using symbol_shorthand::L;
+
+static const double tol = 1e-4;
+
 /* ************************************************************************* */
-TEST( testLinearTools, splitFactor ) {
+TEST( testBayesTreeOperations, splitFactor ) {
 
   // Build upper-triangular system
   JacobianFactor initFactor(
@@ -118,6 +128,107 @@ TEST_UNSAFE( testLinearTools, splitFactor2 ) {
        model2);
 
   EXPECT(assert_equal(expSplit, actSplit));
+}
+
+/* ************************************************************************* */
+// Some numbers that should be consistent among all smoother tests
+
+//static double sigmax1 = 0.786153, /*sigmax2 = 1.0/1.47292,*/ sigmax3 = 0.671512, sigmax4 =
+//    0.669534 /*, sigmax5 = sigmax3, sigmax6 = sigmax2*/, sigmax7 = sigmax1;
+
+/* ************************************************************************* */
+TEST( testBayesTreeOperations, liquefy ) {
+  using namespace example;
+
+  // Create smoother with 7 nodes
+  Ordering ordering;
+  ordering += X(1),X(3),X(5),X(7),X(2),X(6),X(4);
+  GaussianFactorGraph smoother = createSmoother(7, ordering).first;
+
+  // Create the Bayes tree
+  GaussianBayesTree bayesTree = *GaussianMultifrontalSolver(smoother).eliminate();
+//  bayesTree.print("Full tree");
+
+  SharedDiagonal unit6 = noiseModel::Diagonal::Sigmas(Vector_(ones(6)));
+  SharedDiagonal unit4 = noiseModel::Diagonal::Sigmas(Vector_(ones(4)));
+  SharedDiagonal unit2 = noiseModel::Diagonal::Sigmas(Vector_(ones(2)));
+
+  // Liquefy the tree back into a graph
+  {
+    GaussianFactorGraph actGraph = liquefy(bayesTree, false); // Doesn't split conditionals
+    GaussianFactorGraph expGraph;
+
+    Matrix A12 = Matrix_(6, 2,
+        1.73205081,          0.0,
+               0.0,   1.73205081,
+               0.0,          0.0,
+               0.0,          0.0,
+               0.0,          0.0,
+               0.0,          0.0);
+
+    Matrix A15 = Matrix_(6, 2,
+       -0.577350269,          0.0,
+                0.0, -0.577350269,
+         1.47196014,          0.0,
+                0.0,   1.47196014,
+                0.0,          0.0,
+                0.0,          0.0);
+
+    Matrix A16 = Matrix_(6, 2,
+      -0.577350269,          0.0,
+               0.0, -0.577350269,
+      -0.226455407,          0.0,
+               0.0, -0.226455407,
+        1.49357599,          0.0,
+               0.0,   1.49357599);
+    expGraph.add(2, A12, 5, A15, 6, A16, zeros(6,1), unit6);
+
+    Matrix A21 = Matrix_(4, 2,
+        1.73205081,          0.0,
+               0.0,   1.73205081,
+               0.0,          0.0,
+               0.0,          0.0);
+
+    Matrix A24 = Matrix_(4, 2,
+      -0.577350269,          0.0,
+               0.0, -0.577350269,
+        1.47196014,          0.0,
+               0.0,   1.47196014);
+
+    Matrix A26 = Matrix_(4, 2,
+      -0.577350269,          0.0,
+               0.0, -0.577350269,
+      -0.226455407,          0.0,
+               0.0, -0.226455407);
+
+    expGraph.add(1, A21, 4, A24, 6, A26, zeros(4,1), unit4);
+
+    Matrix A30 = Matrix_(2, 2,
+        1.41421356,          0.0,
+               0.0,   1.41421356);
+
+    Matrix A34 = Matrix_(2, 2,
+      -0.707106781,          0.0,
+               0.0, -0.707106781);
+
+    expGraph.add(0, A30, 4, A34, zeros(2,1), unit2);
+
+    Matrix A43 = Matrix_(2, 2,
+        1.41421356,          0.0,
+               0.0,   1.41421356);
+    Matrix A45 = Matrix_(2, 2,
+      -0.707106781,          0.0,
+               0.0, -0.707106781);
+
+    expGraph.add(3, A43, 5, A45, zeros(2,1), unit2);
+
+    EXPECT(assert_equal(expGraph, actGraph));
+    LONGS_EQUAL(4, actGraph.size());
+    EXPECT(assert_equal(*expGraph.at(0), *actGraph.at(0)));
+    EXPECT(assert_equal(*expGraph.at(1), *actGraph.at(1)));
+    EXPECT(assert_equal(*expGraph.at(2), *actGraph.at(2)));
+    EXPECT(assert_equal(*expGraph.at(3), *actGraph.at(3)));
+  }
 }
 
 /* ************************************************************************* */
