cfd81093bd754da3faf12f4ef34d89f2d990c2aa
Alex Cunningham <alexgcunningham@gmail.com>
2013-08-08 17:59:19 +0000

    Fixing compile/interface issues, making (failing) testBetweenFactor more verbose

--- gtsam/slam/tests/testBetweenFactor.cpp
+++ gtsam/slam/tests/testBetweenFactor.cpp
@@ -15,6 +15,7 @@ using namespace gtsam;
 using namespace gtsam::symbol_shorthand;
 using namespace gtsam::noiseModel;
 
+/* ************************************************************************* */
 TEST(BetweenFactor, Rot3) {
   Rot3 R1 = Rot3::rodriguez(0.1, 0.2, 0.3);
   Rot3 R2 = Rot3::rodriguez(0.4, 0.5, 0.6);
@@ -26,19 +27,19 @@ TEST(BetweenFactor, Rot3) {
   Vector actual = factor.evaluateError(R1, R2, actualH1, actualH2);
 
   Vector expected = Rot3::Logmap(measured.inverse() * R1.between(R2));
-  CHECK(assert_equal(expected,actual, 1e-100));
+  EXPECT(assert_equal(expected,actual, 1e-100));
 
   Matrix numericalH1 = numericalDerivative21(
       boost::function<Vector(const Rot3&, const Rot3&)>(boost::bind(
           &BetweenFactor<Rot3>::evaluateError, factor, _1, _2, boost::none,
           boost::none)), R1, R2, 1e-5);
-  CHECK(assert_equal(numericalH1,actualH1));
+  EXPECT(assert_equal(numericalH1,actualH1, 1e-5));
 
   Matrix numericalH2 = numericalDerivative22(
       boost::function<Vector(const Rot3&, const Rot3&)>(boost::bind(
           &BetweenFactor<Rot3>::evaluateError, factor, _1, _2, boost::none,
           boost::none)), R1, R2, 1e-5);
-  CHECK(assert_equal(numericalH2,actualH2));
+  EXPECT(assert_equal(numericalH2,actualH2, 1e-5));
 }
 
 /* ************************************************************************* */
