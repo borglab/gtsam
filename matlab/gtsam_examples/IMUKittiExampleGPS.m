a518dae06aa51ca56f6333a82d9feb4b3665e45a
Luca Carlone <lucacar85@gmail.com>
2013-08-12 20:45:44 +0000

    Fixed GPS Kitti example, VO works but bad results

--- matlab/gtsam_examples/IMUKittiExampleGPS.m
+++ matlab/gtsam_examples/IMUKittiExampleGPS.m
@@ -2,7 +2,7 @@ close all
 clc
 
 import gtsam.*;
-disp('Example of application of ISAM2 for visual-inertial navigation on the KITTI VISION BENCHMARK SUITE (http://www.computervisiononline.com/dataset/kitti-vision-benchmark-suite)')
+disp('Example of application of ISAM2 for GPS-aided navigation on the KITTI VISION BENCHMARK SUITE (http://www.computervisiononline.com/dataset/kitti-vision-benchmark-suite)')
 
 %% Read metadata and compute relative sensor pose transforms
 % IMU metadata
@@ -15,14 +15,11 @@ if ~IMUinBody.equals(Pose3, 1e-5)
   error 'Currently only support IMUinBody is identity, i.e. IMU and body frame are the same';
 end
 
-% VO metadata
-VO_metadata = importdata('KittiRelativePose_metadata.txt');
-VO_metadata = cell2struct(num2cell(VO_metadata.data), VO_metadata.colheaders, 2);
-VOinBody = Pose3.Expmap([VO_metadata.BodyPtx; VO_metadata.BodyPty; VO_metadata.BodyPtz;
-  VO_metadata.BodyPrx; VO_metadata.BodyPry; VO_metadata.BodyPrz; ]);
-VOinIMU = IMUinBody.inverse().compose(VOinBody);
+% GPS metadata
+GPS_metadata = importdata('KittiRelativePose_metadata.txt');
+GPS_metadata = cell2struct(num2cell(GPS_metadata.data), GPS_metadata.colheaders, 2);
 
-%% Read data and change coordinate frame of GPS and VO measurements to IMU frame
+%% Read data
 disp('-- Reading sensor data from file')
 % IMU data
 IMU_data = importdata('KittiEquivBiasedImu.txt');
@@ -31,24 +28,21 @@ imum = cellfun(@(x) x', num2cell([ [IMU_data.accelX]' [IMU_data.accelY]' [IMU_da
 [IMU_data.acc_omega] = deal(imum{:});
 clear imum
 
-% VO data
-VO_data = importdata('KittiRelativePose.txt');
-VO_data = cell2struct(num2cell(VO_data.data), VO_data.colheaders, 2);
-% Merge relative pose fields and convert to Pose3
-logposes = [ [VO_data.dtx]' [VO_data.dty]' [VO_data.dtz]' [VO_data.drx]' [VO_data.dry]' [VO_data.drz]' ];
-logposes = num2cell(logposes, 2);
-relposes = arrayfun(@(x) {gtsam.Pose3.Expmap(x{:}')}, logposes);
-relposes = arrayfun(@(x) {VOinIMU.compose(x{:}).compose(VOinIMU.inverse())}, relposes);
-[VO_data.RelativePose] = deal(relposes{:});
-VO_data = rmfield(VO_data, { 'dtx' 'dty' 'dtz' 'drx' 'dry' 'drz' });
-noiseModelVO = noiseModel.Diagonal.Sigmas([ VO_metadata.RotationSigma * [1;1;1]; VO_metadata.TranslationSigma * [1;1;1] ]);
-clear logposes relposes
+% GPS data
+GPS_data = importdata('Gps_converted.txt');
+GPS_data = cell2struct(num2cell(GPS_data.data), GPS_data.colheaders, 2);
+for i = 1:numel(GPS_data)
+    GPS_data(i).Position = gtsam.Point3(GPS_data(i).X, GPS_data(i).Y, GPS_data(i).Z);
+end
+noiseModelGPS = noiseModel.Diagonal.Precisions([ [0;0;0]; 1.0/0.07 * [1;1;1] ]);
+firstGPSPose = 2;
+GPSskip = 10; % Skip this many GPS measurements each time
 
 %% Get initial conditions for the estimated trajectory
-currentPoseGlobal = Pose3;
+currentPoseGlobal = Pose3(Rot3, GPS_data(firstGPSPose).Position); % initial pose is the reference frame (navigation frame)
 currentVelocityGlobal = LieVector([0;0;0]); % the vehicle is stationary at the beginning
 currentBias = imuBias.ConstantBias(zeros(3,1), zeros(3,1));
-sigma_init_x = noiseModel.Isotropic.Sigmas([ 1.0; 1.0; 0.01; 0.01; 0.01; 0.01 ]);
+sigma_init_x = noiseModel.Isotropic.Precisions([ 0.0; 0.0; 0.0; 1; 1; 1 ]);
 sigma_init_v = noiseModel.Isotropic.Sigma(3, 1000.0);
 sigma_init_b = noiseModel.Isotropic.Sigmas([ 0.100; 0.100; 0.100; 5.00e-05; 5.00e-05; 5.00e-05 ]);
 sigma_between_b = [ IMU_metadata.AccelerometerBiasSigma * ones(3,1); IMU_metadata.GyroscopeBiasSigma * ones(3,1) ];
@@ -67,22 +61,19 @@ newValues = Values;
 % (1) we read the measurements
 % (2) we create the corresponding factors in the graph
 % (3) we solve the graph to obtain and optimal estimate of robot trajectory
-timestamps = [VO_data.Time]';
-
-timestamps = timestamps(15:end,:); % there seem to be issues with the initial IMU measurements
 IMUtimes = [IMU_data.Time];
 
-disp('-- Starting main loop: inference is performed at each time step, but we plot trajectory every 100 steps')
+disp('-- Starting main loop: inference is performed at each time step, but we plot trajectory every 10 steps')
 
-for measurementIndex = 1:length(timestamps)
+for measurementIndex = firstGPSPose:length(GPS_data)
   
   % At each non=IMU measurement we initialize a new node in the graph
   currentPoseKey = symbol('x',measurementIndex);
   currentVelKey =  symbol('v',measurementIndex);
   currentBiasKey = symbol('b',measurementIndex);
-  t = timestamps(measurementIndex, 1);
+  t = GPS_data(measurementIndex, 1).Time;
   
-  if measurementIndex == 1
+  if measurementIndex == firstGPSPose
     %% Create initial estimate and prior on initial pose, velocity, and biases
     newValues.insert(currentPoseKey, currentPoseGlobal);
     newValues.insert(currentVelKey, currentVelocityGlobal);
@@ -91,7 +82,7 @@ for measurementIndex = 1:length(timestamps)
     newFactors.add(PriorFactorLieVector(currentVelKey, currentVelocityGlobal, sigma_init_v));
     newFactors.add(PriorFactorConstantBias(currentBiasKey, currentBias, sigma_init_b));
   else
-    t_previous = timestamps(measurementIndex-1, 1);
+    t_previous = GPS_data(measurementIndex-1, 1).Time;
     %% Summarize IMU data between the previous GPS measurement and now
     IMUindices = find(IMUtimes >= t_previous & IMUtimes <= t);
     
@@ -112,39 +103,45 @@ for measurementIndex = 1:length(timestamps)
       currentPoseKey, currentVelKey, ...
       currentBiasKey, currentSummarizedMeasurement, g, w_coriolis));
     
-    % LC: sigma_init_b is wrong: this should be some uncertainty on bias evolution given in the IMU metadata
+    % Bias evolution as given in the IMU metadata
     newFactors.add(BetweenFactorConstantBias(currentBiasKey-1, currentBiasKey, imuBias.ConstantBias(zeros(3,1), zeros(3,1)), ...
       noiseModel.Diagonal.Sigmas(sqrt(numel(IMUindices)) * sigma_between_b)));
-    
-    %% Create VO factor
-      VOpose = VO_data(measurementIndex).RelativePose;
-      newFactors.add(BetweenFactorPose3(currentPoseKey - 1, currentPoseKey, VOpose, noiseModelVO));
-    
+
+    % Create GPS factor
+    GPSPose = Pose3(currentPoseGlobal.rotation, GPS_data(measurementIndex).Position);
+    if mod(measurementIndex, GPSskip) == 0
+      newFactors.add(PriorFactorPose3(currentPoseKey, GPSPose, noiseModelGPS));
+    end
+
     % Add initial value
-    newValues.insert(currentPoseKey, currentPoseGlobal.compose(VOpose));
+    newValues.insert(currentPoseKey, GPSPose);
     newValues.insert(currentVelKey, currentVelocityGlobal);
     newValues.insert(currentBiasKey, currentBias);
     
     % Update solver
     % =======================================================================
-    isam.update(newFactors, newValues);
-    newFactors = NonlinearFactorGraph;
-    newValues = Values;
-    
-    if rem(measurementIndex,100)==0 % plot every 100 time steps
-      cla;
-      plot3DTrajectory(isam.calculateEstimate, 'g-');
-      title('Estimated trajectory using ISAM2 (IMU+VO)')
-      xlabel('[m]')
-      ylabel('[m]')
-      zlabel('[m]')
-      axis equal
-      drawnow;
+    % We accumulate 2*GPSskip GPS measurements before updating the solver at
+    % first so that the heading becomes observable.
+    if measurementIndex > firstGPSPose + 2*GPSskip
+      isam.update(newFactors, newValues);
+      newFactors = NonlinearFactorGraph;
+      newValues = Values;
+      
+      if rem(measurementIndex,10)==0 % plot every 10 time steps
+        cla;
+        plot3DTrajectory(isam.calculateEstimate, 'g-');
+        title('Estimated trajectory using ISAM2 (IMU+GPS)')
+        xlabel('[m]')
+        ylabel('[m]')
+        zlabel('[m]')
+        axis equal
+        drawnow;
+      end
+      % =======================================================================
+      currentPoseGlobal = isam.calculateEstimate(currentPoseKey);
+      currentVelocityGlobal = isam.calculateEstimate(currentVelKey);
+      currentBias = isam.calculateEstimate(currentBiasKey);
     end
-    % ======================================================================= 
-    currentPoseGlobal = isam.calculateEstimate(currentPoseKey);
-    currentVelocityGlobal = isam.calculateEstimate(currentVelKey);
-    currentBias = isam.calculateEstimate(currentBiasKey);   
   end
    
 end % end main loop
