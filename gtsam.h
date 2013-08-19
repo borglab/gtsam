4a8fda3eab791a9e3d155e0239c1462093de6991
Duy-Nguyen Ta <thduynguyen@gmail.com>
2013-08-19 17:06:28 +0000

    comment out summarization in gtsam.h, since summarization.h disappeared.

--- gtsam.h
+++ gtsam.h
@@ -1697,16 +1697,16 @@ virtual class LinearContainerFactor : gtsam::NonlinearFactor {
 }; // \class LinearContainerFactor
 
 // Summarization functionality
-#include <gtsam/nonlinear/summarization.h>
-
-// Uses partial QR approach by default
-gtsam::GaussianFactorGraph summarize(
-    const gtsam::NonlinearFactorGraph& graph, const gtsam::Values& values,
-    const gtsam::KeySet& saved_keys);
-
-gtsam::NonlinearFactorGraph summarizeAsNonlinearContainer(
-    const gtsam::NonlinearFactorGraph& graph, const gtsam::Values& values,
-    const gtsam::KeySet& saved_keys);
+//#include <gtsam/nonlinear/summarization.h>
+//
+//// Uses partial QR approach by default
+//gtsam::GaussianFactorGraph summarize(
+//    const gtsam::NonlinearFactorGraph& graph, const gtsam::Values& values,
+//    const gtsam::KeySet& saved_keys);
+//
+//gtsam::NonlinearFactorGraph summarizeAsNonlinearContainer(
+//    const gtsam::NonlinearFactorGraph& graph, const gtsam::Values& values,
+//    const gtsam::KeySet& saved_keys);
 
 //*************************************************************************
 // Nonlinear optimizers
