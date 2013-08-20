4fb723531092f8b7e75d885d5329c84a07e036a7
Richard Roberts <richard.jw.roberts@gmail.com>
2013-08-20 16:53:44 +0000

    Ported a fix from Steve for keeping factor order consistent (I don't remember why this is needed)

--- gtsam/nonlinear/ISAM2.cpp
+++ gtsam/nonlinear/ISAM2.cpp
@@ -762,7 +762,8 @@ void ISAM2::marginalizeLeaves(const FastList<Key>& leafKeysList,
 
   // Keep track of marginal factors - map from clique to the marginal factors
   // that should be incorporated into it, passed up from it's children.
-  multimap<sharedClique, GaussianFactor::shared_ptr> marginalFactors;
+//  multimap<sharedClique, GaussianFactor::shared_ptr> marginalFactors;
+  map<Key, vector<GaussianFactor::shared_ptr> > marginalFactors; // Steve's fix for making factor order consistent
 
   // Remove each variable and its subtrees
   BOOST_REVERSE_FOREACH(Key j, leafKeys) {
@@ -784,7 +785,7 @@ void ISAM2::marginalizeLeaves(const FastList<Key>& leafKeysList,
         // because their information is already incorporated in the new
         // marginal factor.  So, now associate this marginal factor with the
         // parent of this clique.
-        marginalFactors.insert(make_pair(clique->parent(), marginalFactor));
+        marginalFactors[clique->parent()->conditional()->front()].push_back(marginalFactor);
         // Now remove this clique and its subtree - all of its marginal
         // information has been stored in marginalFactors.
         const Cliques removedCliques = this->removeSubtree(clique); // Remove the subtree and throw away the cliques
