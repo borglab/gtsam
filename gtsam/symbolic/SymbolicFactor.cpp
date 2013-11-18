67f70c37b5d1cf94cb0c5af9c620e2f9b54df501
Richard Roberts <richard.jw.roberts@gmail.com>
2013-11-18 19:23:13 +0000

    Factored out symbolic elimination into a templated function

--- gtsam/symbolic/SymbolicFactor.cpp
+++ gtsam/symbolic/SymbolicFactor.cpp
@@ -15,14 +15,12 @@
  * @date    Oct 17, 2010
  */
 
-#include <boost/foreach.hpp>
-#include <boost/make_shared.hpp>
-
 #include <gtsam/base/FastVector.h>
 #include <gtsam/inference/Ordering.h>
 #include <gtsam/symbolic/SymbolicFactor.h>
 #include <gtsam/symbolic/SymbolicConditional.h>
 #include <gtsam/symbolic/SymbolicFactorGraph.h>
+#include <gtsam/symbolic/SymbolicFactor-inst.h>
 
 using namespace std;
 
@@ -32,32 +30,7 @@ namespace gtsam {
   std::pair<boost::shared_ptr<SymbolicConditional>, boost::shared_ptr<SymbolicFactor> >
     EliminateSymbolic(const SymbolicFactorGraph& factors, const Ordering& keys)
   {
-    // Gather all keys
-    FastSet<Key> allKeys;
-    BOOST_FOREACH(const SymbolicFactor::shared_ptr& factor, factors) {
-      allKeys.insert(factor->begin(), factor->end()); }
-
-    // Check keys
-    BOOST_FOREACH(Key key, keys) {
-      if(allKeys.find(key) == allKeys.end())
-        throw std::runtime_error("Requested to eliminate a key that is not in the factors");
-    }
-
-    // Sort frontal keys
-    FastSet<Key> frontals(keys);
-    const size_t nFrontals = keys.size();
-
-    // Build a key vector with the frontals followed by the separator
-    FastVector<Key> orderedKeys(allKeys.size());
-    std::copy(keys.begin(), keys.end(), orderedKeys.begin());
-    std::set_difference(allKeys.begin(), allKeys.end(), frontals.begin(), frontals.end(), orderedKeys.begin() + nFrontals);
-
-    // Return resulting conditional and factor
-    return make_pair(
-      boost::make_shared<SymbolicConditional>(
-      SymbolicConditional::FromKeys(orderedKeys, nFrontals)),
-      boost::make_shared<SymbolicFactor>(
-      SymbolicFactor::FromIterators(orderedKeys.begin() + nFrontals, orderedKeys.end())));
+    return internal::EliminateSymbolic(factors, keys);
   }
 
   /* ************************************************************************* */
