046da246baa451fdbdb4aba8f22dba8f3c291f46
Frank Dellaert <dellaert@gmail.com>
2013-12-21 15:06:39 +0000

    RotateFactor now takes full rotations, old one that takes directions is RotateDirectionsFactor (in same header)
    git-svn-id: https://svn.cc.gatech.edu/borg/gtsam/trunk@20421 898a188c-9671-0410-8e00-e3fd810bbb7f

--- gtsam/slam/RotateFactor.h
+++ gtsam/slam/RotateFactor.h
@@ -13,9 +13,52 @@
 namespace gtsam {
 
 /**
- * Factor that evaluates distance between two rotated directions
+ * Factor on unknown rotation iRC that relates two incremental rotations
+ *   c1Rc2 = iRc' * i1Ri2 * iRc
+ * Which we can write (see doc/math.lyx)
+ *   e^[z] = iRc' * e^[p] * iRc = e^([iRc'*p])
+ * with z and p measured and predicted angular velocities, and hence
+ *   p = iRc * z
  */
 class RotateFactor: public NoiseModelFactor1<Rot3> {
+
+  Point3 p_, z_; ///< Predicted and measured directions, p = iRc * z
+
+  typedef NoiseModelFactor1<Rot3> Base;
+
+public:
+
+  /// Constructor
+  RotateFactor(Key key, const Rot3& P, const Rot3& Z,
+      const SharedNoiseModel& model) :
+      Base(model, key), p_(Rot3::Logmap(P)), z_(Rot3::Logmap(Z)) {
+  }
+
+  /// print
+  virtual void print(const std::string& s = "",
+      const KeyFormatter& keyFormatter = DefaultKeyFormatter) const {
+    Base::print(s);
+    std::cout << "RotateFactor:" << std::endl;
+    p_.print("p");
+    z_.print("z");
+  }
+
+  /// vector of errors returns 2D vector
+  Vector evaluateError(const Rot3& R,
+      boost::optional<Matrix&> H = boost::none) const {
+    // predict p_ as q = R*z_, derivative H will be filled if not none
+    Point3 q = R.rotate(z_,H);
+    // error is just difference, and note derivative of that wrpt q is I3
+    return Vector(3) << q.x()-p_.x(), q.y()-p_.y(), q.z()-p_.z();
+  }
+
+};
+
+/**
+ * Factor on unknown rotation R that relates two directions p_i = iRc * z_c
+ * Directions provide less constraints than a full rotation
+ */
+class RotateDirectionsFactor: public NoiseModelFactor1<Rot3> {
 
   Sphere2 p_, z_; ///< Predicted and measured directions, p = iRc * z
 
@@ -24,7 +67,7 @@ class RotateFactor: public NoiseModelFactor1<Rot3> {
 public:
 
   /// Constructor
-  RotateFactor(Key key, const Sphere2& p, const Sphere2& z,
+  RotateDirectionsFactor(Key key, const Sphere2& p, const Sphere2& z,
       const SharedNoiseModel& model) :
       Base(model, key), p_(p), z_(z) {
   }
@@ -33,6 +76,7 @@ public:
   virtual void print(const std::string& s = "",
       const KeyFormatter& keyFormatter = DefaultKeyFormatter) const {
     Base::print(s);
+    std::cout << "RotateDirectionsFactor:" << std::endl;
     p_.print("p");
     z_.print("z");
   }
@@ -48,19 +92,6 @@ public:
       *H = (*H) * DR;
     }
     return e;
-  }
-
-  /// Obsolete: vector of errors returns 1D vector
-  Vector evaluateError1(const Rot3& R,
-      boost::optional<Matrix&> H = boost::none) const {
-    Sphere2 q = R * z_;
-    double e = p_.distance(q, H);
-    if (H) {
-      Matrix DR;
-      R.rotate(z_, DR);
-      *H = (*H) * DR;
-    }
-    return (Vector(1) << e);
   }
 
 };
