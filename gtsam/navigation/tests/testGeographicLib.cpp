ee018ecbeec7f23e5134b3c4dca5bc72a01515a5
dellaert <dellaert@cc.gatech.edu>
2014-01-25 18:22:23 -0500

    Added unit test scaffold, but no idea how to link

---
+++ gtsam/navigation/tests/testGeographicLib.cpp
@@ -0,0 +1,42 @@
+/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation,
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file    testGeographicLib.cpp
+ * @brief   Unit tests for coordinate conversions
+ * @author  Frank Dellaert
+ */
+
+#include <GeographicLib/Geocentric.hpp>
+#include <CppUnitLite/TestHarness.h>
+
+using namespace std;
+//using namespace gtsam;
+using namespace GeographicLib;
+
+/* ************************************************************************* */
+
+TEST( GeographicLib, Geocentric) {
+
+  Geocentric earth(Constants::WGS84_a(), Constants::WGS84_f());
+
+  // Dekalb-Peachtree Airport runway 2L
+  double lat = 33.87071, lon = -84.30482000000001, h = 274;
+
+
+}
+
+/* ************************************************************************* */
+int main() {
+  TestResult tr;
+  return TestRegistry::runAllTests(tr);
+}
+/* ************************************************************************* */
