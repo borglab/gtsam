ca255a4cee768f09f257da3429acab6ccc22681f
Natesh Srinivasan <natesh.rvce@gmail.com>
2014-01-31 11:57:50 -0500

    Removed shared pointer to Isotropic becasue it has a protected constructor

--- gtsam/linear/NoiseModel.h
+++ gtsam/linear/NoiseModel.h
@@ -870,7 +870,6 @@ namespace gtsam {
   typedef noiseModel::Gaussian::shared_ptr SharedGaussian;
   typedef noiseModel::Diagonal::shared_ptr SharedDiagonal;
   typedef noiseModel::Constrained::shared_ptr SharedConstrained;
-  typedef noiseModel::Isotropic::shared_ptr SharedIsotropic;
 
 } // namespace gtsam
 
