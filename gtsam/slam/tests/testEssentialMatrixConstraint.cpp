ce78be5089e36b093656e5dcad096baa15986d86
Luca <luca.carlone@gatech.edu>
2014-03-02 19:59:14 -0500

    add example BAinSO2

--- gtsam/slam/tests/testEssentialMatrixConstraint.cpp
+++ gtsam/slam/tests/testEssentialMatrixConstraint.cpp
@@ -86,8 +86,8 @@ TEST(EssentialMatrixConstraint, optimization) {
   Pose3 P1true = Pose3();
   Pose3 P2true = Pose3(Rot3(),Point3(1,1,1));
 
-  //Pose3 noisePose = Pose3(Rot3::rodriguez(0.5, 0.5, 0.3),Point3(1,-1,1));
-  Pose3 noisePose = Pose3(Rot3::rodriguez(0.0, 0.0, 0.0),Point3(0.01,-0.01,0.01));
+  Pose3 noisePose = Pose3(Rot3::rodriguez(0.5, 0.5, 0.3),Point3(1,-1,1));
+  //Pose3 noisePose = Pose3(Rot3::rodriguez(0.0, 0.0, 0.0),Point3(0.01,-0.01,0.01));
   Pose3 P1(P1true);
   Pose3 P2 = P2true.compose(noisePose);
 
@@ -168,6 +168,181 @@ TEST(EssentialMatrixConstraint, optimization) {
   EXPECT(assert_equal(P1true,actual.at<Pose3>(1),1e-4));
   EXPECT(assert_equal(expectedRotation,actualRotation,1e-4));
   EXPECT(assert_equal(expectedDirection,actualDirection,1e-4));
+}
+
+#include <gtsam/inference/Symbol.h>
+#include <boost/foreach.hpp>
+#include <boost/assign.hpp>
+#include <boost/assign/std/vector.hpp>
+#include <fstream>
+#include <iostream>
+
+Values loadInitialGuess(const string& filename) {
+  Values initialValues;
+
+  // Load the data file
+  ifstream fin(filename.c_str(),ifstream::in);
+  if(!fin)
+    cout << "Error in loadInitialGuess: can not find the file!!" << endl;
+
+  int pose_id;
+  while (fin >> pose_id) {
+    Matrix3 rotationMatrix;
+    fin >> rotationMatrix(0,0);
+    fin >> rotationMatrix(0,1);
+    fin >> rotationMatrix(0,2);
+    fin >> rotationMatrix(1,0);
+    fin >> rotationMatrix(1,1);
+    fin >> rotationMatrix(1,2);
+    fin >> rotationMatrix(2,0);
+    fin >> rotationMatrix(2,1);
+    fin >> rotationMatrix(2,2);
+    Rot3 R = Rot3(rotationMatrix);
+
+    Vector3 translationVector;
+    for (size_t i = 0; i < 3; ++i) { // fill in translation
+      fin >> translationVector(i);
+    }
+    Point3 t = Point3(translationVector);
+    initialValues.insert(pose_id, Pose3(R,t));
+  }
+  fin.close();
+  return initialValues;
+}
+
+
+bool loadBetween(const string& filename, NonlinearFactorGraph& graph) {
+
+  // Load the data file
+  ifstream fin(filename.c_str(),ifstream::in);
+  if(!fin){
+    cout << "Error in loadBetween: can not find the file!!" << endl;
+    return false;
+  }
+
+  int id1, id2;
+  while (fin >> id1) {
+    fin >> id2;
+
+    Matrix3 rotationMatrix;
+    fin >> rotationMatrix(0,0);
+    fin >> rotationMatrix(0,1);
+    fin >> rotationMatrix(0,2);
+    fin >> rotationMatrix(1,0);
+    fin >> rotationMatrix(1,1);
+    fin >> rotationMatrix(1,2);
+    fin >> rotationMatrix(2,0);
+    fin >> rotationMatrix(2,1);
+    fin >> rotationMatrix(2,2);
+    Rot3 R_E = Rot3(rotationMatrix);
+
+    Vector3 directionVector;
+    for (size_t i = 0; i < 3; ++i) { // fill in translation
+      fin >> directionVector(i);
+    }
+    Unit3 t_E = Unit3(Point3(directionVector));
+    EssentialMatrix E(R_E,t_E);
+    noiseModel::Isotropic::shared_ptr noise = noiseModel::Isotropic::Sigma(5, 1.0);
+    EssentialMatrixConstraint factor(id1, id2, E, noise);
+    graph.push_back(factor);
+
+  }
+  fin.close();
+  return true;
+}
+
+bool loadGravity(const string& filename, NonlinearFactorGraph& graph) {
+
+  // Load the data file
+  ifstream fin(filename.c_str(),ifstream::in);
+  if(!fin){
+    cout << "Error in loadGravity: can not find the file!!" << endl;
+    return false;
+  }
+
+  int id1;
+  while (fin >> id1) {
+    Vector3 gravityVector;
+    for (size_t i = 0; i < 3; ++i) {
+      fin >> gravityVector(i);
+    }
+    Unit3 g_local = Unit3(Point3(gravityVector));
+    SharedNoiseModel model = noiseModel::Isotropic::Sigma(2, 0.5);
+    Pose3AttitudeFactor factor(id1, nDown, model, bZ);
+    graph.push_back(factor);
+  }
+  fin.close();
+  return true;
+}
+
+bool writeEstimate(const string& filename, const Values& estimate) {
+
+  // Open the output file
+  ofstream os;
+  os.open(filename.c_str());
+  os.precision(20);
+  if (!os.is_open()) {
+    cout << "Error in writeEstimate: can not open the file!!" << endl;
+    return false;
+  }
+
+  for(size_t i=0; i<estimate.size(); ++i){ // for all poses
+    Pose3 estimate_i = estimate.at<Pose3>(i);
+    Rot3 R_i   = estimate_i.rotation();
+    Point3 t_i = estimate_i.translation();
+    Matrix3 rotationMatrix = R_i.matrix();
+    os << rotationMatrix(0,0) << " ";
+    os << rotationMatrix(0,1) << " ";
+    os << rotationMatrix(0,2) << " ";
+    os << rotationMatrix(1,0) << " ";
+    os << rotationMatrix(1,1) << " ";
+    os << rotationMatrix(1,2) << " ";
+    os << rotationMatrix(2,0) << " ";
+    os << rotationMatrix(2,1) << " ";
+    os << rotationMatrix(2,2) << " ";
+
+    Vector3 translationVector = t_i.vector();
+    os << translationVector(0) << " ";
+    os << translationVector(1) << " ";
+    os << translationVector(2);
+    os << endl;
+  }
+
+  os.close();
+  return true;
+}
+
+/* ************************************************************************* */
+TEST(EssentialMatrixConstraint, BAinSO2) {
+
+  // create initial values
+  string initialGuessFile = "/home/aspn/borg/BAinSO2/data/initialGuessNL.txt";
+  Values initialValues = loadInitialGuess(initialGuessFile);
+  // initialValues.print("initialValues\n");
+
+  // create EssentialMatrixConstraints
+  string betweenFile = "/home/aspn/borg/BAinSO2/data/betweenNL.txt";
+  NonlinearFactorGraph graph;
+  loadBetween(betweenFile, graph);
+  // graph.print("");
+
+  // create EssentialMatrixConstraints
+//  string gravityFile = "/home/aspn/borg/BAinSO2/data/gravityNL.txt";
+//  loadGravity(gravityFile, graph);
+  // graph.print("");
+
+  LevenbergMarquardtParams params;
+  params.relativeErrorTol = 0.0;
+  params.absoluteErrorTol = 0.0;
+  params.setVerbosityLM("TRYLAMBDA");
+  //params.setVerbosity("DELTA");
+  LevenbergMarquardtOptimizer lm(graph, initialValues, params);
+  Values estimate = lm.optimize();
+
+  // write results
+  string outputFile = "/home/aspn/borg/BAinSO2/data/estimateNL.txt";
+  bool success = writeEstimate(outputFile, estimate);
+  std::cout << "success: " << success << std::endl;
 }
 
 /* ************************************************************************* */
