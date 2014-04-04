d394ec5574beb045816efd36865945fb8da6d7f9
djensen3 <david.jensen@gtri.gatech.edu>
2014-04-04 17:00:20 -0400

    Added ground truth factor graph creation. Added OdometryExample3D as a modified version of OdometryExample for reference (can be removed later)

--- matlab/unstable_examples/+imuSimulator/covarianceAnalysisBetween.m
+++ matlab/unstable_examples/+imuSimulator/covarianceAnalysisBetween.m
@@ -1,3 +1,5 @@
+import gtsam.*;
+
 % Test GTSAM covariances on a graph with betweenFactors
 
 clc
@@ -5,26 +7,56 @@ clear all
 close all
 
 %% Create ground truth trajectory
-trajectoryLength = 100;
+trajectoryLength = 5;
 
 % possibly create random trajectory
 currentPoseKey = symbol('x', 0);
 currentPose = Pose3;
 gtValues = Values;
 gtValues.insert(currentPoseKey, currentPose);
+gtGraph = NonlinearFactorGraph;
+gtNoise = noiseModel.Diagonal.Sigmas([0.1; 0.1; 0.1; 0.05; 0.05; 0.05]); % Noise for GT measurements
 
 for i=1:trajectoryLength
   currentPoseKey = symbol('x', i);
-  deltaPosition = % create random vector with mean [x 0 0]
-  deltaRotation = % create random rotation with mean [0 0 0]
-  deltaPose = Pose3(deltaRotation, Point3(deltaPosition));
+  deltaPosition = 0.5*rand(3,1) + [1;0;0]; % create random vector with mean = [1 0 0] and sigma = 0.5
+  deltaRotation = 0.1*rand(3,1) + [0;0;0]; % create random rotation with mean [0 0 0] and sigma = 0.1 (rad)
+  deltaPose = Pose3.Expmap([deltaRotation; deltaPosition]);
+  deltaPoseNoise = gtNoise;
+  
   % "Deduce" ground truth measurements
   % deltaPose are the gt measurements - save them in some structure
+  gtMeasurementPose(i) = deltaPose;
   currentPose = currentPose.compose(deltaPose);
   gtValues.insert(currentPoseKey, currentPose);
+  
+  % Add the factor to the factor graph
+  if(i == 1)
+      gtGraph.add(PriorFactorPose3(currentPoseKey, deltaPose, deltaPoseNoise));
+  else
+      gtGraph.add(BetweenFactorPose3(previousPoseKey, currentPoseKey, deltaPose, deltaPoseNoise));
+  end
+  previousPoseKey = currentPoseKey;
 end
 
+gtGraph.print(sprintf('\nGround Truth - Factor graph:\n'));
+gtValues.print(sprintf('\nGround Truth - Values:\n'));
+
 %% Create gt graph (using between with ground truth measurements)
+% Optimize using Levenberg-Marquardt
+optimizer = LevenbergMarquardtOptimizer(gtGraph, gtValues);
+gtResult = optimizer.optimizeSafely();
+gtResult.print(sprintf('\nGround Truth - Final Result:\n'));
+
+% Plot trajectory and covariance ellipses
+% Couldn't get this to work in the modified example (OdometryExample3D).
+% Something strange with 3D trajectories?
+cla;
+hold on;
+
+plot3DTrajectory(gtResult, [], Marginals(gtGraph, gtResult));
+axis equal
+
 % Compute covariances using gtGraph and gtValues (for visualization)
 
 % decide measurement covariance
