4037d1ec1af5f76eab4a17aef65ff078a3b54ed9
thduynguyen <thduynguyen@gmail.com>
2014-05-07 08:34:04 -0400

    wrap QPSolver

--- gtsam.h
+++ gtsam.h
@@ -1539,6 +1539,15 @@ class KalmanFilter {
       Vector z, Matrix Q);
 };
 
+#include <gtsam/linear/QPSolver.h>
+class QPSolver {
+  QPSolver(const gtsam::GaussianFactorGraph &graph);
+  pair<gtsam::VectorValues, gtsam::VectorValues> optimize(const gtsam::VectorValues& initials) const;
+  pair<gtsam::VectorValues, gtsam::VectorValues> optimize() const;
+  pair<bool, gtsam::VectorValues> findFeasibleInitialValues() const;
+};
+
+
 //*************************************************************************
 // nonlinear
 //*************************************************************************
