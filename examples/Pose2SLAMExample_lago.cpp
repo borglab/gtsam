801539261010a8e8a7b11dd35d5a1b2b8526f857
Luca <luca.carlone@gatech.edu>
2014-05-20 18:12:26 -0400

    examples with lago and GN

--- examples/Pose2SLAMExample_lago.cpp
+++ examples/Pose2SLAMExample_lago.cpp
@@ -10,10 +10,11 @@
  * -------------------------------------------------------------------------- */
 
 /**
- * @file Pose2SLAMExample_g2o.cpp
- * @brief A 2D Pose SLAM example that reads input from g2o, converts it to a factor graph and does the
- * optimization. Output is written on a file, in g2o format
- * Syntax for the script is ./Pose2SLAMExample_g2o input.g2o output.g2o
+ * @file Pose2SLAMExample_lago.cpp
+ * @brief A 2D Pose SLAM example that reads input from g2o, and solve the Pose2 problem
+ * using LAGO (Linear Approximation for Graph Optimization). See class LagoInitializer.h
+ * Output is written on a file, in g2o format
+ * Syntax for the script is ./Pose2SLAMExample_lago input.g2o output.g2o
  * @date May 15, 2014
  * @author Luca Carlone
  */
@@ -22,10 +23,9 @@
 #include <gtsam/inference/Key.h>
 #include <gtsam/slam/PriorFactor.h>
 #include <gtsam/slam/dataset.h>
-#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/nonlinear/LagoInitializer.h>
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
 #include <gtsam/nonlinear/GaussNewtonOptimizer.h>
-#include <gtsam/nonlinear/Marginals.h>
 #include <gtsam/nonlinear/Values.h>
 #include <fstream>
 #include <sstream>
@@ -49,14 +49,13 @@ int main(const int argc, const char *argv[]){
   noiseModel::Diagonal::shared_ptr priorModel = noiseModel::Diagonal::Variances((Vector(3) << 1e-6, 1e-6, 1e-8));
   graphWithPrior.add(PriorFactor<Pose2>(0, Pose2(), priorModel));
 
-  std::cout << "Optimizing the factor graph" << std::endl;
-  GaussNewtonOptimizer optimizer(graphWithPrior, initial); // , parameters);
-  Values result = optimizer.optimize();
-  std::cout << "Optimization complete" << std::endl;
+  std::cout << "Computing LAGO estimate" << std::endl;
+  Values estimateLago = initializeLago(graphWithPrior);
+  std::cout << "done!" << std::endl;
 
   const string outputFile = argv[2];
   std::cout << "Writing results to file: " << outputFile << std::endl;
-  writeG2o(outputFile, graph, result);
+  writeG2o(outputFile, graph, estimateLago);
   std::cout << "done! " << std::endl;
 
   return 0;
