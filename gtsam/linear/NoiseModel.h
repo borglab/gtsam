24f1da0e0174907ebf56a3c4092d62d257e952f1
cbeall3 <cbeall3@gatech.edu>
2014-08-14 00:15:15 -0400

    Fixes for latest boost 1.56 on Linux

--- gtsam/linear/NoiseModel.h
+++ gtsam/linear/NoiseModel.h
@@ -19,6 +19,8 @@
 #pragma once
 
 #include <boost/serialization/nvp.hpp>
+#include <boost/serialization/extended_type_info.hpp>
+#include <boost/serialization/singleton.hpp>
 #include <boost/serialization/shared_ptr.hpp>
 #include <boost/serialization/optional.hpp>
 #include <gtsam/base/Matrix.h>
