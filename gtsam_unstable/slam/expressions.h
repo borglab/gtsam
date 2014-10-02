d5709facf635f641cabef862d4af3cf87dd117c2
dellaert <dellaert@cc.gatech.edu>
2014-10-02 20:20:00 +0200

    Added Pose2SLAMExample

--- gtsam_unstable/slam/expressions.h
+++ gtsam_unstable/slam/expressions.h
@@ -13,15 +13,36 @@
 
 namespace gtsam {
 
+// Generics
+
+template<class T>
+Expression<T> between(const Expression<T>& t1, const Expression<T>& t2) {
+  return Expression<T>(t1, &T::between, t2);
+}
+
+// 2D Geometry
+
 typedef Expression<Point2> Point2_;
+typedef Expression<Rot2> Rot2_;
+typedef Expression<Pose2> Pose2_;
+
+Point2_ transform_to(const Pose2_& x, const Point2_& p) {
+  return Point2_(x, &Pose2::transform_to, p);
+}
+
+// 3D Geometry
+
 typedef Expression<Point3> Point3_;
 typedef Expression<Rot3> Rot3_;
 typedef Expression<Pose3> Pose3_;
-typedef Expression<Cal3_S2> Cal3_S2_;
 
 Point3_ transform_to(const Pose3_& x, const Point3_& p) {
   return Point3_(x, &Pose3::transform_to, p);
 }
+
+// Projection
+
+typedef Expression<Cal3_S2> Cal3_S2_;
 
 Point2_ project(const Point3_& p_cam) {
   return Point2_(PinholeCamera<Cal3_S2>::project_to_camera, p_cam);
