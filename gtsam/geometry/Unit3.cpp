fcc49bd22ae925f9ae22581830a6fb345e51cf25
cbeall3 <cbeall3@gatech.edu>
2014-10-14 16:30:13 -0400

    Fix for nasty intermittent TBB crashes in testEssentialMatrixFactor and testGeneralSFMFactor. Fixes issue 93

--- gtsam/geometry/Unit3.cpp
+++ gtsam/geometry/Unit3.cpp
@@ -67,11 +67,11 @@ Unit3 Unit3::Random(boost::mt19937 & rng) {
 }
 
 /* ************************************************************************* */
-const Matrix& Unit3::basis() const {
+const Unit3::Matrix32& Unit3::basis() const {
 
   // Return cached version if exists
-  if (B_.rows() == 3)
-    return B_;
+  if (B_)
+    return *B_;
 
   // Get the axis of rotation with the minimum projected length of the point
   Point3 axis;
@@ -92,9 +92,9 @@ const Matrix& Unit3::basis() const {
   b2 = b2 / b2.norm();
 
   // Create the basis matrix
-  B_ = Matrix(3, 2);
-  B_ << b1.x(), b2.x(), b1.y(), b2.y(), b1.z(), b2.z();
-  return B_;
+  B_.reset(Unit3::Matrix32());
+  (*B_) << b1.x(), b2.x(), b1.y(), b2.y(), b1.z(), b2.z();
+  return *B_;
 }
 
 /* ************************************************************************* */
