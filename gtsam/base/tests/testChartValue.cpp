aabdc622c310dd9b6fe12d4a045f9e4a30b5adb5
Mike Bosse <mike.bosse@mavt.ethz.ch>
2014-10-22 23:30:35 +0200

    testing adding Chart to Values works (test fails as it is supposed to)

--- gtsam/base/tests/testChartValue.cpp
+++ gtsam/base/tests/testChartValue.cpp
@@ -16,13 +16,16 @@
 
 #include <CppUnitLite/TestHarness.h>
 
-#include <gtsam/base/Testable.h>
 #include <gtsam/base/ChartValue.h>
+#include <gtsam/base/Testable.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/nonlinear/Values.h>
 
 using namespace gtsam;
 
 GTSAM_CONCEPT_TESTABLE_INST(ChartValue<double>)
 GTSAM_CONCEPT_TESTABLE_INST(ChartValue<Vector3>)
+//GTSAM_CONCEPT_TESTABLE_INST(ChartValue<Pose3>)  // traits of Pose3 not ready yet :-(
 
 /* ************************************************************************* */
 TEST( testChartValue, construction_double ) {
@@ -42,6 +45,36 @@ TEST( testChartValue, construction_Vector3 ) {
 
   EXPECT(value1.dim() == 3);
   EXPECT(assert_equal(value1, value2));
+}
+
+/* ************************************************************************* */
+// TEST( testChartValue, construction_Pose3 ) {
+//   Pose3 p;
+//   DefaultChart<Pose3> chart(p);
+//   ChartValue<Pose3> value1(p), value2(chart);
+
+//   EXPECT(value1.dim() == 3);
+//   EXPECT(assert_equal(value1, value2));
+// }
+
+/* ************************************************************************* */
+TEST( testChartValue, addition_to_values ) {
+  //  ChartValue<Pose3> pose(Pose3());
+  ChartValue<double> x(5.0);
+  Vector4 v_; v_ << 1.0,2.0,3.0,4.0;
+  ChartValue<Vector4> v(v_); 
+
+  Values values;
+  //values.insert(42,pose);
+  values.insert(43,x);
+  values.insert(44,v);
+
+  //ChartValue<Pose3> p = values.at<ChartValue<Pose3> >(42);
+  ChartValue<double> y = values.at<ChartValue<double> >(43);
+  ChartValue<Vector3> v3 = values.at<ChartValue<Vector3> >(44); // throws ! // not sure how to do an EXPECT_FAIL
+
+  //EXPECT(assert_equal(pose, p));
+  EXPECT(assert_equal(x, y));
 }
 
 /* ************************************************************************* */
