06e3f36f22a800c53070c01953be8aa4c2d5af94
thduynguyen <thduynguyen@gmail.com>
2014-11-21 14:19:43 -0500

    remove printing

--- gtsam/3rdparty/lp_solve_5.5/cmake/Findlpsolve.cmake
+++ gtsam/3rdparty/lp_solve_5.5/cmake/Findlpsolve.cmake
@@ -6,19 +6,15 @@
 #  lpsolve_LIBRARY = /usr/local/lib/liblpsolve.so
 #  lpsolve_LIBRARY_DIRS = /usr/local/lib
 
-find_library (lpsolve_LIBRARY liblpsolve55.a
-  PATHS "${CMAKE_SOURCE_DIR}/gtsam/3rdparty/lp_solve_5.5/lpsolve55")
-
-message("Finding lpsolve lib: " ${lpsolve_LIBRARY})
+set(lpsolve_BUILD_PATH "${CMAKE_SOURCE_DIR}/gtsam/3rdparty/lp_solve_5.5/lpsolve55")
+find_library (lpsolve_LIBRARY liblpsolve55.a PATHS ${lpsolve_BUILD_PATH})
 
 if (lpsolve_LIBRARY)
-  get_filename_component (lpsolve_LIBRARY_DIRS
-    "${lpsolve_LIBRARY}" PATH)
+  get_filename_component (lpsolve_LIBRARY_DIRS "${lpsolve_LIBRARY}" PATH)
   get_filename_component (_ROOT_DIR "${lpsolve_LIBRARY_DIRS}" PATH)
   set (lpsolve_FOUND TRUE)
   set (lpsolve_INCLUDE_DIRS "${_ROOT_DIR}")
   unset (_ROOT_DIR)
-  message("lpsolve_INCLUDE_DIRS: " ${lpsolve_INCLUDE_DIRS})
   if (NOT EXISTS "${lpsolve_INCLUDE_DIRS}/lp_lib.h")
     unset (lpsolve_INCLUDE_DIRS)
     unset (lpsolve_LIBRARY)
