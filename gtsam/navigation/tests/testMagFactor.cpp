a8f942f19d6dd1bf9940c773bfb78092f7186a7f
Paul Furgale <paul.furgale@gmail.com>
2014-11-24 07:57:47 +0100

    Fixing the mag factor tests

--- gtsam/navigation/tests/testMagFactor.cpp
+++ gtsam/navigation/tests/testMagFactor.cpp
@@ -19,6 +19,7 @@
 #include <gtsam/navigation/MagFactor.h>
 #include <gtsam/base/Testable.h>
 #include <gtsam/base/numericalDerivative.h>
+#include <gtsam/base/LieScalar.h>
 
 #include <CppUnitLite/TestHarness.h>
 
