8fcf9d98661a1cfc3ca54ed25557942380f7f6dc
dellaert <dellaert@cc.gatech.edu>
2014-12-08 23:58:54 +0100

    SO(3) is a *Matrix* Lie group. This is it. Rot3 will inherit from this *or* Quaternion.

---
+++ gtsam/geometry/SO3.h
@@ -0,0 +1,101 @@
+/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation,
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file    SO3.h
+ * @brief   3*3 matrix representation o SO(3)
+ * @author  Frank Dellaert
+ * @date    December 2014
+ */
+
+#pragma once
+
+#include <gtsam/base/Matrix.h>
+#include <gtsam/base/concepts.h>
+
+#include <cmath>
+
+namespace gtsam {
+
+/**
+ *  True SO(3), i.e., 3*3 matrix subgroup
+ *  We guarantee (all but first) constructors only generate from sub-manifold.
+ *  However, round-off errors in repeated composition could move off it...
+ */
+class SO3: public Matrix3 {
+
+protected:
+
+public:
+
+  /// Constructor from Eigen Matrix
+  template<typename Derived>
+  SO3(const MatrixBase<Derived>& R) :
+      Matrix3(R.eval()) {
+  }
+
+  /// Constructor from AngleAxisd
+  SO3(const Eigen::AngleAxisd& angleAxis) :
+      Matrix3(angleAxis) {
+  }
+};
+
+/// Default Chart for SO3
+struct SO3Chart {
+
+  typedef SO3 ManifoldType;
+
+  /// Exponential map given axis/angle representation of Lie algebra
+  static SO3 Expmap(const double& theta, const Vector3& w);
+
+  /// Exponential map, simply be converting omega to axis/angle representation
+  static SO3 Expmap(const Eigen::Ref<const Vector3>& omega) {
+    if (omega.isZero())
+      return SO3::Identity();
+    else {
+      double angle = omega.norm();
+      return Expmap(angle, omega / angle);
+    }
+  }
+
+  /// retract, simply be converting omega to AngleAxis
+  static SO3 Retract(const SO3& p, const Eigen::Ref<const Vector3>& omega) {
+    return p * Expmap(omega);
+  }
+
+  /// We use our own Logmap, as there is a slight bug in Eigen
+  static Vector3 Logmap(const SO3& R);
+
+  /// local is our own, as there is a slight bug in Eigen
+  static Vector3 Local(const SO3& q1, const SO3& q2) {
+    return Logmap(q1.inverse() * q2);
+  }
+
+};
+
+#define SO3_TEMPLATE
+GTSAM_GROUP_IDENTITY0(SO3)
+GTSAM_MULTIPLICATIVE_GROUP(SO3_TEMPLATE, SO3)
+
+#define SO3_TANGENT Vector3
+#define SO3_CHART SO3Chart
+GTSAM_MANIFOLD(SO3_TEMPLATE, SO3, 3, SO3_TANGENT, SO3_CHART)
+
+/// Define SO3 to be a model of the Lie Group concept
+namespace traits {
+template<>
+struct structure_category<SO3> {
+  typedef lie_group_tag type;
+};
+}
+
+} // end namespace gtsam
+
---
+++ gtsam/geometry/tests/testSO3.cpp
@@ -0,0 +1,83 @@
+/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation, 
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file   testQuaternion.cpp
+ * @brief  Unit tests for SO3, as a GTSAM-adapted Lie Group
+ * @author Frank Dellaert
+ **/
+
+#include <gtsam/geometry/SO3.h>
+#include <CppUnitLite/TestHarness.h>
+
+using namespace std;
+using namespace gtsam;
+
+//******************************************************************************
+TEST(SO3 , Concept) {
+  BOOST_CONCEPT_ASSERT((IsGroup<SO3 >));
+  BOOST_CONCEPT_ASSERT((IsManifold<SO3 >));
+//  BOOST_CONCEPT_ASSERT((IsLieGroup<SO3 >));
+}
+
+//******************************************************************************
+TEST(SO3 , Constructor) {
+  SO3 q(Eigen::AngleAxisd(1, Vector3(0, 0, 1)));
+}
+
+//******************************************************************************
+TEST(SO3 , Invariants) {
+  SO3 q1(Eigen::AngleAxisd(1, Vector3(0, 0, 1)));
+  SO3 q2(Eigen::AngleAxisd(2, Vector3(0, 1, 0)));
+  // group::check_invariants(q1,q2); Does not satisfy Testable concept (yet!)
+}
+
+//******************************************************************************
+TEST(SO3 , Local) {
+  Vector3 z_axis(0, 0, 1);
+  SO3 q1(Eigen::AngleAxisd(0, z_axis));
+  SO3 q2(Eigen::AngleAxisd(0.1, z_axis));
+  typedef manifold::traits::DefaultChart<SO3>::type Chart;
+  Vector3 expected(0, 0, 0.1);
+  Vector3 actual = Chart::Local(q1, q2);
+  EXPECT(assert_equal((Vector)expected,actual));
+}
+
+//******************************************************************************
+TEST(SO3 , Retract) {
+  Vector3 z_axis(0, 0, 1);
+  SO3 q(Eigen::AngleAxisd(0, z_axis));
+  SO3 expected(Eigen::AngleAxisd(0.1, z_axis));
+  typedef manifold::traits::DefaultChart<SO3>::type Chart;
+  Vector3 v(0, 0, 0.1);
+  SO3 actual = Chart::Retract(q, v);
+  EXPECT(actual.isApprox(expected));
+}
+
+//******************************************************************************
+TEST(SO3 , Compose) {
+}
+
+//******************************************************************************
+TEST(SO3 , Between) {
+}
+
+//******************************************************************************
+TEST(SO3 , Inverse) {
+}
+
+//******************************************************************************
+int main() {
+  TestResult tr;
+  return TestRegistry::runAllTests(tr);
+}
+//******************************************************************************
+
