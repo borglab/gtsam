c1ec3799f5f61ee51a7e9d734fdf72c443cf512f
krunalchande <krunalchande@gmail.com>
2014-12-22 18:20:44 -0500

    Working nonlinear inequality constraints with unit tests.

--- gtsam_unstable/nonlinear/NonlinearInequality.h
+++ gtsam_unstable/nonlinear/NonlinearInequality.h
@@ -11,30 +11,11 @@
 
 namespace gtsam {
 
-class NonlinearInequality : public NonlinearConstraint {
-  bool active_;
-
-  typedef NonlinearConstraint Base;
-
-public:
-  typedef boost::shared_ptr<NonlinearInequality> shared_ptr;
-
-public:
-  /// Construct with dual key
-  NonlinearInequality(Key dualKey) : Base(dualKey), active_(true) {}
-
-  /**
-   * compute the HessianFactor of the (-dual * constraintHessian) for the qp subproblem's objective function
-   */
-  virtual GaussianFactor::shared_ptr multipliedHessian(const Values& x,
-      const VectorValues& duals) const = 0;
-};
-
 /* ************************************************************************* */
 /** A convenient base class for creating a nonlinear equality constraint with 1
  * variables.  To derive from this class, implement evaluateError(). */
 template<class VALUE>
-class NonlinearInequality1: public NonlinearConstraint1<VALUE>, public NonlinearInequality {
+class NonlinearInequality1: public NonlinearConstraint1<VALUE> {
 
 public:
 
@@ -62,8 +43,8 @@ public:
    * @param j key of the variable
    * @param constraintDim number of dimensions of the constraint error function
    */
-  NonlinearInequality1(Key key, Key dualKey, size_t constraintDim = 1) :
-      Base(noiseModel::Constrained::All(constraintDim), key), NonlinearConstraint(dualKey) {
+  NonlinearInequality1(Key key, Key dualKey) :
+      Base(key, dualKey, 1) {
   }
 
   virtual ~NonlinearInequality1() {
@@ -74,9 +55,63 @@ public:
    *  If any of the optional Matrix reference arguments are specified, it should compute
    *  both the function evaluation and its derivative(s) in X1 (and/or X2).
    */
+  virtual double
+  computeError(const X&, boost::optional<Matrix&> H1 = boost::none) const = 0;
+
+  /** predefine evaluateError to return a 1-dimension vector */
   virtual Vector
-  evaluateError(const X&, boost::optional<Matrix&> H1 = boost::none) const {
-    return (Vector(1) << computeError(X, H1));
+  evaluateError(const X& x, boost::optional<Matrix&> H1 = boost::none) const {
+    return (Vector(1) << computeError(x, H1)).finished();
+  }
+//
+//  virtual GaussianFactor::shared_ptr multipliedHessian(const Values& x,
+//      const VectorValues& duals) const {
+//    return Base::multipliedHessian(x, duals);
+//  }
+
+};
+// \class NonlinearConstraint1
+
+/* ************************************************************************* */
+/** A convenient base class for creating your own NonlinearConstraint with 2
+ * variables.  To derive from this class, implement evaluateError(). */
+template<class VALUE1, class VALUE2>
+class NonlinearInequality2: public NonlinearConstraint2<VALUE1, VALUE2> {
+
+public:
+
+  // typedefs for value types pulled from keys
+  typedef VALUE1 X1;
+  typedef VALUE2 X2;
+
+protected:
+
+  typedef NonlinearConstraint2<VALUE1, VALUE2> Base;
+  typedef NonlinearInequality2<VALUE1, VALUE2> This;
+
+private:
+  static const int X1Dim = traits::dimension<VALUE1>::value;
+  static const int X2Dim = traits::dimension<VALUE2>::value;
+
+public:
+
+  /**
+   * Default Constructor for I/O
+   */
+  NonlinearInequality2() {
+  }
+
+  /**
+   * Constructor
+   * @param j1 key of the first variable
+   * @param j2 key of the second variable
+   * @param constraintDim number of dimensions of the constraint error function
+   */
+  NonlinearInequality2(Key j1, Key j2, Key dualKey) :
+    Base(j1, j2, 1) {
+  }
+
+  virtual ~NonlinearInequality2() {
   }
 
   /**
@@ -85,9 +120,17 @@ public:
    *  both the function evaluation and its derivative(s) in X1 (and/or X2).
    */
   virtual double
-  computeError(const X&, boost::optional<Matrix&> H1 = boost::none) const = 0;
+  computeError(const X1&, const X2&, boost::optional<Matrix&> H1 = boost::none,
+      boost::optional<Matrix&> H2 = boost::none) const = 0;
 
+  /** predefine evaluateError to return a 1-dimension vector */
+  virtual Vector
+  evaluateError(const X1& x1, const X2& x2, boost::optional<Matrix&> H1 = boost::none,
+      boost::optional<Matrix&> H2 = boost::none) const {
+    return (Vector(1) << computeError(x1, x2, H1, H2)).finished();
+  }
 };
-// \class NonlinearConstraint1
+// \class NonlinearConstraint2
+
 
 } /* namespace gtsam */
