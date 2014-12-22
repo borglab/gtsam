128cc741acc7805a645d30766f509e7aa6587ba8
dellaert <dellaert@cc.gatech.edu>
2014-12-22 14:55:14 +0100

    LieScalar loses its mojo (superfluous Lie/Manifold stuff)

--- gtsam/base/LieScalar.h
+++ gtsam/base/LieScalar.h
@@ -49,6 +49,9 @@ namespace gtsam {
     /** Automatic conversion to underlying value */
     operator double() const { return d_; }
 
+    /// @name Testable
+    /// @{
+
     /** print @param name optional string naming the object */
     void print(const std::string& name="") const;
 
@@ -57,65 +60,7 @@ namespace gtsam {
       return fabs(expected.d_ - d_) <= tol;
     }
 
-    // Manifold requirements
-
-    /** Returns dimensionality of the tangent space */
-    size_t dim() const { return 1; }
-    static size_t Dim() { return 1; }
-
-    /** Update the LieScalar with a tangent space update */
-    LieScalar retract(const Vector& v) const { return LieScalar(value() + v(0)); }
-
-    /** @return the local coordinates of another object */
-    Vector localCoordinates(const LieScalar& t2) const { return (Vector(1) << (t2.value() - value())).finished(); }
-
-    // Group requirements
-
-    /** identity */
-    static LieScalar identity() {
-      return LieScalar();
-    }
-
-    /** compose with another object */
-    LieScalar compose(const LieScalar& p,
-        boost::optional<Matrix&> H1=boost::none,
-        boost::optional<Matrix&> H2=boost::none) const {
-      if(H1) *H1 = eye(1);
-      if(H2) *H2 = eye(1);
-      return LieScalar(d_ + p.d_);
-    }
-
-    /** between operation */
-    LieScalar between(const LieScalar& l2,
-        boost::optional<Matrix&> H1=boost::none,
-        boost::optional<Matrix&> H2=boost::none) const {
-      if(H1) *H1 = -eye(1);
-      if(H2) *H2 = eye(1);
-      return LieScalar(l2.value() - value());
-    }
-
-    /** invert the object and yield a new one */
-    LieScalar inverse() const {
-      return LieScalar(-1.0 * value());
-    }
-
-    // Lie functions
-
-    /** Expmap around identity */
-    static LieScalar Expmap(const Vector& v) { return LieScalar(v(0)); }
-
-    /** Logmap around identity - just returns with default cast back */
-    static Vector Logmap(const LieScalar& p) { return (Vector(1) << p.value()).finished(); }
-
-    /// Left-trivialized derivative of the exponential map
-    static Matrix dexpL(const Vector& v) {
-      return eye(1);
-    }
-
-    /// Left-trivialized derivative inverse of the exponential map
-    static Matrix dexpInvL(const Vector& v) {
-      return eye(1);
-    }
+    /// @}
 
   private:
       double d_;
