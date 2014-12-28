401ee703647a575483a8ab115fa65c388d3818c4
dellaert <dellaert@cc.gatech.edu>
2014-12-28 17:14:04 +0100

    Fixed overload resoltion error in template matching by removing overloads in traits

--- gtsam/nonlinear/expressions.h
+++ gtsam/nonlinear/expressions.h
@@ -8,14 +8,14 @@
 #pragma once
 
 #include <gtsam/nonlinear/Expression.h>
-#include <boost/bind.hpp>
+#include <gtsam/base/Lie.h>
 
 namespace gtsam {
 
 // Generics
 template<typename T>
 Expression<T> between(const Expression<T>& t1, const Expression<T>& t2) {
-  return Expression<T>(t1, &T::between, t2);
+  return Expression<T>(traits<T>::Between, t1, t2);
 }
 
 typedef Expression<double> double_;
