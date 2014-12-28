4d6455fa7fd248be62737270c5ac6b7cd4362437
dellaert <dellaert@cc.gatech.edu>
2014-12-28 20:35:26 +0100

    Made tests work again after removing overloads (in develop)

--- gtsam/base/testLie.h
+++ gtsam/base/testLie.h
@@ -34,23 +34,25 @@ void testLieGroupDerivatives(TestResult& result_, const std::string& name_,
 
   Matrix H1, H2;
   typedef traits<G> T;
+  typedef OptionalJacobian<T::dimension,T::dimension> OJ;
 
   // Inverse
+  OJ none;
   EXPECT(assert_equal(t1.inverse(),T::Inverse(t1, H1)));
-  EXPECT(assert_equal(numericalDerivative11(T::Inverse, t1),H1));
+  EXPECT(assert_equal(numericalDerivative21<G,G,OJ>(T::Inverse, t1, none),H1));
 
   EXPECT(assert_equal(t2.inverse(),T::Inverse(t2, H1)));
-  EXPECT(assert_equal(numericalDerivative11(T::Inverse, t2),H1));
+  EXPECT(assert_equal(numericalDerivative21<G,G,OJ>(T::Inverse, t2, none),H1));
 
   // Compose
   EXPECT(assert_equal(t1 * t2,T::Compose(t1, t2, H1, H2)));
-  EXPECT(assert_equal(numericalDerivative21(T::Compose, t1, t2), H1));
-  EXPECT(assert_equal(numericalDerivative22(T::Compose, t1, t2), H2));
+  EXPECT(assert_equal(numericalDerivative41<G,G,G,OJ,OJ>(T::Compose, t1, t2, none, none), H1));
+  EXPECT(assert_equal(numericalDerivative42<G,G,G,OJ,OJ>(T::Compose, t1, t2, none, none), H2));
 
   // Between
   EXPECT(assert_equal(t1.inverse() * t2,T::Between(t1, t2, H1, H2)));
-  EXPECT(assert_equal(numericalDerivative21(T::Between, t1, t2), H1));
-  EXPECT(assert_equal(numericalDerivative22(T::Between, t1, t2), H2));
+  EXPECT(assert_equal(numericalDerivative41<G,G,G,OJ,OJ>(T::Between, t1, t2, none, none), H1));
+  EXPECT(assert_equal(numericalDerivative42<G,G,G,OJ,OJ>(T::Between, t1, t2, none, none), H2));
 }
 // Do a comprehensive test of Lie Group Chart derivatives
 template<typename G>
@@ -59,17 +61,20 @@ void testChartDerivatives(TestResult& result_, const std::string& name_,
 
   Matrix H1, H2;
   typedef traits<G> T;
+  typedef typename G::TangentVector V;
+  typedef OptionalJacobian<T::dimension,T::dimension> OJ;
 
   // Retract
-  typename G::TangentVector w12 = T::Local(t1, t2);
+  OJ none;
+  V w12 = T::Local(t1, t2);
   EXPECT(assert_equal(t2, T::Retract(t1,w12, H1, H2)));
-  EXPECT(assert_equal(numericalDerivative21(T::Retract, t1, w12), H1));
-  EXPECT(assert_equal(numericalDerivative22(T::Retract, t1, w12), H2));
+  EXPECT(assert_equal(numericalDerivative41<G,G,V,OJ,OJ>(T::Retract, t1, w12, none, none), H1));
+  EXPECT(assert_equal(numericalDerivative42<G,G,V,OJ,OJ>(T::Retract, t1, w12, none, none), H2));
 
   // Local
   EXPECT(assert_equal(w12, t1.localCoordinates(t2, H1, H2)));
-  EXPECT(assert_equal(numericalDerivative21(T::Local, t1, t2), H1));
-  EXPECT(assert_equal(numericalDerivative22(T::Local, t1, t2), H2));
+  EXPECT(assert_equal(numericalDerivative41<V,G,G,OJ,OJ>(T::Local, t1, t2, none, none), H1));
+  EXPECT(assert_equal(numericalDerivative42<V,G,G,OJ,OJ>(T::Local, t1, t2, none, none), H2));
 }
 } // namespace gtsam
 
