2d98056497aac3fa467a7cc41f8b8a7745ff6deb
Frank Dellaert <frank@skyd.io>
2015-06-07 20:24:45 -0700

    organize headers

--- gtsam/slam/dataset.cpp
+++ gtsam/slam/dataset.cpp
@@ -15,19 +15,34 @@
  * @brief utility functions for loading datasets
  */
 
-#include <gtsam/slam/dataset.h>
-#include <gtsam/slam/BetweenFactor.h>
 #include <gtsam/slam/BearingRangeFactor.h>
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/slam/dataset.h>
+#include <gtsam/geometry/Point3.h>
 #include <gtsam/geometry/Pose2.h>
-#include <gtsam/geometry/Pose3.h>
-#include <gtsam/linear/Sampler.h>
+#include <gtsam/geometry/Rot3.h>
+#include <gtsam/inference/FactorGraph.h>
 #include <gtsam/inference/Symbol.h>
+#include <gtsam/nonlinear/NonlinearFactor.h>
+#include <gtsam/nonlinear/Values.h>
+#include <gtsam/nonlinear/Values-inl.h>
+#include <gtsam/linear/Sampler.h>
+#include <gtsam/base/GenericValue.h>
+#include <gtsam/base/Lie.h>
+#include <gtsam/base/Matrix.h>
+#include <gtsam/base/types.h>
+#include <gtsam/base/Value.h>
+#include <gtsam/base/Vector.h>
 
-#include <boost/filesystem.hpp>
+#include <boost/assign/list_inserter.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/foreach.hpp>
 
+#include <cmath>
 #include <fstream>
-#include <sstream>
-#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
 
 using namespace std;
 namespace fs = boost::filesystem;
