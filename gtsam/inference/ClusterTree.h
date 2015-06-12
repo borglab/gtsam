692ddd8f361ad957f4adf7738a19ef41acdcbf47
Frank Dellaert <frank@skyd.io>
2015-06-11 23:38:57 -0700

    Made node keys ordered to avoid copy constructor in eliminate

--- gtsam/inference/ClusterTree.h
+++ gtsam/inference/ClusterTree.h
@@ -11,7 +11,7 @@
 
 #include <gtsam/base/Testable.h>
 #include <gtsam/base/FastVector.h>
-#include <gtsam/inference/Key.h>
+#include <gtsam/inference/Ordering.h>
 
 namespace gtsam
 {
@@ -37,7 +37,7 @@ namespace gtsam
     typedef typename FactorGraphType::Eliminate Eliminate; ///< Typedef for an eliminate subroutine
 
     struct Cluster {
-      typedef FastVector<Key> Keys;
+      typedef Ordering Keys;
       typedef FastVector<sharedFactor> Factors;
       typedef FastVector<boost::shared_ptr<Cluster> > Children;
 
