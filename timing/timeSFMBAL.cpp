2b9e9ae9787921ac4c8bd93597d2a515ab6e3584
dellaert <dellaert@cc.gatech.edu>
2015-06-21 18:20:12 -0700

    Turned off AutoDiff by default

--- timing/timeSFMBAL.cpp
+++ timing/timeSFMBAL.cpp
@@ -34,7 +34,7 @@
 using namespace std;
 using namespace gtsam;
 
-//#define USE_GTSAM_FACTOR
+#define USE_GTSAM_FACTOR
 #ifdef USE_GTSAM_FACTOR
 #include <gtsam/slam/GeneralSFMFactor.h>
 #include <gtsam/geometry/Cal3Bundler.h>
