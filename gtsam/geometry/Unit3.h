440a9557101e33e0dfad8dd896376b0f99d98bd1
dellaert <dellaert@cc.gatech.edu>
2015-07-03 11:33:30 -0700

    Added missing includes

--- gtsam/geometry/Unit3.h
+++ gtsam/geometry/Unit3.h
@@ -20,11 +20,16 @@
 
 #pragma once
 
-#include <gtsam/base/Manifold.h>
 #include <gtsam/geometry/Point3.h>
+#include <gtsam/base/Manifold.h>
+#include <gtsam/base/Matrix.h>
+#include <gtsam/dllexport.h>
 
-#include <boost/random/mersenne_twister.hpp>
 #include <boost/optional.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/serialization/nvp.hpp>
+
+#include <string>
 
 namespace gtsam {
 
