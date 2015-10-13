702c73fc758255d25c2988e42d489ef249758d3d
cbeall3 <cbeall3@gatech.edu>
2015-10-13 13:30:24 -0400

    Patch Memory.h

--- gtsam/3rdparty/Eigen/Eigen/src/Core/util/Memory.h
+++ gtsam/3rdparty/Eigen/Eigen/src/Core/util/Memory.h
@@ -630,6 +630,8 @@ template<typename T> class aligned_stack_memory_handler
       } \
       void operator delete(void * ptr) throw() { Eigen::internal::conditional_aligned_free<NeedsToAlign>(ptr); } \
       void operator delete[](void * ptr) throw() { Eigen::internal::conditional_aligned_free<NeedsToAlign>(ptr); } \
+      void operator delete(void * ptr, std::size_t /* sz */) throw() { Eigen::internal::conditional_aligned_free<NeedsToAlign>(ptr); } \
+      void operator delete[](void * ptr, std::size_t /* sz */) throw() { Eigen::internal::conditional_aligned_free<NeedsToAlign>(ptr); } \
       /* in-place new and delete. since (at least afaik) there is no actual   */ \
       /* memory allocated we can safely let the default implementation handle */ \
       /* this particular case. */ \
