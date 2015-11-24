92bfcaa00488d36cd0d319b1a52e0b71f603919d
Ellon Mendes <ellon.mendes@laas.fr>
2015-11-24 11:30:35 +0100

    Wrap some properties of ISAM2Params to python

--- python/handwritten/nonlinear/ISAM2.cpp
+++ python/handwritten/nonlinear/ISAM2.cpp
@@ -30,6 +30,21 @@ void exportISAM2(){
 
 // TODO(Ellon): Export all properties of ISAM2Params
 class_<ISAM2Params>("ISAM2Params")
+  .add_property("relinearize_skip", &ISAM2Params::getRelinearizeSkip, &ISAM2Params::setRelinearizeSkip)
+  .add_property("enable_relinearization", &ISAM2Params::isEnableRelinearization, &ISAM2Params::setEnableRelinearization)
+  .add_property("evaluate_non_linear_error", &ISAM2Params::isEvaluateNonlinearError, &ISAM2Params::setEvaluateNonlinearError)
+  .add_property("factorization", &ISAM2Params::getFactorization, &ISAM2Params::setFactorization)
+  .add_property("cache_linearized_factors", &ISAM2Params::isCacheLinearizedFactors, &ISAM2Params::setCacheLinearizedFactors)
+  .add_property("enable_detailed_results", &ISAM2Params::isEnableDetailedResults, &ISAM2Params::setEnableDetailedResults)
+  .add_property("enable_partial_linearization_check", &ISAM2Params::isEnablePartialRelinearizationCheck, &ISAM2Params::setEnablePartialRelinearizationCheck)
+  // TODO(Ellon): Check if it works with FastMap; Implement properly if it doesn't.
+  .add_property("relinearization_threshold", &ISAM2Params::getRelinearizeThreshold, &ISAM2Params::setRelinearizeThreshold)
+  // TODO(Ellon): Wrap the following setters/getters:
+  //     void 	setOptimizationParams (OptimizationParams optimizationParams)
+  //     OptimizationParams 	getOptimizationParams () const
+  //     void 	setKeyFormatter (KeyFormatter keyFormatter)
+  //     KeyFormatter 	getKeyFormatter () const
+  //     GaussianFactorGraph::Eliminate 	getEliminationFunction () const 
 ;
 
 // TODO(Ellon): Export useful methods/properties of ISAM2Result
