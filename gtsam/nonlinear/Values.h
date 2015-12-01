6ea06445590c8f00d3b3dca5210e83060cf75444
cbeall3 <cbeall3@gatech.edu>
2015-12-01 10:39:23 -0500

    Smallest commit ever to properly shut up warnings! Wunused-local-typedef -> Wunused-local-typedefs

--- gtsam/nonlinear/Values.h
+++ gtsam/nonlinear/Values.h
@@ -32,7 +32,7 @@
 #ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-variable"
-#pragma GCC diagnostic ignored "-Wunused-local-typedef"
+#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
 #endif
 #include <boost/bind.hpp>
 #ifdef __GNUC__
