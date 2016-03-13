ae19e078c7849a86b1a05638813742e7c7ec822a
Alex Hagiopol <alexhagiopol@gmail.com>
2016-03-12 20:44:05 -0500

    Changed argument and implementation of correct() in Mechanization_bRn2

--- gtsam_unstable/slam/Mechanization_bRn2.cpp
+++ gtsam_unstable/slam/Mechanization_bRn2.cpp
@@ -66,14 +66,14 @@ Mechanization_bRn2 Mechanization_bRn2::initialize(const Matrix& U,
 }
 
 /* ************************************************************************* */
-Mechanization_bRn2 Mechanization_bRn2::correct(const Vector3& dx) const {
-  Vector3 rho = dx.segment(0,3);
+Mechanization_bRn2 Mechanization_bRn2::correct(const Vector9& dx) const {
+  Vector3 rho = dx.segment<3>(0);
 
   Rot3 delta_nRn = Rot3::Rodrigues(rho);
   Rot3 bRn = bRn_ * delta_nRn;
 
-  Vector3 x_g = x_g_ + dx.segment(3,3);
-  Vector3 x_a = x_a_ + dx.segment(6,3);
+  Vector3 x_g = x_g_ + dx.segment<3>(3);
+  Vector3 x_a = x_a_ + dx.segment<3>(6);
 
   return Mechanization_bRn2(bRn, x_g, x_a);
 }
