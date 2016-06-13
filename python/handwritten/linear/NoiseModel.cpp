62e0cb661311f0676aace1e0e670df9b7af670c3
Ellon Mendes <ellon.mendes@laas.fr>
2016-06-13 15:13:49 +0200

    [python] Put the macro into a separated file (python/handwritten/common.h)

--- python/handwritten/linear/NoiseModel.cpp
+++ python/handwritten/linear/NoiseModel.cpp
@@ -28,19 +28,7 @@
 
 #include "gtsam/linear/NoiseModel.h"
 
-/* Fix to avoid registration warnings */
-// Solution taken from https://github.com/BVLC/caffe/pull/4069/commits/673e8cfc0b8f05f9fa3ebbad7cc6202822e5d9c5 
-#define REGISTER_SHARED_PTR_TO_PYTHON(PTR) do { \
-  const boost::python::type_info info = \
-    boost::python::type_id<boost::shared_ptr<PTR > >(); \
-  const boost::python::converter::registration* reg = \
-    boost::python::converter::registry::query(info); \
-  if (reg == NULL) { \
-    boost::python::register_ptr_to_python<boost::shared_ptr<PTR > >(); \
-  } else if ((*reg).m_to_python == NULL) { \
-    boost::python::register_ptr_to_python<boost::shared_ptr<PTR > >(); \
-  } \
-} while (0)
+#include "python/handwritten/common.h"
 
 using namespace boost::python;
 using namespace gtsam;
