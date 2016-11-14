a18f11097c33ff80e0ef39d348f9ee7d3263eb51
Duy-Nguyen Ta <thduynguyen@gmail.com>
2016-11-13 23:58:11 -0500

    format

--- wrap/MethodBase.cpp
+++ wrap/MethodBase.cpp
@@ -151,9 +151,11 @@ void MethodBase::emit_cython_pyx_function_call(FileWriter& file,
     returnVals_[iOverload].emit_cython_pyx_return_type(file);
     file.oss << " ret = ";
   }
-  if (!returnVals_[iOverload].isPair && !returnVals_[iOverload].type1.isPtr && returnVals_[iOverload].type1.isNonBasicType())
+  if (!returnVals_[iOverload].isPair && !returnVals_[iOverload].type1.isPtr &&
+      returnVals_[iOverload].type1.isNonBasicType()) {
     file.oss << returnVals_[iOverload].type1.pyxSharedCythonClass() << "(new "
-  << returnVals_[iOverload].type1.pyxCythonClass() << "(";
+             << returnVals_[iOverload].type1.pyxCythonClass() << "(";
+  }
 
   //... function call
   file.oss << caller << "." << funcName;
@@ -162,7 +164,8 @@ void MethodBase::emit_cython_pyx_function_call(FileWriter& file,
   argumentList(iOverload).emit_cython_pyx_asParams(file);
   file.oss << ")";
 
-  if (!returnVals_[iOverload].isPair && !returnVals_[iOverload].type1.isPtr && returnVals_[iOverload].type1.isNonBasicType())
+  if (!returnVals_[iOverload].isPair && !returnVals_[iOverload].type1.isPtr &&
+      returnVals_[iOverload].type1.isNonBasicType())
     file.oss << "))";
   file.oss << "\n";
 
