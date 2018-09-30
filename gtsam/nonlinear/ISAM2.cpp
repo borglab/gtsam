d401edc9175cee01c5307263c0ac60523ca1ec2f
Frank Dellaert <dellaert@cc.gatech.edu>
2018-09-30 12:58:48 -0400

    Split ISAM2 Clique, Params, and Result out in different files, cleaned up headers, and removed ISAM2-inl.h header

--- gtsam/nonlinear/ISAM2.cpp
+++ gtsam/nonlinear/ISAM2.cpp
@@ -10,22 +10,21 @@
  * -------------------------------------------------------------------------- */
 
 /**
- * @file    ISAM2-inl.h
+ * @file    ISAM2.cpp
  * @brief   Incremental update functionality (ISAM2) for BayesTree, with fluid
  * relinearization.
- * @author  Michael Kaess, Richard Roberts
+ * @author  Michael Kaess, Richard Roberts, Frank Dellaert
  */
 
 #include <gtsam/nonlinear/ISAM2.h>
 
-#include <algorithm>
-#include <map>
-#include <stack>
-#include <utility>
+#include <gtsam/base/debug.h>
+#include <gtsam/base/timing.h>
+#include <gtsam/inference/BayesTree-inst.h>
+#include <gtsam/inference/JunctionTree-inst.h>  // We need the inst file because we'll make a special JT templated on ISAM2
+#include <gtsam/linear/GaussianEliminationTree.h>
+#include <gtsam/nonlinear/LinearContainerFactor.h>
 
-#include <boost/assign/std/list.hpp>  // for operator +=
-using namespace boost::assign;
-#include <boost/algorithm/string.hpp>
 #include <boost/range/adaptors.hpp>
 #include <boost/range/algorithm/copy.hpp>
 namespace br {
@@ -33,30 +32,19 @@ using namespace boost::range;
 using namespace boost::adaptors;
 }  // namespace br
 
-#include <gtsam/base/debug.h>
-#include <gtsam/base/timing.h>
-#include <gtsam/inference/BayesTree-inst.h>
-#include <gtsam/inference/BayesTreeCliqueBase-inst.h>
-#include <gtsam/inference/JunctionTree-inst.h>  // We need the inst file because we'll make a special JT templated on ISAM2
-#include <gtsam/linear/GaussianEliminationTree.h>
-#include <gtsam/linear/GaussianFactorGraph.h>
-#include <gtsam/linear/HessianFactor.h>
-#include <gtsam/linear/linearAlgorithms-inst.h>
-
-#include <gtsam/nonlinear/DoglegOptimizerImpl.h>
-#include <gtsam/nonlinear/LinearContainerFactor.h>
-#include <gtsam/nonlinear/nonlinearExceptions.h>
+#include <algorithm>
+#include <map>
+#include <utility>
 
 using namespace std;
 
 namespace gtsam {
 
-// Instantiate base classes
-template class BayesTreeCliqueBase<ISAM2Clique, GaussianFactorGraph>;
+// Instantiate base class
 template class BayesTree<ISAM2Clique>;
 
-static const bool disableReordering = false;
-static const double batchThreshold = 0.65;
+static const bool kDisableReordering = false;
+static const double kBatchThreshold = 0.65;
 
 /* ************************************************************************* */
 // Special BayesTree class that uses ISAM2 cliques - this is the result of
@@ -80,344 +68,9 @@ class ISAM2JunctionTree
   typedef ISAM2JunctionTree This;
   typedef boost::shared_ptr<This> shared_ptr;
 
-  ISAM2JunctionTree(const GaussianEliminationTree& eliminationTree)
+  explicit ISAM2JunctionTree(const GaussianEliminationTree& eliminationTree)
       : Base(eliminationTree) {}
 };
-
-/* ************************************************************************* */
-string ISAM2DoglegParams::adaptationModeTranslator(
-    const DoglegOptimizerImpl::TrustRegionAdaptationMode& adaptationMode)
-    const {
-  string s;
-  switch (adaptationMode) {
-    case DoglegOptimizerImpl::SEARCH_EACH_ITERATION:
-      s = "SEARCH_EACH_ITERATION";
-      break;
-    case DoglegOptimizerImpl::ONE_STEP_PER_ITERATION:
-      s = "ONE_STEP_PER_ITERATION";
-      break;
-    default:
-      s = "UNDEFINED";
-      break;
-  }
-  return s;
-}
-
-/* ************************************************************************* */
-DoglegOptimizerImpl::TrustRegionAdaptationMode
-ISAM2DoglegParams::adaptationModeTranslator(
-    const string& adaptationMode) const {
-  string s = adaptationMode;
-  boost::algorithm::to_upper(s);
-  if (s == "SEARCH_EACH_ITERATION")
-    return DoglegOptimizerImpl::SEARCH_EACH_ITERATION;
-  if (s == "ONE_STEP_PER_ITERATION")
-    return DoglegOptimizerImpl::ONE_STEP_PER_ITERATION;
-
-  /* default is SEARCH_EACH_ITERATION */
-  return DoglegOptimizerImpl::SEARCH_EACH_ITERATION;
-}
-
-/* ************************************************************************* */
-ISAM2Params::Factorization ISAM2Params::factorizationTranslator(
-    const string& str) {
-  string s = str;
-  boost::algorithm::to_upper(s);
-  if (s == "QR") return ISAM2Params::QR;
-  if (s == "CHOLESKY") return ISAM2Params::CHOLESKY;
-
-  /* default is CHOLESKY */
-  return ISAM2Params::CHOLESKY;
-}
-
-/* ************************************************************************* */
-string ISAM2Params::factorizationTranslator(
-    const ISAM2Params::Factorization& value) {
-  string s;
-  switch (value) {
-    case ISAM2Params::QR:
-      s = "QR";
-      break;
-    case ISAM2Params::CHOLESKY:
-      s = "CHOLESKY";
-      break;
-    default:
-      s = "UNDEFINED";
-      break;
-  }
-  return s;
-}
-
-/* ************************************************************************* */
-void ISAM2Clique::setEliminationResult(
-    const FactorGraphType::EliminationResult& eliminationResult) {
-  conditional_ = eliminationResult.first;
-  cachedFactor_ = eliminationResult.second;
-  // Compute gradient contribution
-  gradientContribution_.resize(conditional_->cols() - 1);
-  // Rewrite -(R * P')'*d   as   -(d' * R * P')'   for computational speed
-  // reasons
-  gradientContribution_ << -conditional_->get_R().transpose() *
-                               conditional_->get_d(),
-      -conditional_->get_S().transpose() * conditional_->get_d();
-}
-
-/* ************************************************************************* */
-bool ISAM2Clique::equals(const This& other, double tol) const {
-  return Base::equals(other) &&
-         ((!cachedFactor_ && !other.cachedFactor_) ||
-          (cachedFactor_ && other.cachedFactor_ &&
-           cachedFactor_->equals(*other.cachedFactor_, tol)));
-}
-
-/* ************************************************************************* */
-void ISAM2Clique::print(const string& s, const KeyFormatter& formatter) const {
-  Base::print(s, formatter);
-  if (cachedFactor_)
-    cachedFactor_->print(s + "Cached: ", formatter);
-  else
-    cout << s << "Cached empty" << endl;
-  if (gradientContribution_.rows() != 0)
-    gtsam::print(gradientContribution_, "Gradient contribution: ");
-}
-
-/* ************************************************************************* */
-bool ISAM2Clique::isDirty(const KeySet& replaced, const KeySet& changed) const {
-  // if none of the variables in this clique (frontal and separator!) changed
-  // significantly, then by the running intersection property, none of the
-  // cliques in the children need to be processed
-
-  // Are any clique variables part of the tree that has been redone?
-  bool dirty = replaced.exists(conditional_->frontals().front());
-#if !defined(NDEBUG) && defined(GTSAM_EXTRA_CONSISTENCY_CHECKS)
-  for (Key frontal : conditional_->frontals()) {
-    assert(dirty == replaced.exists(frontal));
-  }
-#endif
-
-  // If not, then has one of the separator variables changed significantly?
-  if (!dirty) {
-    for (Key parent : conditional_->parents()) {
-      if (changed.exists(parent)) {
-        dirty = true;
-        break;
-      }
-    }
-  }
-  return dirty;
-}
-
-/* ************************************************************************* */
-/**
- * Back-substitute - special version stores solution pointers in cliques for
- * fast access.
- */
-void ISAM2Clique::fastBackSubstitute(VectorValues* delta) const {
-#ifdef USE_BROKEN_FAST_BACKSUBSTITUTE
-  // TODO(gareth): This code shares a lot of logic w/ linearAlgorithms-inst,
-  // potentially refactor
-
-  // Create solution part pointers if necessary and possible - necessary if
-  // solnPointers_ is empty, and possible if either we're a root, or we have
-  // a parent with valid solnPointers_.
-  ISAM2::sharedClique parent = parent_.lock();
-  if (solnPointers_.empty() && (isRoot() || !parent->solnPointers_.empty())) {
-    for (Key frontal : conditional_->frontals())
-      solnPointers_.emplace(frontal, delta->find(frontal));
-    for (Key parentKey : conditional_->parents()) {
-      assert(parent->solnPointers_.exists(parentKey));
-      solnPointers_.emplace(parentKey, parent->solnPointers_.at(parentKey));
-    }
-  }
-
-  // See if we can use solution part pointers - we can if they either
-  // already existed or were created above.
-  if (!solnPointers_.empty()) {
-    GaussianConditional& c = *conditional_;
-    // Solve matrix
-    Vector xS;
-    {
-      // Count dimensions of vector
-      DenseIndex dim = 0;
-      FastVector<VectorValues::const_iterator> parentPointers;
-      parentPointers.reserve(conditional_->nrParents());
-      for (Key parent : conditional_->parents()) {
-        parentPointers.push_back(solnPointers_.at(parent));
-        dim += parentPointers.back()->second.size();
-      }
-
-      // Fill parent vector
-      xS.resize(dim);
-      DenseIndex vectorPos = 0;
-      for (const VectorValues::const_iterator& parentPointer : parentPointers) {
-        const Vector& parentVector = parentPointer->second;
-        xS.block(vectorPos, 0, parentVector.size(), 1) =
-            parentVector.block(0, 0, parentVector.size(), 1);
-        vectorPos += parentVector.size();
-      }
-    }
-
-    // NOTE(gareth): We can no longer write: xS = b - S * xS
-    // This is because Eigen (as of 3.3) no longer evaluates S * xS into
-    // a temporary, and the operation trashes valus in xS.
-    // See: http://eigen.tuxfamily.org/index.php?title=3.3
-    const Vector rhs = c.getb() - c.get_S() * xS;
-    const Vector solution = c.get_R().triangularView<Eigen::Upper>().solve(rhs);
-
-    // Check for indeterminant solution
-    if (solution.hasNaN())
-      throw IndeterminantLinearSystemException(c.keys().front());
-
-    // Insert solution into a VectorValues
-    DenseIndex vectorPosition = 0;
-    for (GaussianConditional::const_iterator frontal = c.beginFrontals();
-         frontal != c.endFrontals(); ++frontal) {
-      solnPointers_.at(*frontal)->second =
-          solution.segment(vectorPosition, c.getDim(frontal));
-      vectorPosition += c.getDim(frontal);
-    }
-  } else {
-    // Just call plain solve because we couldn't use solution pointers.
-    delta->update(conditional_->solve(*delta));
-  }
-#else
-  delta->update(conditional_->solve(*delta));
-#endif
-}
-
-/* ************************************************************************* */
-bool ISAM2Clique::valuesChanged(const KeySet& replaced,
-                                const Vector& originalValues,
-                                const VectorValues& delta,
-                                double threshold) const {
-  auto frontals = conditional_->frontals();
-  if (replaced.exists(frontals.front())) return true;
-  auto diff = originalValues - delta.vector(frontals);
-  return diff.lpNorm<Eigen::Infinity>() >= threshold;
-}
-
-/* ************************************************************************* */
-/// Set changed flag for each frontal variable
-void ISAM2Clique::markFrontalsAsChanged(KeySet* changed) const {
-  for (Key frontal : conditional_->frontals()) {
-    changed->insert(frontal);
-  }
-}
-
-/* ************************************************************************* */
-void ISAM2Clique::restoreFromOriginals(const Vector& originalValues,
-                                       VectorValues* delta) const {
-  size_t pos = 0;
-  for (Key frontal : conditional_->frontals()) {
-    auto v = delta->at(frontal);
-    v = originalValues.segment(pos, v.size());
-    pos += v.size();
-  }
-}
-
-/* ************************************************************************* */
-// Note: not being used right now in favor of non-recursive version below.
-void ISAM2Clique::optimizeWildfire(const KeySet& replaced, double threshold,
-                                   KeySet* changed, VectorValues* delta,
-                                   size_t* count) const {
-  if (isDirty(replaced, *changed)) {
-    // Temporary copy of the original values, to check how much they change
-    auto originalValues = delta->vector(conditional_->frontals());
-
-    // Back-substitute
-    fastBackSubstitute(delta);
-    count += conditional_->nrFrontals();
-
-    if (valuesChanged(replaced, originalValues, *delta, threshold)) {
-      markFrontalsAsChanged(changed);
-    } else {
-      restoreFromOriginals(originalValues, delta);
-    }
-
-    // Recurse to children
-    for (const auto& child : children) {
-      child->optimizeWildfire(replaced, threshold, changed, delta, count);
-    }
-  }
-}
-
-size_t optimizeWildfire(const ISAM2::sharedClique& root, double threshold,
-                        const KeySet& keys, VectorValues* delta) {
-  KeySet changed;
-  size_t count = 0;
-  // starting from the root, call optimize on each conditional
-  if (root) root->optimizeWildfire(keys, threshold, &changed, delta, &count);
-  return count;
-}
-
-/* ************************************************************************* */
-bool ISAM2Clique::optimizeWildfireNode(const KeySet& replaced, double threshold,
-                                       KeySet* changed, VectorValues* delta,
-                                       size_t* count) const {
-  // TODO(gareth): This code shares a lot of logic w/ linearAlgorithms-inst,
-  // potentially refactor
-  bool dirty = isDirty(replaced, *changed);
-  if (dirty) {
-    // Temporary copy of the original values, to check how much they change
-    auto originalValues = delta->vector(conditional_->frontals());
-
-    // Back-substitute
-    fastBackSubstitute(delta);
-    count += conditional_->nrFrontals();
-
-    if (valuesChanged(replaced, originalValues, *delta, threshold)) {
-      markFrontalsAsChanged(changed);
-    } else {
-      restoreFromOriginals(originalValues, delta);
-    }
-  }
-
-  return dirty;
-}
-
-size_t optimizeWildfireNonRecursive(const ISAM2::sharedClique& root,
-                                    double threshold, const KeySet& keys,
-                                    VectorValues* delta) {
-  KeySet changed;
-  size_t count = 0;
-
-  if (root) {
-    std::stack<ISAM2::sharedClique> travStack;
-    travStack.push(root);
-    ISAM2::sharedClique currentNode = root;
-    while (!travStack.empty()) {
-      currentNode = travStack.top();
-      travStack.pop();
-      bool dirty = currentNode->optimizeWildfireNode(keys, threshold, &changed,
-                                                     delta, &count);
-      if (dirty) {
-        for (const auto& child : currentNode->children) {
-          travStack.push(child);
-        }
-      }
-    }
-  }
-
-  return count;
-}
-
-/* ************************************************************************* */
-void ISAM2Clique::nnz_internal(size_t* result) const {
-  size_t dimR = conditional_->rows();
-  size_t dimSep = conditional_->get_S().cols();
-  *result += ((dimR + 1) * dimR) / 2 + dimSep * dimR;
-  // traverse the children
-  for (const auto& child : children) {
-    child->nnz_internal(result);
-  }
-}
-
-/* ************************************************************************* */
-size_t ISAM2Clique::calculate_nnz() const {
-  size_t result = 0;
-  nnz_internal(&result);
-  return result;
-}
 
 /* ************************************************************************* */
 ISAM2::ISAM2(const ISAM2Params& params) : params_(params), update_count_(0) {
@@ -622,7 +275,7 @@ boost::shared_ptr<KeySet> ISAM2::recalculate(
   boost::shared_ptr<KeySet> affectedKeysSet(
       new KeySet());  // Will return this result
 
-  if (affectedKeys.size() >= theta_.size() * batchThreshold) {
+  if (affectedKeys.size() >= theta_.size() * kBatchThreshold) {
     // Do a batch step - reorder and relinearize all variables
     gttic(batch);
 
@@ -867,7 +520,7 @@ ISAM2Result ISAM2::update(
   // Update delta if we need it to check relinearization later
   if (relinearizeThisStep) {
     gttic(updateDelta);
-    updateDelta(disableReordering);
+    updateDelta(kDisableReordering);
     gttoc(updateDelta);
   }
 
@@ -982,7 +635,7 @@ ISAM2Result ISAM2::update(
     else
       relinKeys =
           Impl::CheckRelinearizationFull(delta_, params_.relinearizeThreshold);
-    if (disableReordering)
+    if (kDisableReordering)
       relinKeys = Impl::CheckRelinearizationFull(
           delta_, 0.0);  // This is used for debugging
 
@@ -1485,4 +1138,3 @@ VectorValues ISAM2::gradientAtZero() const {
 }
 
 }  // namespace gtsam
-/// namespace gtsam
