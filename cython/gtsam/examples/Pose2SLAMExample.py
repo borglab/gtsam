16f8fb031a6d9c4c336adfecba462992e7f8a30c
Frank Dellaert <dellaert@gmail.com>
2018-10-14 12:11:33 -0400

    Updated copyright

--- cython/gtsam/examples/Pose2SLAMExample.py
+++ cython/gtsam/examples/Pose2SLAMExample.py
@@ -1,5 +1,5 @@
 """
-GTSAM Copyright 2010, Georgia Tech Research Corporation,
+GTSAM Copyright 2010-2018, Georgia Tech Research Corporation,
 Atlanta, Georgia 30332-0415
 All Rights Reserved
 Authors: Frank Dellaert, et al. (see THANKS for the full author list)
