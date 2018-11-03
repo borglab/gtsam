142797723067a765c53378b6440aa4bb6d04be1b
Frank Dellaert <dellaert@cc.gatech.edu>
2018-11-03 10:20:31 -0400

    Undo Eigen change (needed to remove GTSAM_EXPORT in SO3 to do this)

--- gtsam/3rdparty/Eigen/Eigen/src/Core/PlainObjectBase.h
+++ gtsam/3rdparty/Eigen/Eigen/src/Core/PlainObjectBase.h
@@ -431,7 +431,7 @@ class PlainObjectBase : public internal::dense_xpr_base<Derived>::type
     EIGEN_DEVICE_FUNC
     EIGEN_STRONG_INLINE void conservativeResize(Index size)
     {
-      internal::conservative_resize_like_impl<Derived>::run(*this, size, 1);
+      internal::conservative_resize_like_impl<Derived>::run(*this, size);
     }
 
     /** Resizes the matrix to \a rows x \a cols of \c other, while leaving old values untouched.
