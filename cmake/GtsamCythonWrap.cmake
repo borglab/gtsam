09ac7f7c069a0c5f6d53d98b87a3e36e8c65d5e9
Matthew Broadway <matthew.broadway@navenio.com>
2019-02-13 09:55:09 +0000

    removed requirement for python 2.7 in cmake

--- cmake/GtsamCythonWrap.cmake
+++ cmake/GtsamCythonWrap.cmake
@@ -31,7 +31,7 @@ endfunction()
 
 function(set_up_required_cython_packages)
   # Set up building of cython module
-  find_package(PythonLibs 2.7 REQUIRED)
+  find_package(PythonLibs REQUIRED)
   include_directories(${PYTHON_INCLUDE_DIRS})
   find_package(NumPy REQUIRED)
   include_directories(${NUMPY_INCLUDE_DIRS})
