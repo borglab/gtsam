b7ee7e8db750f04ba35ea51e4c5729c062e6573f
Matthew Sklar <matthewsklar227@gmail.com>
2019-03-13 18:02:46 -0400

    Cleanup using lint

--- wrap/matlab_wrapper.py
+++ wrap/matlab_wrapper.py
@@ -1,25 +1,28 @@
-"""
-Matlab wrapper
-Author: Matthew Sklar
-Date: March 2019
-"""
-
 import os
 import argparse
 
 import interface_parser as parser
 import template_instantiator as instantiator
 
-test_dir = "wrap/tests/"
-
 
 class MatlabWrapper(object):
-    # TODO: Store text instead of outputting it
+    """ Wrap the given C++ code into Matlab.
+
+    Keyword arguments:
+    module -- the C++ module being wrapped
+    module_name -- name of the C++ module being wrapped
+    top_module_namespace -- C++ namespace for the top module (default '')
+    ignore_classes -- A list of classes to ignore (default [])
+    """
+
     # TODO: Define more types
     # TODO: May only need this for constructors
     data_type = {'int': 'numeric', 'double': 'double'}
+
     # TODO: Figure out what this number is
     val = 85
+
+    content = []
 
     def __init__(self, module, module_name, top_module_namespace='',
                  ignore_classes=[]):
@@ -27,21 +30,6 @@ class MatlabWrapper(object):
         self.module_name = module_name
         self.top_module_namespace = top_module_namespace
         self.ignore_classe = ignore_classes
-
-        """ Generate the file starting comment for the given namespace """
-        pass
-
-    def _write_file(self, filename, content):
-        """ Write the content to the specified file. """
-        with open(test_dir + 'actual-matlab/' + filename, 'w') as f:
-            f.write(content)
-
-    def _mkdir(self, folder_name):
-        """ Make a folder in the actual-matlab folder if it does not exist """
-        folder_path = test_dir + 'actual-matlab/' + folder_name
-
-        if not os.path.exists(folder_path):
-            os.mkdir(folder_path)
 
     def _partial_match(self, namespace1, namespace2):
         """ Return if one array of namespaces is a subset of the other """
@@ -53,14 +41,13 @@ class MatlabWrapper(object):
 
     def _group_methods(self, methods):
         """ Group overloaded methods together """
-        # Map method name to its index in the output
         method_map = {}
         method_out = []
 
         for method in methods:
             method_index = method_map.get(method.name)
 
-            if method_index == None:
+            if method_index is None:
                 method_map[method.name] = len(method_out)
                 method_out.append([method])
             else:
@@ -69,15 +56,26 @@ class MatlabWrapper(object):
         return method_out
 
     def _clean_class_name(self, instantiated_class):
-        """ Reformated the C++ class name to fit Matlab defined naming standards """
+        """ Reformated the C++ class name to fit Matlab defined naming
+        standards """
         if len(instantiated_class.ctors) != 0:
             return instantiated_class.ctors[0].name
 
-        # TODO: May have to group together same name templates if they don't have constructors
-        # TODO: This would require another if statement if text before < is a duplicate method
+        """ TODO: May have to group together same name templates if they don't
+        have constructors. This would require another if statement if text
+        before < is a duplicate method
+        """
         return instantiated_class.cpp_class()
 
     def class_comment(self, class_name, ctors, methods):
+        """ Generate comments for the given class in Matlab.
+
+        Keyword arguments:
+        class_name -- the name of the class being wrapped
+        ctors -- a list of the constructors in the class
+        methods -- a list of the methods in the class
+        """
+
         comment = '%class {class_name}, see Doxygen page for details\n'\
             '%at http://research.cc.gatech.edu/borg/sites/edu.borg/html/index.html\n'\
             '%\n'\
@@ -115,7 +113,7 @@ class MatlabWrapper(object):
                 return_type=method.return_type.type1.typename.name)
 
         comment += '%\n'
-        
+
         # TODO: Support more method types
 
         return comment
@@ -151,29 +149,33 @@ class MatlabWrapper(object):
                 param_check += str(len(p.args_list))
 
                 for num, arg in enumerate(p.args_list):
-                    param_check += " && isa(varargin{open}{num}{close},'{data_type}')".format(
-                        open='{', num=num + 1, close='}', data_type=self.data_type[str(arg.ctype).strip()])
+                    param_check += " && isa(varargin{open}{num}{close},"\
+                        "'{data_type}')".format(
+                            open='{', num=num + 1, close='}',
+                            data_type=self.data_type[str(arg.ctype).strip()])
 
                 param_check += '\n'
 
             # TODO: Figure out what number to put at val
-            param_check += '        varargout{open}1{close} = {module_name}_wrapper({val}, varargin{open}:{close});\n'.format(
-                open='{', close='}', module_name=self.module_name, val=self.val)
+            param_check += '        varargout{open}1{close} = {module_name}'\
+                '_wrapper({val}, varargin{open}:{close});\n'.format(
+                    open='{', close='}', module_name=self.module_name,
+                    val=self.val)
             self.val += 1
 
         param_check += '      else\n'\
-            "        error('Arguments do not match any overload of function {func_name}');".format(
-                func_name=m_method)
+            "        error('Arguments do not match any overload of function "\
+            "{func_name}');".format(func_name=m_method)
 
         return 'function varargout = {m_method}(varargin)\n'\
             '{statements}\n'\
-            '      end\n'.format(
-                m_method=m_method, statements=param_check)
+            '      end\n'.format(m_method=m_method, statements=param_check)
 
     def wrap_methods(self, methods, globals=False):
-        """ 
-        Wrap a sequence of methods. Groups methods with the same names 
-        together. If globals is True then output every method into its own file.
+        """
+        Wrap a sequence of methods. Groups methods with the same names
+        together. If globals is True then output every method into its own
+        file.
         """
         output = ''
 
@@ -182,7 +184,7 @@ class MatlabWrapper(object):
         for method in methods:
             if globals:
                 method_text = self.wrap_global_method(method)
-                self._write_file(method[0].name + '.m', method_text)
+                self.content.append((method[0].name + '.m', method_text))
             else:
                 method_text = self.wrap_method(method)
                 output += ''
@@ -190,20 +192,20 @@ class MatlabWrapper(object):
         return output
 
     def wrap_instantiated_class(self, instantiated_class):
-        """ Wrap classes """
+        """ Generate comments and code for given class """
         file_name = self._clean_class_name(instantiated_class)
 
-        # TODO: Create and output content
-        content = self.class_comment(
+        content_text = self.class_comment(
             file_name, instantiated_class.ctors, instantiated_class.methods)
-        content += self.wrap_methods(instantiated_class.methods)
+        content_text += self.wrap_methods(instantiated_class.methods)
 
-        self._write_file(file_name + '.m', content)
+        # TODO: Generate file code
+
+        return (file_name + '.m', content_text)
 
     def wrap_namespace(self, namespace):
         # TODO: Add documentation
-        wrapped = ''
-        includes = ''
+        wrapped = []
 
         test_output = ""
 
@@ -216,12 +218,12 @@ class MatlabWrapper(object):
                 if isinstance(element, parser.Include):
                     pass
                 elif isinstance(element, parser.Namespace):
-                    self._mkdir('+' + element.name)
                     self.wrap_namespace(element)
                 elif isinstance(element, instantiator.InstantiatedClass):
-                    self.wrap_instantiated_class(element)
+                    class_text = self.wrap_instantiated_class(element)
+                    self.content.append((class_text[0], class_text[1]))
 
-        # Handle global functions
+        # Global functions
         all_funcs = [
             func for func in namespace.content
             if isinstance(func, parser.GlobalFunction)
@@ -229,13 +231,12 @@ class MatlabWrapper(object):
 
         test_output += self.wrap_methods(all_funcs, True)
 
-        return wrapped, includes
+        return wrapped
 
     def wrap(self):
-        wrapped_namespace, includes = self.wrap_namespace(self.module)
+        wrapped_namespace = self.wrap_namespace(self.module)
 
-        # TODO: Fix this
-        return wrapped_namespace
+        return self.content
 
 
 if __name__ == "__main__":
