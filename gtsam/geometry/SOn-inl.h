353aaaef6056119cdf6d49d9cfe8e8dfb837526d
Frank Dellaert <dellaert@gmail.com>
2019-05-06 18:45:16 -0400

    Specialized Hat/Vee

--- gtsam/geometry/SOn-inl.h
+++ gtsam/geometry/SOn-inl.h
@@ -22,62 +22,16 @@
 
 namespace gtsam {
 
+// Implementation for N>5 just uses dynamic version
 template <int N>
-Matrix SO<N>::Hat(const Vector& xi) {
-  size_t n = AmbientDim(xi.size());
-  if (n < 2) throw std::invalid_argument("SO<N>::Hat: n<2 not supported");
-
-  Matrix X(n, n);  // allocate space for n*n skew-symmetric matrix
-  X.setZero();
-  if (n == 2) {
-    // Handle SO(2) case as recursion bottom
-    assert(xi.size() == 1);
-    X << 0, -xi(0), xi(0), 0;
-  } else {
-    // Recursively call SO(n-1) call for top-left block
-    const size_t dmin = (n - 1) * (n - 2) / 2;
-    X.topLeftCorner(n - 1, n - 1) = Hat(xi.tail(dmin));
-
-    // Now fill last row and column
-    double sign = 1.0;
-    for (size_t i = 0; i < n - 1; i++) {
-      const size_t j = n - 2 - i;
-      X(n - 1, j) = sign * xi(i);
-      X(j, n - 1) = -X(n - 1, j);
-      sign = -sign;
-    }
-  }
-  return X;
+typename SO<N>::MatrixNN SO<N>::Hat(const TangentVector& xi) {
+  return SOn::Hat(xi);
 }
 
+// Implementation for N>5 just uses dynamic version
 template <int N>
-Vector SO<N>::Vee(const Matrix& X) {
-  const size_t n = X.rows();
-  if (n < 2) throw std::invalid_argument("SO<N>::Hat: n<2 not supported");
-
-  if (n == 2) {
-    // Handle SO(2) case as recursion bottom
-    Vector xi(1);
-    xi(0) = X(1, 0);
-    return xi;
-  } else {
-    // Calculate dimension and allocate space
-    const size_t d = n * (n - 1) / 2;
-    Vector xi(d);
-
-    // Fill first n-1 spots from last row of X
-    double sign = 1.0;
-    for (size_t i = 0; i < n - 1; i++) {
-      const size_t j = n - 2 - i;
-      xi(i) = sign * X(n - 1, j);
-      sign = -sign;
-    }
-
-    // Recursively call Vee to fill remainder of x
-    const size_t dmin = (n - 1) * (n - 2) / 2;
-    xi.tail(dmin) = Vee(X.topLeftCorner(n - 1, n - 1));
-    return xi;
-  }
+typename SO<N>::TangentVector SO<N>::Vee(const MatrixNN& X) {
+  return SOn::Vee(X);
 }
 
 template <int N>
