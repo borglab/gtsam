e8967367001d5d0abfac5530b6fa1a685cb2a84c
Matthew Sklar <matthewsklar227@gmail.com>
2019-05-09 20:21:18 -0400

    Create templates for documentation

---
+++ wrap/docs/doc_template.py
@@ -0,0 +1,44 @@
+class ClassDoc():
+    def __init__(self, tree):
+        self.tree = tree
+
+    def get_tree(self):
+        return self.tree
+
+
+class FreeDoc():
+    def __init__(self, tree):
+        self.tree = tree
+
+    def get_tree(self):
+        return self.tree
+
+
+class Docs():
+    def __init__(self, class_docs, free_docs):
+        self.class_docs = class_docs
+        self.free_docs = free_docs
+
+    def get_class_doc(self, class_name):
+        '''Get the documentation for the class.
+
+        Arguments:
+        class_name -- the name of the class
+
+        Returns:
+        The ClassDoc with the class's documentation. None if the class does not
+        exist.
+        '''
+        return self.class_docs.get(class_name)
+
+    def get_free_doc(self, free_func_name):
+        '''Get the documentation for a free function.
+
+        Arguments:
+        free_func_name -- the name of the free function
+
+        Returns:
+        The FreeDoc with the free function's documentation. None if the class
+        does not exist.
+        '''
+        return self.free_docs.get(free_func_name)
