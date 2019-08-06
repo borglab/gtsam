955c01e31d3bf34d2f59bb5652c18e9b511688ca
mxie32 <mxie32@gatech.edu>
2019-08-06 14:36:12 -0400

    create separate files for eigen sparse solver

---
+++ gtsam/linear/EigenOptimizer.h
@@ -0,0 +1,33 @@
+/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation,
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file EigenOptimizer.h
+ *
+ * @brief optimizer linear factor graph using eigen solver as backend
+ *
+ * @date Aug 2019
+ * @author Mandy Xie
+ */
+#pragma once
+
+#include <gtsam/linear/GaussianFactorGraph.h>
+#include <gtsam/linear/VectorValues.h>
+#include <Eigen/Sparse>
+
+namespace gtsam {
+/// Optimize using Eigen's SparseQR factorization
+VectorValues optimizeEigenQR(const GaussianFactorGraph &gfg);
+
+/// Optimize using Eigen's SimplicailLDLT factorization
+VectorValues optimizeEigenCholesky(const GaussianFactorGraph &gfg);
+
+} //namespace gtsam
