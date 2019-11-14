f382d3b016cdb5c460bcd827e9e0fecbfe4bbb6f
jingwuOUO <wujing2951@gmail.com>
2019-11-14 00:58:02 -0500

    generate the convergence rate for each p value

--- gtsam_unstable/timing/process_shonan_timing_results.py
+++ gtsam_unstable/timing/process_shonan_timing_results.py
@@ -5,6 +5,9 @@ Process timing results from timeShonanAveraging
 import xlrd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+import heapq
+from collections import Counter
 
 def make_time_plot(timings):
     """Plot timings given as a dictionary {name:time}."""
@@ -20,6 +23,7 @@ def make_time_plot(timings):
 def make_combined_plot(name, p_values, times, costs, min_cost_range=10):
     """ Make a plot that combines timing and SO(3) cost.
         Arguments:
+            name: string of the plot title
             p_values: list of p-values (int)
             times: list of timings (seconds)
             costs: list of costs (double)
@@ -42,6 +46,55 @@ def make_combined_plot(name, p_values, times, costs, min_cost_range=10):
     ax1.legend(loc="upper left")
     ax2.legend(loc="upper right")
     plt.show()
+
+def make_convergence_plot(name, p_values, times, costs, iter=10):
+    """ Make a bar that show the success rate for each p_value according to whether the SO(3) cost converges
+        Arguments:
+            name: string of the plot title
+            p_values: list of p-values (int)
+            times: list of timings (seconds)
+            costs: list of costs (double)
+            iter: int of iteration number for each p_value
+    """
+    
+    max_cost = np.mean(np.array(heapq.nlargest(iter, costs)))
+    # calculate mean costs for each p value
+    p_values = list(dict(Counter(p_values)).keys())
+    p_mean_cost = [np.mean(np.array(costs[i*iter:(i+1)*iter])) for i in range(len(p_values))]
+    p_max = p_values[p_mean_cost.index(max(p_mean_cost))]
+    print(p_mean_cost)
+    print(p_max)
+
+    # calculate the convergence rate for each p_value
+    p_success_rates = []
+    if p_mean_cost[0] >= 0.9*np.mean(np.array(costs)) and p_mean_cost[0] <= 1.1*np.mean(np.array(costs)):
+        p_success_rates = [ 1.0 for p in p_values]
+    else:
+        for p in p_values:
+            if p > p_max:
+                p_costs = costs[p_values.index(p)*iter:(p_values.index(p)+1)*iter]
+                print(p_costs)
+                converged = [ int(p_cost < 0.3*max_cost) for p_cost in p_costs]
+                success_rate = sum(converged)/len(converged)    
+                p_success_rates.append(success_rate)
+            else:
+                p_success_rates.append(0)
+    print(p_success_rates)
+    # def success_rate(x, pos):
+    #     """ value and tick position
+    #     """
+    #     return (r'%d \%' % (x*100))
+    # formatter = FuncFormatter(success_rate)
+
+    fig, ax = plt.subplots()
+    # ax.yaxis.set_major_formatter(formatter)
+    plt.bar(p_values, p_success_rates)
+    plt.xticks(p_values)
+    plt.xlabel("p_value")
+    plt.ylabel("success rate")
+    plt.title(name, fontsize=12)
+    plt.show()
+
 
 # Process arguments and call plot function
 import argparse
@@ -74,9 +127,19 @@ names[6] = 'cubicle vertex = 5750, edge = 16869'
 # Parse CSV file
 for key, name in names.items():
     print(key, name)
+
+    # find  according file to process
+    name_file = None
+    for path in file_path:
+        if name[0:7] in path:
+            name_file = path
+    if name_file == None:
+        print("The file %s is not in the path" % name)
+        continue
+
     p_values, times, costs = [],[],[]
-    with open(file_path[key]) as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
+    with open(name_file) as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
         for row in reader:
             print(row)
             p_values.append(int(row[0]))
@@ -84,4 +147,5 @@ for key, name in names.items():
             costs.append(float(row[3]))
 
     #plot
-    make_combined_plot(name, p_values, times, costs)
+    # make_combined_plot(name, p_values, times, costs)
+    make_convergence_plot(name, p_values, times, costs)
