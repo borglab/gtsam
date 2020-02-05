80ff5334ce52ee194d4c9f93a3bc665d52ac653c
Varun Agrawal <varagrawal@gmail.com>
2020-02-05 11:27:57 -0500

    support K() and matrix() for Cal3_S2

--- gtsam.h
+++ gtsam.h
@@ -875,6 +875,7 @@ class Cal3_S2 {
   gtsam::Point2 principalPoint() const;
   Vector vector() const;
   Matrix K() const;
+  Matrix matrix() const;
   Matrix matrix_inverse() const;
 
   // enabling serialization functionality
