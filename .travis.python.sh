82f4f11df786b7610212362e158315843ae18d7f
Fan Jiang <prof.fan@foxmail.com>
2020-02-11 14:43:56 -0500

    Add Cython CI

---
+++ .travis.python.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -x
+
+# Install a system package required by our library
+sudo apt-get install wget libicu libicu-dev
+
+CURRDIR=$(pwd)
+
+mkdir $CURRDIR/build
+cd $CURRDIR/build
+
+cmake $CURRDIR -DCMAKE_BUILD_TYPE=Release \
+    -DGTSAM_BUILD_TESTS=OFF -DGTSAM_BUILD_UNSTABLE=ON \
+    -DGTSAM_USE_QUATERNIONS=OFF \
+    -DGTSAM_BUILD_EXAMPLES_ALWAYS=OFF \
+    -DGTSAM_INSTALL_CYTHON_TOOLBOX=ON \
+    -DGTSAM_PYTHON_VERSION=Default \
+    -DGTSAM_ALLOW_DEPRECATED_SINCE_V4=OFF \
+    -DCMAKE_INSTALL_PREFIX=$CURRDIR/../gtsam_install
+
+make -j$(nproc) install
+
+cd $CURRDIR/../gtsam_install/cython
+
+sudo python setup.py install
+
+cd $CURRDIR/cython/gtsam/tests
+
+python -m unittest discover
