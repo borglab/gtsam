845fd12feb6727a8167ff4d696e8dae7b2ae0f8e
Fan Jiang <prof.fan@foxmail.com>
2020-02-24 12:13:37 -0500

    Fix BetweenFactor for Windows

--- gtsam/slam/BetweenFactor.h
+++ gtsam/slam/BetweenFactor.h
@@ -21,6 +21,12 @@
 #include <gtsam/base/Lie.h>
 #include <gtsam/nonlinear/NonlinearFactor.h>
 
+#ifdef _WIN32
+#define BETWEENFACTOR_VISIBILITY
+#else
+#define BETWEENFACTOR_VISIBILITY GTSAM_EXPORT
+#endif
+
 namespace gtsam {
 
   /**
@@ -29,7 +35,7 @@ namespace gtsam {
    * @addtogroup SLAM
    */
   template<class VALUE>
-  class GTSAM_EXPORT BetweenFactor: public NoiseModelFactor2<VALUE, VALUE> {
+  class BETWEENFACTOR_VISIBILITY BetweenFactor: public NoiseModelFactor2<VALUE, VALUE> {
 
     // Check that VALUE type is a testable Lie group
     BOOST_CONCEPT_ASSERT((IsTestable<VALUE>));
