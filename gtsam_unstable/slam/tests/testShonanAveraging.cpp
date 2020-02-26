d5028dff8abb20c6c575b62c6d4069fbb68bed13
ss <ss>
2020-02-26 16:39:01 -0500

    Finish runWithRandomKlaus unittest.

--- gtsam_unstable/slam/tests/testShonanAveraging.cpp
+++ gtsam_unstable/slam/tests/testShonanAveraging.cpp
@@ -147,17 +147,55 @@ TEST(ShonanAveraging, runWithRandomKlaus) {
   // Check nr poses
   EXPECT_LONGS_EQUAL(3, shonan.nrPoses());
 
-  // Check first pose
-  const auto& poses = shonan.poses();
-  Point3 r1(0, 0, -1), r2(-1, 0, 0), r3(0, 1, 0);
+  // Check poses in g2o file
+  const auto &poses = shonan.poses();
+  Point3 r1(1, 0, 0), r2(0, 1, 0), r3(0, 0, 1);
   const Rot3 wRc(r1, r2, r3);
-  const Pose3 wTc(wRc, Point3(-3.9, 0, 0));
-  EXPECT(assert_equal(wTc, poses.at(0)));
+  // const Pose3 wTc(wRc, Point3(-3.9, 0, 0));
+  // Check rotation from the datafile 
+  Rot3 rot0 = poses.at(0).rotation();
+  Rot3 rot1 = poses.at(1).rotation();
+  Rot3 rot2 = poses.at(2).rotation();
+  EXPECT(assert_equal(wRc, rot0, 0.2));
+  EXPECT(assert_equal(wRc, rot1, 0.2));
+  EXPECT(assert_equal(wRc, rot2, 0.2));
+
+  // Check measurement data
+  Rot3 R01(0.9995433591728293, -0.022048798853273946, -0.01796327847857683,
+           0.010210006313668573);
+  Rot3 R12(0.9927742290779572, -0.054972994022992064, 0.10432547598981769,
+           -0.02221474884651081);
+  Rot3 R02(0.9922479626852876, -0.03174661848656213, 0.11646825423134777,
+           -0.02951742735854383);
+  EXPECT(assert_equal(rot1, rot0.compose(R01), 0.1));
+  EXPECT(assert_equal(rot2, rot1.compose(R12), 0.1));
+  EXPECT(assert_equal(rot2, rot0.compose(R02), 0.1));
 
   auto result = shonan.runWithRandom(5);
   EXPECT_DOUBLES_EQUAL(0, shonan.cost(result.first), 1e-3);
   EXPECT_DOUBLES_EQUAL(-5.427688831332745e-07, result.second,
                        1e-4);  // Regression test
+
+  // Normalize the rotations
+  Rot3 expected_rot0 = rot0.compose(rot0.inverse());
+  Rot3 expected_rot1 = rot1.compose(rot0.inverse());
+  Rot3 expected_rot2 = rot2.compose(rot0.inverse());
+  Rot3 result_rot0 = Rot3(result.first.at<SO3>(0));
+  Rot3 result_rot1 = Rot3(result.first.at<SO3>(1));
+  Rot3 result_rot2 = Rot3(result.first.at<SO3>(2));
+  Rot3 actual_rot0 = result_rot0.compose(result_rot0.inverse());
+  Rot3 actual_rot1 = result_rot1.compose(result_rot0.inverse());
+  Rot3 actual_rot2 = result_rot2.compose(result_rot0.inverse());
+
+  // Check shonan result
+  EXPECT(assert_equal(Rot3(), expected_rot0))
+  EXPECT(assert_equal(Rot3(), actual_rot0))
+  EXPECT(assert_equal(expected_rot1, actual_rot1));
+  EXPECT(assert_equal(expected_rot2, actual_rot2));
+  EXPECT(assert_equal(Rot3::Logmap(expected_rot1), Rot3::Logmap(actual_rot1),
+                      0.1));
+  EXPECT(assert_equal(Rot3::Logmap(expected_rot2), Rot3::Logmap(actual_rot2),
+                      0.1));
 }
 
 /* ************************************************************************* */
