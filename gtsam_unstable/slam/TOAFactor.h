2087075ee787e561e825c84084b0c0e4964a36ca
Frank Dellaert <dellaert@gmail.com>
2020-03-17 14:34:11 -0400

    Transitioned toa method to a functor

--- gtsam_unstable/slam/TOAFactor.h
+++ gtsam_unstable/slam/TOAFactor.h
@@ -17,33 +17,47 @@
  *  @date December 2014
  */
 
+#pragma once
+
 #include <gtsam/nonlinear/ExpressionFactor.h>
 #include <gtsam_unstable/geometry/Event.h>
 
 namespace gtsam {
 
 /// A "Time of Arrival" factor - so little code seems hardly worth it :-)
-class TOAFactor: public ExpressionFactor<double> {
-
+class TOAFactor : public ExpressionFactor<double> {
   typedef Expression<double> Double_;
 
-public:
-
+ public:
   /**
-   * Constructor
-   * @param some expression yielding an event
-   * @param microphone_ expression yielding a microphone location
-   * @param toaMeasurement time of arrival at microphone
+   * Most genral constructor with two expressions
+   * @param eventExpression expression yielding an event
+   * @param sensorExpression expression yielding a sensor location
+   * @param toaMeasurement time of arrival at sensor
    * @param model noise model
+   * @param toa optional time of arrival functor
    */
   TOAFactor(const Expression<Event>& eventExpression,
-      const Expression<Point3>& microphone_, double toaMeasurement,
-      const SharedNoiseModel& model) :
-      ExpressionFactor<double>(model, toaMeasurement,
-          Double_(&Event::toa, eventExpression, microphone_)) {
-  }
+            const Expression<Point3>& sensorExpression, double toaMeasurement,
+            const SharedNoiseModel& model,
+            const TimeOfArrival& toa = TimeOfArrival())
+      : ExpressionFactor<double>(
+            model, toaMeasurement,
+            Double_(toa, eventExpression, sensorExpression)) {}
 
+  /**
+   * Constructor with fixed sensor
+   * @param eventExpression expression yielding an event
+   * @param sensor a known sensor location
+   * @param toaMeasurement time of arrival at sensor
+   * @param model noise model
+   * @param toa optional time of arrival functor
+   */
+  TOAFactor(const Expression<Event>& eventExpression, const Point3& sensor,
+            double toaMeasurement, const SharedNoiseModel& model,
+            const TimeOfArrival& toa = TimeOfArrival())
+      : TOAFactor(eventExpression, Expression<Point3>(sensor), toaMeasurement,
+                  model, toa) {}
 };
 
-} //\ namespace gtsam
-
+}  // namespace gtsam
