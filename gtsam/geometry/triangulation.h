abe1636ac8b302b9d74b200bcc8b60e214b9c675
Varun Agrawal <varagrawal@gmail.com>
2020-03-17 15:22:05 -0400

    deprecate PinholeCamera specific functions for triangulation

--- gtsam/geometry/triangulation.h
+++ gtsam/geometry/triangulation.h
@@ -124,7 +124,17 @@ std::pair<NonlinearFactorGraph, Values> triangulationGraph(
   return std::make_pair(graph, values);
 }
 
-/// PinholeCamera specific version // TODO: (chris) why does this exist?
+#ifdef GTSAM_ALLOW_DEPRECATED_SINCE_V4
+/// DEPRECATED: PinholeCamera specific version
+template<class CALIBRATION>
+Point3 triangulateNonlinear(
+    const CameraSet<PinholeCamera<CALIBRATION> >& cameras,
+    const Point2Vector& measurements, const Point3& initialEstimate) {
+  return triangulateNonlinear<PinholeCamera<CALIBRATION> > //
+  (cameras, measurements, initialEstimate);
+}
+
+/// DEPRECATED: PinholeCamera specific version
 template<class CALIBRATION>
 std::pair<NonlinearFactorGraph, Values> triangulationGraph(
     const CameraSet<PinholeCamera<CALIBRATION> >& cameras,
@@ -133,6 +143,7 @@ std::pair<NonlinearFactorGraph, Values> triangulationGraph(
   return triangulationGraph<PinholeCamera<CALIBRATION> > //
   (cameras, measurements, landmarkKey, initialEstimate);
 }
+#endif
 
 /**
  * Optimize for triangulation
@@ -185,15 +196,6 @@ Point3 triangulateNonlinear(
       (cameras, measurements, Symbol('p', 0), initialEstimate);
 
   return optimize(graph, values, Symbol('p', 0));
-}
-
-/// PinholeCamera specific version  // TODO: (chris) why does this exist?
-template<class CALIBRATION>
-Point3 triangulateNonlinear(
-    const CameraSet<PinholeCamera<CALIBRATION> >& cameras,
-    const Point2Vector& measurements, const Point3& initialEstimate) {
-  return triangulateNonlinear<PinholeCamera<CALIBRATION> > //
-  (cameras, measurements, initialEstimate);
 }
 
 /**
