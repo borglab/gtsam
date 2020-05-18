d05279ee5e3021b1d7e88a0fec446520adc55e24
Varun Agrawal <varagrawal@gmail.com>
2020-05-18 16:39:03 -0500

    updated demangle code to not use unique_ptr, support for Windows, and a simple test

--- gtsam/base/GenericValue.h
+++ gtsam/base/GenericValue.h
@@ -20,6 +20,7 @@
 #pragma once
 
 #include <gtsam/base/Manifold.h>
+#include <gtsam/base/types.h>
 #include <gtsam/base/Value.h>
 
 #include <boost/make_shared.hpp>
@@ -30,33 +31,6 @@
 #include <typeinfo> // operator typeid
 
 namespace gtsam {
-
-#ifdef __GNUG__
-#include <cstdlib>
-#include <cxxabi.h>
-#include <string>
-
-/// Pretty print Value type name
-static std::string demangle(const char* name) {
-  int status = -4; // some arbitrary value to eliminate the compiler warning
-  // enable c++11 by passing the flag -std=c++11 to g++
-  std::unique_ptr<char, void(*)(void*)> res {
-      abi::__cxa_demangle(name, NULL, NULL, &status),
-      std::free
-  };
-  return (status==0) ? res.get() : name ;
-}
-
-#else
-
-// does nothing if not g++
-static std::string demangle(const char* name) {
-    return name;
-}
-
-#endif
-
-
 
 /**
  * Wraps any type T so it can play as a Value
