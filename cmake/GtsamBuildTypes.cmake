a29b4dbd7418b6be4748023fd381648b59872314
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2020-06-02 13:18:46 +0200

    Do not optimize native by default

--- cmake/GtsamBuildTypes.cmake
+++ cmake/GtsamBuildTypes.cmake
@@ -160,7 +160,7 @@ if(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
 endif()
 
 if (NOT MSVC)
-  option(GTSAM_BUILD_WITH_MARCH_NATIVE  "Enable/Disable building with all instructions supported by native architecture (binary may not be portable!)" ON)
+  option(GTSAM_BUILD_WITH_MARCH_NATIVE  "Enable/Disable building with all instructions supported by native architecture (binary may not be portable!)" OFF)
   if(GTSAM_BUILD_WITH_MARCH_NATIVE)
     # Add as public flag so all dependant projects also use it, as required
     # by Eigen to avid crashes due to SIMD vectorization:
