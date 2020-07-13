7dc0e2c0b201f3230e1cac88bcb035e9d7352077
Fan Jiang <i@fanjiang.me>
2020-07-13 10:56:20 -0400

    Remove wrap

--- wrap/tests/expected_namespaces/+ns2/overloadedGlobalFunction.m
+++
@@ -1,8 +0,0 @@
-function varargout = overloadedGlobalFunction(varargin)
-      if length(varargin) == 1 && isa(varargin{1},'ns1.ClassA')
-        varargout{1} = testNamespaces_wrapper(24, varargin{:});
-      elseif length(varargin) == 2 && isa(varargin{1},'ns1.ClassA') && isa(varargin{2},'double')
-        varargout{1} = testNamespaces_wrapper(25, varargin{:});
-      else
-        error('Arguments do not match any overload of function ns2.overloadedGlobalFunction');
-      end
