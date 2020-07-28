0c3e9c9228d3df98333158584b233dbb9fdd600d
Varun Agrawal <varagrawal@gmail.com>
2020-07-28 13:41:18 -0500

    updated init to not include numpy

--- python/gtsam_py/python/__init__.py
+++ python/gtsam_py/python/__init__.py
@@ -1,11 +1,23 @@
 from .gtsam import *
 
-import numpy as np
+
+def _init():
+    import numpy as np
+
+    global Point2  # export function
+
+    def Point2(x=0, y=0):
+        return np.array([x, y], dtype=float)
+
+    global Point3  # export function
+
+    def Point3(x=0, y=0, z=0):
+        return np.array([x, y, z], dtype=float)
+
+    # for interactive debugging
+    if __name__ == "__main__":
+        # we want all definitions accessible
+        globals().update(locals())
 
 
-def Point2(x=0, y=0):
-    return np.array([x, y], dtype=float)
-
-
-def Point3(x=0, y=0, z=0):
-    return np.array([x, y, z], dtype=float)
+_init()
