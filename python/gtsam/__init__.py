3d477f3a38e9cf1ce2ea22f93849e2e88673da57
Frank dellaert <dellaert@gmail.com>
2020-08-20 14:47:55 -0400

    Fixed Point2 and Point3 to have similar behavior as in C++ - to also take vectors - to be initialized to Nan

--- python/gtsam/__init__.py
+++ python/gtsam/__init__.py
@@ -5,17 +5,24 @@ def _init():
     """This function is to add shims for the long-gone Point2 and Point3 types"""
 
     import numpy as np
+    import math
 
     global Point2  # export function
 
-    def Point2(x=0, y=0):
+    def Point2(x=math.nan, y=math.nan):
         """Shim for the deleted Point2 type."""
+        if isinstance(x, np.ndarray):
+            assert x.shape == (2,), "Point2 takes 2-vector"
+            return x  # "copy constructor"
         return np.array([x, y], dtype=float)
 
     global Point3  # export function
 
-    def Point3(x=0, y=0, z=0):
+    def Point3(x=math.nan, y=math.nan, z=math.nan):
         """Shim for the deleted Point3 type."""
+        if isinstance(x, np.ndarray):
+            assert x.shape == (3,), "Point3 takes 3-vector"
+            return x  # "copy constructor"
         return np.array([x, y, z], dtype=float)
 
     # for interactive debugging
