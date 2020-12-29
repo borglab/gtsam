7a28b4a8e026059a97f80656b34786e2f7bc0f1e
lcarlone <lcarlone@mit.edu>
2020-12-28 21:03:20 -0500

    addressed all except 2 comments by Frank. waiting for inputs on the 2 outstanding issues

--- gtsam/nonlinear/GncParams.h
+++ gtsam/nonlinear/GncParams.h
@@ -34,47 +34,50 @@ namespace gtsam {
 /* ************************************************************************* */
 template<class BaseOptimizerParameters>
 class GncParams {
-public:
-  /** For each parameter, specify the corresponding optimizer: e.g., GaussNewtonParams -> GaussNewtonOptimizer */
+ public:
+  /// For each parameter, specify the corresponding optimizer: e.g., GaussNewtonParams -> GaussNewtonOptimizer.
   typedef typename BaseOptimizerParameters::OptimizerType OptimizerType;
 
-  /** Verbosity levels */
+  /// Verbosity levels
   enum Verbosity {
-    SILENT = 0, SUMMARY, VALUES
+    SILENT = 0,
+    SUMMARY,
+    VALUES
   };
 
-  /** Choice of robust loss function for GNC */
+  /// Choice of robust loss function for GNC.
   enum GncLossType {
-    GM /*Geman McClure*/, TLS /*Truncated least squares*/
+    GM /*Geman McClure*/,
+    TLS /*Truncated least squares*/
   };
 
-  /// Constructor
-  GncParams(const BaseOptimizerParameters& baseOptimizerParams) :
-      baseOptimizerParams(baseOptimizerParams) {
+  /// Constructor.
+  GncParams(const BaseOptimizerParameters& baseOptimizerParams)
+      : baseOptimizerParams(baseOptimizerParams) {
   }
 
-  /// Default constructor
-  GncParams() :
-      baseOptimizerParams() {
+  /// Default constructor.
+  GncParams()
+      : baseOptimizerParams() {
   }
 
-  /// GNC parameters
-  BaseOptimizerParameters baseOptimizerParams; /*optimization parameters used to solve the weighted least squares problem at each GNC iteration*/
+  /// GNC parameters.
+  BaseOptimizerParameters baseOptimizerParams;  ///< Optimization parameters used to solve the weighted least squares problem at each GNC iteration
   /// any other specific GNC parameters:
-  GncLossType lossType = TLS; /* default loss*/
-  size_t maxIterations = 100; /* maximum number of iterations*/
-  double barcSq = 1.0; /* a factor is considered an inlier if factor.error() < barcSq. Note that factor.error() whitens by the covariance*/
-  double muStep = 1.4; /* multiplicative factor to reduce/increase the mu in gnc */
-  double relativeCostTol = 1e-5; ///< if relative cost change is below this threshold, stop iterating
-  double weightsTol = 1e-4; ///< if the weights are within weightsTol from being binary, stop iterating (only for TLS)
-  Verbosity verbosity = SILENT; /* verbosity level */
-  std::vector<size_t> knownInliers = std::vector<size_t>(); /* slots in the factor graph corresponding to measurements that we know are inliers */
+  GncLossType lossType = TLS;  ///< Default loss
+  size_t maxIterations = 100;  ///<  Maximum number of iterations
+  double barcSq = 1.0;  ///< A factor is considered an inlier if factor.error() < barcSq. Note that factor.error() whitens by the covariance
+  double muStep = 1.4;  ///< Multiplicative factor to reduce/increase the mu in gnc
+  double relativeCostTol = 1e-5;  ///< If relative cost change is below this threshold, stop iterating
+  double weightsTol = 1e-4;  ///< If the weights are within weightsTol from being binary, stop iterating (only for TLS)
+  Verbosity verbosity = SILENT;  ///< Verbosity level
+  std::vector<size_t> knownInliers = std::vector<size_t>();  ///< Slots in the factor graph corresponding to measurements that we know are inliers
 
-  /// Set the robust loss function to be used in GNC (chosen among the ones in GncLossType)
+  /// Set the robust loss function to be used in GNC (chosen among the ones in GncLossType).
   void setLossType(const GncLossType type) {
     lossType = type;
   }
-  /// Set the maximum number of iterations in GNC (changing the max nr of iters might lead to less accurate solutions and is not recommended)
+  /// Set the maximum number of iterations in GNC (changing the max nr of iters might lead to less accurate solutions and is not recommended).
   void setMaxIterations(const size_t maxIter) {
     std::cout
         << "setMaxIterations: changing the max nr of iters might lead to less accurate solutions and is not recommended! "
@@ -85,22 +88,24 @@ public:
    * the inlier threshold is the largest value of f(x) for the corresponding measurement to be considered an inlier.
    * In other words, an inlier at x is such that 0.5 * || r(x) ||^2_Omega <= barcSq.
    * Assuming a isotropic measurement covariance sigma^2 * Identity, the cost becomes: 0.5 * 1/sigma^2 || r(x) ||^2 <= barcSq.
-   * Hence || r(x) ||^2 <= 2 * barcSq * sigma^2
+   * Hence || r(x) ||^2 <= 2 * barcSq * sigma^2.
    * */
   void setInlierCostThreshold(const double inth) {
     barcSq = inth;
   }
-  /// Set the graduated non-convexity step: at each GNC iteration, mu is updated as mu <- mu * muStep
+  /// Set the graduated non-convexity step: at each GNC iteration, mu is updated as mu <- mu * muStep.
   void setMuStep(const double step) {
     muStep = step;
   }
-  /// Set the maximum relative difference in mu values to stop iterating
-  void setRelativeCostTol(double value) { relativeCostTol = value;
+  /// Set the maximum relative difference in mu values to stop iterating.
+  void setRelativeCostTol(double value) {
+    relativeCostTol = value;
   }
-  /// Set the maximum difference between the weights and their rounding in {0,1} to stop iterating
-  void setWeightsTol(double value) { weightsTol = value;
+  /// Set the maximum difference between the weights and their rounding in {0,1} to stop iterating.
+  void setWeightsTol(double value) {
+    weightsTol = value;
   }
-  /// Set the verbosity level
+  /// Set the verbosity level.
   void setVerbosityGNC(const Verbosity value) {
     verbosity = value;
   }
@@ -108,33 +113,32 @@ public:
    * corresponds to the slots in the factor graph. For instance, if you have a nonlinear factor graph nfg,
    * and you provide  knownIn = {0, 2, 15}, GNC will not apply outlier rejection to nfg[0], nfg[2], and nfg[15].
    * This functionality is commonly used in SLAM when one may assume the odometry is outlier free, and
-   * only apply GNC to prune outliers from the loop closures
+   * only apply GNC to prune outliers from the loop closures.
    * */
   void setKnownInliers(const std::vector<size_t>& knownIn) {
     for (size_t i = 0; i < knownIn.size(); i++)
       knownInliers.push_back(knownIn[i]);
   }
-  /// equals
+  /// Equals.
   bool equals(const GncParams& other, double tol = 1e-9) const {
     return baseOptimizerParams.equals(other.baseOptimizerParams)
         && lossType == other.lossType && maxIterations == other.maxIterations
         && std::fabs(barcSq - other.barcSq) <= tol
         && std::fabs(muStep - other.muStep) <= tol
-        && verbosity == other.verbosity
-        && knownInliers == other.knownInliers;
+        && verbosity == other.verbosity && knownInliers == other.knownInliers;
   }
-  /// print function
+  /// Print.
   void print(const std::string& str) const {
     std::cout << str << "\n";
     switch (lossType) {
-    case GM:
-      std::cout << "lossType: Geman McClure" << "\n";
-      break;
-    case TLS:
-      std::cout << "lossType: Truncated Least-squares" << "\n";
-      break;
-    default:
-      throw std::runtime_error("GncParams::print: unknown loss type.");
+      case GM:
+        std::cout << "lossType: Geman McClure" << "\n";
+        break;
+      case TLS:
+        std::cout << "lossType: Truncated Least-squares" << "\n";
+        break;
+      default:
+        throw std::runtime_error("GncParams::print: unknown loss type.");
     }
     std::cout << "maxIterations: " << maxIterations << "\n";
     std::cout << "barcSq: " << barcSq << "\n";
