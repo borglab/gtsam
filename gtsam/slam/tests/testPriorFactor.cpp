a472e7162b958b8031d7df3e8d43446a7b40e6ca
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2021-02-13 18:55:56 +0100

    Negative sigmas unit tests

--- gtsam/slam/tests/testPriorFactor.cpp
+++ gtsam/slam/tests/testPriorFactor.cpp
@@ -5,9 +5,11 @@
  * @date   Nov 4, 2014
  */
 
-#include <gtsam/base/Vector.h>
-#include <gtsam/nonlinear/PriorFactor.h>
 #include <CppUnitLite/TestHarness.h>
+#include <gtsam/base/Vector.h>
+#include <gtsam/nonlinear/GaussNewtonOptimizer.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/nonlinear/PriorFactor.h>
 
 using namespace std;
 using namespace gtsam;
@@ -31,6 +33,35 @@ TEST(PriorFactor, ConstructorDynamicSizeVector) {
   Vector v(5); v << 1, 2, 3, 4, 5;
   SharedNoiseModel model = noiseModel::Isotropic::Sigma(5, 1.0);
   PriorFactor<Vector> factor(1, v, model);
+}
+
+// Test negative sigmas
+TEST(PriorFactor, NegativeSigmas) {
+  auto noise1 = gtsam::noiseModel::Isotropic::Sigma(2, 0.1);
+  auto noise2 = gtsam::noiseModel::Isotropic::Sigma(2, -2.0);
+
+  const Values initValues = {{1, genericValue(Point2(0, 0))}};
+
+  // First, test using positive sigmas:
+  {
+    NonlinearFactorGraph fg;
+    fg.emplace_shared<PriorFactor<Point2>>(1, Point2(0, 0), noise1);
+    fg.emplace_shared<PriorFactor<Point2>>(1, Point2(1, 1), noise1);
+    const Point2 result =
+        GaussNewtonOptimizer(fg, initValues).optimize().at<Point2>(1);
+    EXPECT_DOUBLES_EQUAL(0.5, result.x(), 1e-3);
+    EXPECT_DOUBLES_EQUAL(0.5, result.y(), 1e-3);
+  }
+  // Second, test using negative sigmas:
+  {
+    NonlinearFactorGraph fg;
+    fg.emplace_shared<PriorFactor<Point2>>(1, Point2(0, 0), noise1);
+    fg.emplace_shared<PriorFactor<Point2>>(1, Point2(1, 1), noise2);
+    const Point2 result =
+        GaussNewtonOptimizer(fg, initValues).optimize().at<Point2>(1);
+    EXPECT_DOUBLES_EQUAL(0.5, result.x(), 1e-3);
+    EXPECT_DOUBLES_EQUAL(0.5, result.y(), 1e-3);
+  }
 }
 
 /* ************************************************************************* */
