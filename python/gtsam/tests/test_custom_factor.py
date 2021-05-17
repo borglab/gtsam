5a50609d904af6bade30b0d717aaccf81f394e9c
Fan Jiang <i@fanjiang.me>
2021-05-17 14:22:57 -0400

    Change to using nullptr

--- python/gtsam/tests/test_custom_factor.py
+++ python/gtsam/tests/test_custom_factor.py
@@ -17,10 +17,9 @@ import numpy as np
 import gtsam
 from gtsam.utils.test_case import GtsamTestCase
 
-
 class TestCustomFactor(GtsamTestCase):
-
     def test_new(self):
+        """Test the creation of a new CustomFactor"""
         def error_func(this: CustomFactor, v: gtsam.Values, H: List[np.ndarray]):
             return np.array([1, 0, 0])
         
@@ -28,7 +27,7 @@ class TestCustomFactor(GtsamTestCase):
         cf = CustomFactor(noise_model, gtsam.KeyVector([0]), error_func)
 
     def test_call(self):
-
+        """Test if calling the factor works (only error)"""
         expected_pose = Pose2(1, 1, 0)
 
         def error_func(this: CustomFactor, v: gtsam.Values, H: List[np.ndarray]) -> np.ndarray:
@@ -53,14 +52,18 @@ class TestCustomFactor(GtsamTestCase):
         expected = Pose2(2, 2, np.pi/2)
 
         def error_func(this: CustomFactor, v: gtsam.Values, H: List[np.ndarray]):
-            # print(f"{this = },\n{v = },\n{len(H) = }")
+            """
+            the custom error function. One can freely use variables captured
+            from the outside scope. Or the variables can be acquired by indexing `v`.
+            Jacobian is passed by modifying the H array of numpy matrices.
+            """
 
             key0 = this.keys()[0]
             key1 = this.keys()[1]
             gT1, gT2 = v.atPose2(key0), v.atPose2(key1)
             error = Pose2(0, 0, 0).localCoordinates(gT1.between(gT2))
             
-            if len(H) > 0:
+            if not H is None:
                 result = gT1.between(gT2)
                 H[0] = -result.inverse().AdjointMap()
                 H[1] = np.eye(3)
@@ -81,6 +84,42 @@ class TestCustomFactor(GtsamTestCase):
         J_bf, b_bf = gf_b.jacobian()
         np.testing.assert_allclose(J_cf, J_bf)
         np.testing.assert_allclose(b_cf, b_bf)
+
+    def test_no_jacobian(self):
+        """Tests that we will not calculate the Jacobian if not requested"""
+
+        gT1 = Pose2(1, 2, np.pi/2)
+        gT2 = Pose2(-1, 4, np.pi)
+
+        expected = Pose2(2, 2, np.pi/2)
+
+        def error_func(this: CustomFactor, v: gtsam.Values, H: List[np.ndarray]):
+            # print(f"{this = },\n{v = },\n{len(H) = }")
+
+            key0 = this.keys()[0]
+            key1 = this.keys()[1]
+            gT1, gT2 = v.atPose2(key0), v.atPose2(key1)
+            error = Pose2(0, 0, 0).localCoordinates(gT1.between(gT2))
+
+            self.assertTrue(H is None) # Should be true if we only request the error
+
+            if not H is None:
+                result = gT1.between(gT2)
+                H[0] = -result.inverse().AdjointMap()
+                H[1] = np.eye(3)
+            return error
+
+        noise_model = gtsam.noiseModel.Unit.Create(3)
+        cf = CustomFactor(noise_model, gtsam.KeyVector([0, 1]), error_func)
+        v = Values()
+        v.insert(0, gT1)
+        v.insert(1, gT2)
+
+        bf = gtsam.BetweenFactorPose2(0, 1, Pose2(0, 0, 0), noise_model)
+
+        e_cf = cf.error(v)
+        e_bf = bf.error(v)
+        np.testing.assert_allclose(e_cf, e_bf)
 
 if __name__ == "__main__":
     unittest.main()
