5e0d3987dcae551571eb9c4cc2a296d68937e9ec
lcarlone <lcarlone@mit.edu>
2021-08-27 17:07:08 -0400

    linear triangulation solved!

--- gtsam/geometry/triangulation.cpp
+++ gtsam/geometry/triangulation.cpp
@@ -53,13 +53,34 @@ Vector4 triangulateHomogeneousDLT(
   return v;
 }
 
-Point3 triangulateDLT(const std::vector<Matrix34, Eigen::aligned_allocator<Matrix34>>& projection_matrices,
+Point3 triangulateDLT(
+    const std::vector<Matrix34, Eigen::aligned_allocator<Matrix34>>& projection_matrices,
     const Point2Vector& measurements, double rank_tol) {
 
-  Vector4 v = triangulateHomogeneousDLT(projection_matrices, measurements, rank_tol);
+  Vector4 v = triangulateHomogeneousDLT(projection_matrices, measurements,
+                                        rank_tol);
 
   // Create 3D point from homogeneous coordinates
   return Point3(v.head<3>() / v[3]);
+}
+
+Point3 triangulateDLT(
+    const std::vector<Matrix34, Eigen::aligned_allocator<Matrix34>>& projection_matrices,
+    const std::vector<Unit3>& unit3measurements, double rank_tol) {
+
+  Point2Vector measurements;
+  size_t i=0;
+  for (const Unit3& pu : unit3measurements) {  // get canonical pixel projection from Unit3
+    Point3 p = pu.point3();
+    if (p.z() <= 0) // TODO: maybe we should handle this more delicately
+      throw(TriangulationCheiralityException());
+    measurements.push_back(Point2(p.x() / p.z(), p.y() / p.z()));
+    std::cout << "px" << Point2(pu.point3().x() / pu.point3().z(),
+                                   pu.point3().y() / pu.point3().z())<< std::endl;
+    std::cout << "projection_matrices \n "<< projection_matrices.at(i)<< std::endl;
+    i++;
+  }
+  return triangulateDLT(projection_matrices, measurements, rank_tol);
 }
 
 ///
@@ -71,7 +92,7 @@ Point3 triangulateDLT(const std::vector<Matrix34, Eigen::aligned_allocator<Matri
  * @return refined Point3
  */
 Point3 optimize(const NonlinearFactorGraph& graph, const Values& values,
-    Key landmarkKey) {
+                Key landmarkKey) {
   // Maybe we should consider Gauss-Newton?
   LevenbergMarquardtParams params;
   params.verbosityLM = LevenbergMarquardtParams::TRYLAMBDA;
