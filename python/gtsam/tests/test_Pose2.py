26aacfb8cb69134369c9df792f3d7e4642eb4dc2
John Lambert <johnlambert@gatech.edu>
2021-08-31 06:26:52 -0600

    start python unit test for align()

--- python/gtsam/tests/test_Pose2.py
+++ python/gtsam/tests/test_Pose2.py
@@ -6,7 +6,7 @@ All Rights Reserved
 See LICENSE for the license information
 
 Pose2 unit tests.
-Author: Frank Dellaert & Duy Nguyen Ta (Python)
+Author: Frank Dellaert & Duy Nguyen Ta & John Lambert
 """
 import unittest
 
@@ -20,13 +20,20 @@ from gtsam.utils.test_case import GtsamTestCase
 class TestPose2(GtsamTestCase):
     """Test selected Pose2 methods."""
 
-    def test_adjoint(self):
+    def test_adjoint(self) -> None:
         """Test adjoint method."""
         xi = np.array([1, 2, 3])
         expected = np.dot(Pose2.adjointMap_(xi), xi)
         actual = Pose2.adjoint_(xi, xi)
         np.testing.assert_array_equal(actual, expected)
 
+    def test_align(self) -> None:
+        """Ensure estimation of the Pose2 element to align two 2d point clouds succeeds.
+        
+        """
+        pass
+        
+        
 
 if __name__ == "__main__":
     unittest.main()
