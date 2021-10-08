225ac77f2f524ef4828cb7728628deb03d1aeddf
John Lambert <johnlambert@gatech.edu>
2021-10-07 21:04:58 -0400

    fix assert

--- python/gtsam/tests/test_Rot3.py
+++ python/gtsam/tests/test_Rot3.py
@@ -34,7 +34,7 @@ class TestRot3(GtsamTestCase):
         # get back angle in radians
         _, actual_angle = Rot3(R).axisAngle()
         expected_angle = 3.1396582
-        self.gtsamAssertEquals(actual_angle, expected_angle, 1e-7)
+        np.testing.assert_almost_equal(actual_angle, expected_angle, 1e-7)
 
 
 if __name__ == "__main__":
