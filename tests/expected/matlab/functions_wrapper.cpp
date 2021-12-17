bcfe0a7a2921b4667d5e92592152578ae2f9353d
Varun Agrawal <varagrawal@gmail.com>
2021-12-17 15:55:42 -0500

    Squashed 'wrap/' changes from 086be59be..2cbaf7a3a
    2cbaf7a3a Merge pull request #131 from borglab/remove-pickle
    0c2a4483c add documentation
    2e5af11ad Merge pull request #139 from borglab/fix/matlab-memory-leak
    442b7d3b0 update test fixtures
    f89d5e4bd delete object after clearing it from object collector
    971282703 add the correct variable for RTTI registry creation
    9758dec57 update test fixture
    87aeb8f8c remove need for declaring pickle function in interface file
    
    git-subtree-dir: wrap
    git-subtree-split: 2cbaf7a3a628766ff40657e0150b407ed4af7ccd

--- tests/expected/matlab/functions_wrapper.cpp
+++ tests/expected/matlab/functions_wrapper.cpp
@@ -51,7 +51,7 @@ void _functions_RTTIRegister() {
     mxDestroyArray(registry);
 
     mxArray *newAlreadyCreated = mxCreateNumericMatrix(0, 0, mxINT8_CLASS, mxREAL);
-    if(mexPutVariable("global", "gtsam_geometry_rttiRegistry_created", newAlreadyCreated) != 0) {
+    if(mexPutVariable("global", "gtsam_functions_rttiRegistry_created", newAlreadyCreated) != 0) {
       mexErrMsgTxt("gtsam wrap:  Error indexing RTTI types, inheritance will not work correctly");
     }
     mxDestroyArray(newAlreadyCreated);
