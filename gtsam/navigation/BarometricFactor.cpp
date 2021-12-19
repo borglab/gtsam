cf0830084d3b7495cfecbddb69701a960ccd168d
peterQFR <petermilani80@gmail.com>
2021-12-20 07:24:52 +1000

    Apply Google Format

--- gtsam/navigation/BarometricFactor.cpp
+++ gtsam/navigation/BarometricFactor.cpp
@@ -23,30 +23,31 @@ using namespace std;
 namespace gtsam {
 
 //***************************************************************************
-void BarometricFactor::print(const string& s, const KeyFormatter& keyFormatter) const {
-  cout << (s.empty() ? "" : s + " ") << "Barometric Factor on " << keyFormatter(key1())
-       << "Barometric Bias on " << keyFormatter(key2()) << "\n";
+void BarometricFactor::print(const string& s,
+                             const KeyFormatter& keyFormatter) const {
+    cout << (s.empty() ? "" : s + " ") << "Barometric Factor on "
+         << keyFormatter(key1()) << "Barometric Bias on "
+         << keyFormatter(key2()) << "\n";
 
-  cout << "  Baro measurement: " << nT_ << "\n";
-  noiseModel_->print("  noise model: ");
+    cout << "  Baro measurement: " << nT_ << "\n";
+    noiseModel_->print("  noise model: ");
 }
 
 //***************************************************************************
-bool BarometricFactor::equals(const NonlinearFactor& expected, double tol) const {
-  const This* e = dynamic_cast<const This*>(&expected);
-  return e != nullptr && Base::equals(*e, tol) && traits<double>::Equals(nT_, e->nT_, tol);
+bool BarometricFactor::equals(const NonlinearFactor& expected,
+                              double tol) const {
+    const This* e = dynamic_cast<const This*>(&expected);
+    return e != nullptr && Base::equals(*e, tol) &&
+           traits<double>::Equals(nT_, e->nT_, tol);
 }
 
 //***************************************************************************
-Vector BarometricFactor::evaluateError(const Pose3& p,
-        const double& bias, boost::optional<Matrix&> H,
-    boost::optional<Matrix&> H2) const {
-
-  if (H2) (*H2) = (Matrix(1,1) << 1.0).finished();
-  if(H)(*H) = (Matrix(1, 6) << 0., 0., 0., 0., 0., 1.).finished();
-  return (Vector(1) <<(p.translation().z()+bias - nT_)).finished();
+Vector BarometricFactor::evaluateError(const Pose3& p, const double& bias,
+                                       boost::optional<Matrix&> H,
+                                       boost::optional<Matrix&> H2) const {
+    if (H2) (*H2) = (Matrix(1, 1) << 1.0).finished();
+    if (H) (*H) = (Matrix(1, 6) << 0., 0., 0., 0., 0., 1.).finished();
+    return (Vector(1) << (p.translation().z() + bias - nT_)).finished();
 }
 
-//***************************************************************************
-
-}/// namespace gtsam
+}  // namespace gtsam
