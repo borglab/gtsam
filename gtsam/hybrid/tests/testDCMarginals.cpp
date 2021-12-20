178468ecdccdd0ffb8a0e8b069a5ea2deb2fa756
Varun Agrawal <varagrawal@gmail.com>
2021-12-20 16:09:50 -0500

    simple tests for DCMarginals and DCValues

---
+++ gtsam/hybrid/tests/testDCMarginals.cpp
@@ -0,0 +1,41 @@
+/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation,
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ *  @file  testDCMarginals.cpp
+ *  @brief Unit tests for DCMarginals class
+ *  @author Varun Agrawal
+ *  @date December 2021
+ */
+
+#include <CppUnitLite/TestHarness.h>
+#include <gtsam/base/serializationTestHelpers.h>
+#include <gtsam/hybrid/DCMarginals.h>
+
+using namespace std;
+using namespace gtsam;
+using namespace serializationTestHelpers;
+
+//******************************************************************************
+TEST(DCMarginals, Constructor) {
+  Marginals continuous;
+  DiscreteMarginals discrete;
+
+  DCMarginals dc_marginals(continuous, discrete);
+  EXPECT(assert_type("gtsam::DCMarginals", dc_marginals));
+}
+
+/* ************************************************************************* */
+int main() {
+  TestResult tr;
+  return TestRegistry::runAllTests(tr);
+}
+/* ************************************************************************* */
---
+++ gtsam/hybrid/tests/testDCValues.cpp
@@ -0,0 +1,42 @@
+/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation,
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ *  @file  testDCValues.cpp
+ *  @brief Unit tests for DCValues class
+ *  @author Varun Agrawal
+ *  @date December 2021
+ */
+
+#include <CppUnitLite/TestHarness.h>
+#include <gtsam/base/serializationTestHelpers.h>
+#include <gtsam/hybrid/DCValues.h>
+
+using namespace std;
+using namespace gtsam;
+using namespace serializationTestHelpers;
+
+//******************************************************************************
+TEST(DCValues, Constructor) { 
+  Values contValues;
+  DiscreteValues discValues;
+
+  DCValues dc_values(contValues, discValues);
+  EXPECT(assert_type("gtsam::DCValues", dc_values));
+}
+
+
+/* ************************************************************************* */
+int main() {
+  TestResult tr;
+  return TestRegistry::runAllTests(tr);
+}
+/* ************************************************************************* */
