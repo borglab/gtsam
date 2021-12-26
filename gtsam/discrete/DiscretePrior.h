4bc7b0ba8542b39bda34b6c8260519495da91294
Frank Dellaert <dellaert@gmail.com>
2021-12-26 15:21:02 -0500

    single argument variants

--- gtsam/discrete/DiscretePrior.h
+++ gtsam/discrete/DiscretePrior.h
@@ -75,7 +75,34 @@ class GTSAM_EXPORT DiscretePrior : public DiscreteConditional {
       const KeyFormatter& formatter = DefaultKeyFormatter) const override {
     Base::print(s, formatter);
   }
+  /// @}
+  /// @name Standard interface
+  /// @{
 
+  /// Evaluate given a single value.
+  double operator()(size_t value) const {
+    if (nrFrontals() != 1)
+      throw std::invalid_argument(
+          "Single value operator can only be invoked on single-variable "
+          "priors");
+    DiscreteValues values;
+    values.emplace(keys_[0], value);
+    return Base::operator()(values);
+  }
+
+  /// Evaluate given a single value.
+  std::vector<double> pmf() const {
+    if (nrFrontals() != 1)
+      throw std::invalid_argument(
+          "DiscretePrior::pmf only defined for single-variable priors");
+    const size_t nrValues = cardinalities_.at(keys_[0]);
+    std::vector<double> array;
+    array.reserve(nrValues);
+    for (size_t v = 0; v < nrValues; v++) {
+      array.push_back(operator()(v));
+    }
+    return array;
+  }
   /// @}
 };
 // DiscretePrior
