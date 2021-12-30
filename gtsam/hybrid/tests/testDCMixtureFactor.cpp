f401e8fa839259e17904985e6c8f254c2a8a1aef
Varun Agrawal <varagrawal@gmail.com>
2021-12-30 15:55:26 -0500

    finish the test for DCMixtureFactor

--- gtsam/hybrid/tests/testDCMixtureFactor.cpp
+++ gtsam/hybrid/tests/testDCMixtureFactor.cpp
@@ -21,49 +21,49 @@ using namespace gtsam;
 
 /******************************************************************************/
 
-// /*
-//  * Test DCDiscreteFactor using a simple mixture.
-//  *
-//  * Construct a single factor (for a single variable) consisting of a
-//  * discrete-conditional mixture. Here we have a "null hypothesis" consisting
-//  of
-//  * a Gaussian with large variance and an "alternative hypothesis" consisting
-//  of
-//  * a Gaussian with smaller variance.
-//  */
-// TEST(TestSuite, dcdiscrete_mixture) {
-//   // We'll make a variable with 2 possible assignments
-//   const size_t cardinality = 2;
-//   DiscreteKey dk(Symbol('d', 1), cardinality);
+/*
+ * Test DCDiscreteFactor using a simple mixture.
+ *
+ * Construct a single factor (for a single variable) consisting of a
+ * discrete-conditional mixture. Here we have a "null hypothesis" consisting
+ of
+ * a Gaussian with large variance and an "alternative hypothesis" consisting
+ of
+ * a Gaussian with smaller variance.
+ */
+TEST(TestSuite, dcdiscrete_mixture) {
+  // We'll make a variable with 2 possible assignments
+  const size_t cardinality = 2;
+  DiscreteKey dk(Symbol('d', 1), cardinality);
 
-//   // Make a symbol for a single continuous variable and add to KeyVector
-//   Symbol x1 = Symbol('x', 1);
-//   KeyVector keys;
-//   keys.push_back(x1);
+  // Make a symbol for a single continuous variable and add to KeyVector
+  Symbol x1 = Symbol('x', 1);
+  KeyVector keys;
+  keys.push_back(x1);
 
-//   // Make a factor for non-null hypothesis
-//   const double loc = 0.0;
-//   const double sigma1 = 1.0;
-//   auto prior_noise1 = noiseModel::Isotropic::Sigma(1, sigma1);
-//   PriorFactor<double> f1(x1, loc, prior_noise1);
+  // Make a factor for non-null hypothesis
+  const double loc = 0.0;
+  const double sigma1 = 1.0;
+  auto prior_noise1 = noiseModel::Isotropic::Sigma(1, sigma1);
+  PriorFactor<double> f1(x1, loc, prior_noise1);
 
-//   // Make a factor for null hypothesis
-//   const double sigmaNullHypo = 8.0;
-//   auto prior_noiseNullHypo = noiseModel::Isotropic::Sigma(1, sigmaNullHypo);
-//   PriorFactor<double> fNullHypo(x1, loc, prior_noiseNullHypo);
+  // Make a factor for null hypothesis
+  const double sigmaNullHypo = 8.0;
+  auto prior_noiseNullHypo = noiseModel::Isotropic::Sigma(1, sigmaNullHypo);
+  PriorFactor<double> fNullHypo(x1, loc, prior_noiseNullHypo);
 
-//   DCMixtureFactor<PriorFactor<double>> dcMixture(keys, dk, {f1, fNullHypo});
+  DCMixtureFactor<PriorFactor<double>> dcMixture(keys, dk, {f1, fNullHypo});
 
-//   // Check error.
-//   Values continuousVals;
-//   continuousVals.insert(x1, -2.5);
-//   DiscreteValues discreteVals;
-//   discreteVals[dk.first] = 0;
+  // Check error.
+  Values continuousVals;
+  continuousVals.insert(x1, -2.5);
+  DiscreteValues discreteVals;
+  discreteVals[dk.first] = 0;
 
-//   // regression
-//   EXPECT_DOUBLES_EQUAL(2.2, dcMixture.error(continuousVals, discreteVals),
-//                        1e-1);
-// }
+  // regression
+  EXPECT_DOUBLES_EQUAL(2.2, dcMixture.error(continuousVals, discreteVals),
+                       1e-1);
+}
 
 TEST(DCMixtureFactor, Error) {
   // We'll make a variable with 2 possible assignments
@@ -77,16 +77,35 @@ TEST(DCMixtureFactor, Error) {
   KeyVector keys;
   keys.push_back(x1);
 
-  // Make a factor for non-null hypothesis
+  // Make a factor for non-null hypothesis which has mu=1.0.
   auto prior_noise1 = noiseModel::Isotropic::Sigma(1, 1.0);
-  PriorFactor<double> f1(x1, 0.0, prior_noise1);
+  PriorFactor<double> f1(x1, 1.0, prior_noise1);
 
-  // Make a factor for null hypothesis
+  // Make a factor for null hypothesis with mu = 0.0.
   auto prior_noiseNullHypo = noiseModel::Isotropic::Sigma(1, 8.0);
   PriorFactor<double> fNullHypo(x1, 0.0, prior_noiseNullHypo);
 
-  DCMixtureFactor<PriorFactor<double>> factor(keys, dKeys, {f1, fNullHypo});
+  // Create the factor to test. We set normalize to true so that expected values
+  // are easy to compute manually (they should be 0.0)
+  DCMixtureFactor<PriorFactor<double>> factor(keys, dKeys, {fNullHypo, f1},
+                                              true);
+
+  Values continuousValues;
+  DiscreteValues discreteValues;
+
+  // Test the null hypothesis
+  continuousValues.insert(x1, 0.0);
+  discreteValues[dk.first] = 0;
+  double error = factor.error(continuousValues, discreteValues);
+  EXPECT_DOUBLES_EQUAL(0.0, error, 1e-9);
+
+  // Test the alternate hypothesis
+  continuousValues.update(x1, 1.0);
+  discreteValues[dk.first] = 1;
+  error = factor.error(continuousValues, discreteValues);
+  EXPECT_DOUBLES_EQUAL(0.0, error, 1e-9);
 }
+
 /* ************************************************************************* */
 int main() {
   TestResult tr;
