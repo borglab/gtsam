2151fd385215edff45da30efd70f7484dca238f0
David Wisth <davidwisth@gmail.com>
2022-01-03 16:06:11 +1100

    change from SharedGaussian to SharedNoiseModel

--- gtsam_unstable/slam/LocalOrientedPlane3Factor.h
+++ gtsam_unstable/slam/LocalOrientedPlane3Factor.h
@@ -56,12 +56,12 @@ public:
    * Note: The anchorPoseKey can simply be chosen as the first pose a plane
    * is observed.  
    */
-  LocalOrientedPlane3Factor(const Vector4& z, const SharedGaussian& noiseModel,
+  LocalOrientedPlane3Factor(const Vector4& z, const SharedNoiseModel& noiseModel,
                             Key poseKey, Key anchorPoseKey, Key landmarkKey)
       : Base(noiseModel, poseKey, anchorPoseKey, landmarkKey), measured_p_(z) {}
 
   LocalOrientedPlane3Factor(const OrientedPlane3& z,
-                            const SharedGaussian& noiseModel,
+                            const SharedNoiseModel& noiseModel,
                             Key poseKey, Key anchorPoseKey, Key landmarkKey)
     : Base(noiseModel, poseKey, anchorPoseKey, landmarkKey), measured_p_(z) {}
 
