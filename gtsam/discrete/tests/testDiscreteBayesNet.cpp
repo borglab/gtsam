624dd6f59d4594ab0b6a22f3b20e86dd84d29425
Frank Dellaert <dellaert@gmail.com>
2022-01-26 23:25:18 -0500

    fix unit test

--- gtsam/discrete/tests/testDiscreteBayesNet.cpp
+++ gtsam/discrete/tests/testDiscreteBayesNet.cpp
@@ -152,11 +152,19 @@ TEST(DiscreteBayesNet, Dot) {
   string actual = fragment.dot();
   cout << actual << endl;
   EXPECT(actual ==
-         "digraph G{\n"
-         "3->5\n"
-         "6->5\n"
-         "4->6\n"
-         "0->3\n"
+         "digraph {\n"
+         "  size=\"5,5\";\n"
+         "\n"
+         "  var0[label=\"0\"];\n"
+         "  var3[label=\"3\"];\n"
+         "  var4[label=\"4\"];\n"
+         "  var5[label=\"5\"];\n"
+         "  var6[label=\"6\"];\n"
+         "\n"
+         "  var3->var5\n"
+         "  var6->var5\n"
+         "  var4->var6\n"
+         "  var0->var3\n"
          "}");
 }
 
