719135ce538384788c6cf5d3b230562c7ef2e882
Hal-Zhaodong-Yang <93161408+Hal-Zhaodong-Yang@users.noreply.github.com>
2022-02-04 19:08:33 -0500

    add trifocal tensor class cpp file
    add trifocal tensor constructor from bearing measurements

---
+++ gtsam/geometry/TrifocalTensor2.cpp
@@ -0,0 +1,34 @@
+
+
+#include <gtsam/geometry/TrifocalTensor2.h>
+
+#include <iostream>
+
+namespace gtsam {
+TrifocalTensor2::TrifocalTensor2(const std::vector<Rot2>& bearings_u,
+                                 const std::vector<Rot2>& bearings_v,
+                                 const std::vector<Rot2>& bearings_w) {
+  int i = 0;
+  gtsam::Matrix(bearing_u.size(), 8) system_matrix;
+  for (int i = 0; i < bearing_u.size(); i++) {
+    system_matrix(i, 0) = bearings_u.c() * bearings_v.c() * bearings_w.c();
+    system_matrix(i, 1) = bearings_u.c() * bearings_v.c() * bearings_w.s();
+    system_matrix(i, 2) = bearings_u.c() * bearings_v.s() * bearings_w.c();
+    system_matrix(i, 3) = bearings_u.c() * bearings_v.s() * bearings_w.s();
+    system_matrix(i, 4) = bearings_u.s() * bearings_v.c() * bearings_w.c();
+    system_matrix(i, 5) = bearings_u.s() * bearings_v.c() * bearings_w.s();
+    system_matrix(i, 6) = bearings_u.s() * bearings_v.s() * bearings_w.c();
+    system_matrix(i, 7) = bearings_u.s() * bearings_v.s() * bearings_w.s();
+  }
+
+  gtsam::Matrix U, S, V;
+  svd(system_matrix, U, S, V);
+  gtsam::Vector8 V_last_column = column(V, V.cols() - 1);
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      matrix0_(i, j) = V_last_column(2 * i + j);
+      matrix1_(i, j) = V_last_column(2 * i + j + 4);
+    }
+  }
+}
+}  // namespace gtsam
