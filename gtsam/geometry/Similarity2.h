b2b1fe264b6bdf9c07009c517fb9a80dc26ec63e
Varun Agrawal <varagrawal@gmail.com>
2022-02-06 00:08:54 -0500

    Similarity2 fixes

--- gtsam/geometry/Similarity2.h
+++ gtsam/geometry/Similarity2.h
@@ -17,13 +17,12 @@
 
 #pragma once
 
-#include <gtsam/geometry/Rot2.h>
-#include <gtsam/geometry/Point2.h>
-#include <gtsam/geometry/Pose2.h>
 #include <gtsam/base/Lie.h>
 #include <gtsam/base/Manifold.h>
 #include <gtsam/dllexport.h>
-
+#include <gtsam/geometry/Point2.h>
+#include <gtsam/geometry/Pose2.h>
+#include <gtsam/geometry/Rot2.h>
 
 namespace gtsam {
 
@@ -33,21 +32,19 @@ class Pose2;
 /**
  * 2D similarity transform
  */
-class Similarity2: public LieGroup<Similarity2, 4> {
-
+class Similarity2 : public LieGroup<Similarity2, 4> {
   /// @name Pose Concept
   /// @{
   typedef Rot2 Rotation;
   typedef Point2 Translation;
   /// @}
 
-private:
+ private:
   Rot2 R_;
   Point2 t_;
   double s_;
 
-public:
-
+ public:
   /// @name Constructors
   /// @{
 
@@ -60,11 +57,11 @@ public:
   /// Construct from GTSAM types
   GTSAM_EXPORT Similarity2(const Rot2& R, const Point2& t, double s);
 
-  // /// Construct from Eigen types
-  // GTSAM_EXPORT Similarity2(const Matrix2& R, const Vector2& t, double s);
+  /// Construct from Eigen types
+  GTSAM_EXPORT Similarity2(const Matrix2& R, const Vector2& t, double s);
 
-  // /// Construct from matrix [R t; 0 s^-1]
-  // GTSAM_EXPORT Similarity2(const Matrix3& T);
+  /// Construct from matrix [R t; 0 s^-1]
+  GTSAM_EXPORT Similarity2(const Matrix3& T);
 
   /// @}
   /// @name Testable
@@ -79,7 +76,8 @@ public:
   /// Print with optional string
   GTSAM_EXPORT void print(const std::string& s) const;
 
-  GTSAM_EXPORT friend std::ostream &operator<<(std::ostream &os, const Similarity2& p);
+  GTSAM_EXPORT friend std::ostream& operator<<(std::ostream& os,
+                                               const Similarity2& p);
 
   /// @}
   /// @name Group
@@ -94,22 +92,22 @@ public:
   /// Return the inverse
   GTSAM_EXPORT Similarity2 inverse() const;
 
-  // /// @}
-  // /// @name Group action on Point2
-  // /// @{
+  /// @}
+  /// @name Group action on Point2
+  /// @{
 
   /// Action on a point p is s*(R*p+t)
   GTSAM_EXPORT Point2 transformFrom(const Point2& p) const;
 
-  /** 
+  /**
    * Action on a pose T.
-   * |Rs  ts|   |R t|   |Rs*R Rs*t+ts| 
+   * |Rs  ts|   |R t|   |Rs*R Rs*t+ts|
    * |0  1/s| * |0 1| = | 0      1/s |, the result is still a Sim2 object.
    * To retrieve a Pose2, we normalized the scale value into 1.
    * |Rs*R Rs*t+ts|   |Rs*R s(Rs*t+ts)|
    * | 0      1/s | = |  0       1    |
-   * 
-   * This group action satisfies the compatibility condition. 
+   *
+   * This group action satisfies the compatibility condition.
    * For more details, refer to: https://en.wikipedia.org/wiki/Group_action
    */
   GTSAM_EXPORT Pose2 transformFrom(const Pose2& T) const;
@@ -117,22 +115,26 @@ public:
   /* syntactic sugar for transformFrom */
   GTSAM_EXPORT Point2 operator*(const Point2& p) const;
 
-  // /**
-  //  *  Create Similarity2 by aligning at least two point pairs
-  //  */
-  // GTSAM_EXPORT static Similarity2 Align(const std::vector<Point2Pair>& abPointPairs);
-  
-  // /**
-  //  * Create the Similarity2 object that aligns at least two pose pairs.
-  //  * Each pair is of the form (aTi, bTi).
-  //  * Given a list of pairs in frame a, and a list of pairs in frame b, Align()
-  //  * will compute the best-fit Similarity2 aSb transformation to align them.
-  //  * First, the rotation aRb will be computed as the average (Karcher mean) of
-  //  * many estimates aRb (from each pair). Afterwards, the scale factor will be computed
-  //  * using the algorithm described here:
-  //  * http://www5.informatik.uni-erlangen.de/Forschung/Publikationen/2005/Zinsser05-PSR.pdf
-  //  */
-  // GTSAM_EXPORT static Similarity2 Align(const std::vector<Pose2Pair>& abPosePairs);
+  /**
+   *  Create Similarity2 by aligning at least two point pairs
+   */
+  GTSAM_EXPORT static Similarity2 Align(const Point2Pairs& abPointPairs);
+
+  /**
+   * Create the Similarity2 object that aligns at least two pose pairs.
+   * Each pair is of the form (aTi, bTi).
+   * Given a list of pairs in frame a, and a list of pairs in frame b,
+   Align()
+   * will compute the best-fit Similarity2 aSb transformation to align them.
+   * First, the rotation aRb will be computed as the average (Karcher mean)
+   of
+   * many estimates aRb (from each pair). Afterwards, the scale factor will
+   be computed
+   * using the algorithm described here:
+   * http://www5.informatik.uni-erlangen.de/Forschung/Publikationen/2005/Zinsser05-PSR.pdf
+   */
+  GTSAM_EXPORT static Similarity2 Align(
+      const std::vector<Pose2Pair>& abPosePairs);
 
   /// @}
   /// @name Lie Group
@@ -145,45 +147,33 @@ public:
   /// @{
 
   /// Calculate 4*4 matrix group equivalent
-  GTSAM_EXPORT const Matrix3 matrix() const;
+  GTSAM_EXPORT Matrix3 matrix() const;
 
   /// Return a GTSAM rotation
-  const Rot2& rotation() const {
-    return R_;
-  }
+  Rot2 rotation() const { return R_; }
 
   /// Return a GTSAM translation
-  const Point2& translation() const {
-    return t_;
-  }
+  Point2 translation() const { return t_; }
 
   /// Return the scale
-  double scale() const {
-    return s_;
-  }
+  double scale() const { return s_; }
 
   /// Convert to a rigid body pose (R, s*t)
-  /// TODO(frank): why is this here? Red flag! Definitely don't have it as a cast.
   GTSAM_EXPORT operator Pose2() const;
 
   /// Dimensionality of tangent space = 4 DOF - used to autodetect sizes
-  inline static size_t Dim() {
-    return 4;
-  }
+  inline static size_t Dim() { return 4; }
 
   /// Dimensionality of tangent space = 4 DOF
-  inline size_t dim() const {
-    return 4;
-  }
+  inline size_t dim() const { return 4; }
 
   /// @}
 };
 
+template <>
+struct traits<Similarity2> : public internal::LieGroup<Similarity2> {};
 
-// template<>
-// struct traits<Similarity2> : public internal::LieGroup<Similarity2> {};
+template <>
+struct traits<const Similarity2> : public internal::LieGroup<Similarity2> {};
 
-// template<>
-// struct traits<const Similarity2> : public internal::LieGroup<Similarity2> {};
-
-} // namespace gtsam
+}  // namespace gtsam
