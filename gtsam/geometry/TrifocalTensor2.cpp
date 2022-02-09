98bdb493ea762b851f7d39f94a507a91a54cadd6
Hal-Zhaodong-Yang <zdyang324@gmail.com>
2022-02-08 23:22:48 -0500

    Changed some format and added some comments

--- gtsam/geometry/TrifocalTensor2.cpp
+++ gtsam/geometry/TrifocalTensor2.cpp
@@ -1,9 +1,23 @@
-#include <gtsam/geometry/TrifocalTensor2.h>
+/* ----------------------------------------------------------------------------
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation,
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+ * See LICENSE for the license information
+ * -------------------------------------------------------------------------- */
 
+/**
+ * @file    TrifocalTensor2.cpp
+ * @brief   A 2x2x2 trifocal tensor in a plane, for 1D cameras.
+ * @author  Zhaodong Yang
+ * @author  Akshay Krishnan
+ */
+
+#include <gtsam/geometry/TrifocalTensor2.h>
 #include <iostream>
 
 namespace gtsam {
-
+// Convert bearing measurements to projective form
 std::vector<Point2> convertToProjective(const std::vector<Rot2>& rotations) {
   std::vector<Point2> projectives;
   projectives.reserve(rotations.size());
@@ -13,6 +27,7 @@ std::vector<Point2> convertToProjective(const std::vector<Rot2>& rotations) {
   return projectives;
 }
 
+// Construct from 8 bearing measurements.
 TrifocalTensor2::TrifocalTensor2(const std::vector<Rot2>& bearings_u,
                                  const std::vector<Rot2>& bearings_v,
                                  const std::vector<Rot2>& bearings_w)
@@ -20,19 +35,15 @@ TrifocalTensor2::TrifocalTensor2(const std::vector<Rot2>& bearings_u,
                       convertToProjective(bearings_v),
                       convertToProjective(bearings_w)) {}
 
+// Construct from 8 bearing measurements expressed in projective coordinates.
 TrifocalTensor2::TrifocalTensor2(const std::vector<Point2>& u,
                                  const std::vector<Point2>& v,
                                  const std::vector<Point2>& w) {
   if (u.size() != v.size() || v.size() != w.size()) {
     // throw error here
   }
-  /*
-  std::cout << "u, v, w:" << std::endl;
-  for (int row = 0; row < u.size(); row++) {
-    std::cout << u[row][0] << " " << u[row][1] << " " << v[row][0] << " "
-              << v[row][1] << " " << w[row][0] << " " << w[row][1] << std::endl;
-  }*/
-  Matrix A(u.size() > 8? u.size():8, 8);
+
+  Matrix A(u.size() > 8 ? u.size() : 8, 8);
   std::cout << "system matrix:" << std::endl;
   for (int row = 0; row < u.size(); row++) {
     for (int i = 0; i < 2; i++) {
@@ -45,8 +56,8 @@ TrifocalTensor2::TrifocalTensor2(const std::vector<Point2>& u,
     }
     std::cout << std::endl;
   }
-  for (int row = u.size() - 8; row < 0; row ++){
-    for (int col = 0; col < 8; col ++){
+  for (int row = u.size() - 8; row < 0; row++) {
+    for (int col = 0; col < 8; col++) {
       A(row, col) = 0;
       std::cout << A(row, col) << " ";
     }
@@ -70,6 +81,8 @@ TrifocalTensor2::TrifocalTensor2(const std::vector<Point2>& u,
   }
 }
 
+// Finds a measurement in the first view using measurements from second and
+// third views.
 Rot2 TrifocalTensor2::transform(const Rot2& vZp, const Rot2& wZp) const {
   Rot2 uZp;
   Vector2 v_measurement, w_measurement;
