dbec0d321247b2c345203fb798d2fee639c23c00
Varun Agrawal <varagrawal@gmail.com>
2022-02-17 09:37:17 -0500

    navigation tests passing

--- gtsam/navigation/CombinedImuFactor.h
+++ gtsam/navigation/CombinedImuFactor.h
@@ -351,6 +351,3 @@ template <>
 struct traits<CombinedImuFactor> : public Testable<CombinedImuFactor> {};
 
 }  // namespace gtsam
-
-/// Add Boost serialization export key (declaration) for derived class
-BOOST_CLASS_EXPORT_KEY(gtsam::PreintegrationCombinedParams)
