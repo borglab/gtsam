6ae319e4010282e81cc6f9734f62541e127c4191
acxz <17132214+acxz@users.noreply.github.com>
2022-02-21 11:49:17 -0500

    add missing header guards under gtsam/

--- gtsam/linear/linearAlgorithms-inst.h
+++ gtsam/linear/linearAlgorithms-inst.h
@@ -15,6 +15,8 @@
  * @author  Richard Roberts
  */
 
+#pragma once
+
 #include <gtsam/linear/VectorValues.h>
 #include <gtsam/linear/GaussianConditional.h>
 #include <gtsam/base/treeTraversal-inst.h>
