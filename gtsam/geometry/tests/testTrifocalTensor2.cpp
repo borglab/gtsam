2bf85c5845ed2f5889a9ac08a05b3ce69429a39d
Akshay Krishnan <akshay.krishnan.30@gmail.com>
2022-02-25 00:14:35 -0800

    create test data, update test doc

--- gtsam/geometry/tests/testTrifocalTensor2.cpp
+++ gtsam/geometry/tests/testTrifocalTensor2.cpp
@@ -13,128 +13,93 @@
  * @author  Akshay Krishnan
  */
 
-#include <iostream>
-#include <vector>
-
 #include <CppUnitLite/TestHarness.h>
-
 #include <gtsam/base/Testable.h>
 #include <gtsam/geometry/BearingRange.h>
 #include <gtsam/geometry/Pose2.h>
 #include <gtsam/geometry/TrifocalTensor2.h>
 
+#include <vector>
+
 using namespace std::placeholders;
 using namespace std;
 using namespace gtsam;
 
-TEST(TrifocalTensor2, transform) {
-  // 2D robots poses
-  Pose2 u = Pose2(0, 0, 0);
-  Pose2 v = Pose2(1.0, 2.0, 3.1415926 / 6);
-  Pose2 w = Pose2(-0.5, 0.5, -3.1415926 / 18);
+namespace trifocal {
 
-  // 2D landmarks
-  vector<Point2> landmarks;
-  landmarks.push_back(Point2(2.0, 0.5));
-  landmarks.push_back(Point2(-0.8, 2.4));
-  landmarks.push_back(Point2(1.9, -0.4));
-  landmarks.push_back(Point2(2.3, 1.0));
-  landmarks.push_back(Point2(-0.4, -0.4));
-  landmarks.push_back(Point2(-3.2, -1.0));
-  landmarks.push_back(Point2(1.5, 2.0));
+struct TrifocalTestData {
+  vector<Pose2> gt_poses;
+  vector<Point2> gt_landmarks;
 
-  // getting bearing measurement from landmarks
-  vector<Rot2> measurement_u, measurement_v, measurement_w;
-  for (int i = 0; i < landmarks.size(); ++i) {
-    measurement_u.push_back(u.bearing(landmarks[i]));
-    measurement_v.push_back(v.bearing(landmarks[i]));
-    measurement_w.push_back(w.bearing(landmarks[i]));
+  // Outer vector over poses.
+  std::vector<vector<Rot2>> measurements;
+};
+
+TrifocalTestData getTestData() {
+  TrifocalTestData data;
+
+  // Poses
+  data.gt_poses.emplace_back(0, 0, 0);
+  data.gt_poses.emplace_back(0, 0, 0);
+  data.gt_poses.emplace_back(0, 0, 0);
+
+  // Landmarks
+  data.gt_landmarks.emplace_back(2.0, 0.5);
+  data.gt_landmarks.emplace_back(-0.8, 2.4);
+  data.gt_landmarks.emplace_back(1.9, -0.4);
+  data.gt_landmarks.emplace_back(2.3, 1.0);
+  data.gt_landmarks.emplace_back(-0.4, -0.4);
+  data.gt_landmarks.emplace_back(-3.2, -1.0);
+  data.gt_landmarks.emplace_back(1.5, 2.0);
+
+  // Measurements
+  for (const Pose2& pose : data.gt_poses) {
+    std::vector<Rot2> measurements;
+    for (const Point2& landmark : data.gt_landmarks) {
+      measurements.push_back(pose.bearing(landmark));
+    }
+    data.measurements.push_back(measurements);
   }
+  return data;
+}
+
+}  // namespace trifocal
+
+TEST(TrifocalTensor2, transform) {
+  trifocal::TrifocalTestData data = trifocal::getTestData();
 
   // calculate trifocal tensor
-  TrifocalTensor2 T(measurement_u, measurement_v, measurement_w);
+  TrifocalTensor2 T(data.measurements[0], data.measurements[1],
+                    data.measurements[2]);
 
   // estimate measurement of a robot from the measurements of the other two
   // robots
-  for (unsigned int i = 0; i < measurement_u.size(); i++) {
-    const Rot2 actual_measurement_u =
-        T.transform(measurement_v[i], measurement_w[i]);
+  for (unsigned int i = 0; i < data.measurements[0].size(); i++) {
+    const Rot2 actual_measurement =
+        T.transform(data.measurements[1][i], data.measurements[2][i]);
 
     // there might be two solutions for u1 and u2, comparing the ratio instead
     // of both cos and sin
-    EXPECT(assert_equal(actual_measurement_u.c() * measurement_u[i].s(),
-                        actual_measurement_u.s() * measurement_u[i].c(), 1e-8));
+    EXPECT(assert_equal(actual_measurement.c() * data.measurements[0][i].s(),
+                        actual_measurement.s() * data.measurements[0][i].c(),
+                        1e-8));
   }
 }
 
-TEST(TrifocalTensor2, mat0) {
-  Pose2 u = Pose2(0, 0, 0);
-  Pose2 v = Pose2(1.0, 2.0, 3.1415926 / 6);
-  Pose2 w = Pose2(-0.5, 0.5, -3.1415926 / 18);
-
-  // 2D landmarks
-  vector<Point2> landmarks;
-  landmarks.push_back(Point2(2.0, 0.5));
-  landmarks.push_back(Point2(-0.8, 2.4));
-  landmarks.push_back(Point2(1.9, -0.4));
-  landmarks.push_back(Point2(2.3, 1.0));
-  landmarks.push_back(Point2(-0.4, -0.4));
-  landmarks.push_back(Point2(-3.2, -1.0));
-  landmarks.push_back(Point2(1.5, 2.0));
-
-  // getting bearing measurement from landmarks
-  vector<Rot2> measurement_u, measurement_v, measurement_w;
-  for (unsigned int i = 0; i < landmarks.size(); ++i) {
-    measurement_u.push_back(u.bearing(landmarks[i]));
-    measurement_v.push_back(v.bearing(landmarks[i]));
-    measurement_w.push_back(w.bearing(landmarks[i]));
-  }
+TEST(TrifocalTensor2, tensorRegression) {
+  trifocal::TrifocalTestData data = trifocal::getTestData();
 
   // calculate trifocal tensor
-  TrifocalTensor2 T(measurement_u, measurement_v, measurement_w);
+  TrifocalTensor2 T(data.measurements[0], data.measurements[1],
+                    data.measurements[2]);
 
-  Matrix2 actual_trifocal_tensor_mat0 = T.mat0();
+  Matrix2 expected_tensor_mat0, expected_tensor_mat1;
+  // These values were obtained from a numpy-based python implementation.
+  expected_tensor_mat0 << -0.13178263, 0.29210566, -0.00860471, -0.73975238;
+  expected_tensor_mat1 << -0.27261704, 0.09097327, 0.51699647, 0.0108839;
 
-  Matrix2 exp_trifocal_tensor_mat0;
-  exp_trifocal_tensor_mat0 << -0.13178263, 0.29210566, -0.00860471, -0.73975238;
-
-  EXPECT(assert_equal(actual_trifocal_tensor_mat0, exp_trifocal_tensor_mat0,
-                      1e-2));
-}
-
-TEST(TrifocalTensor2, mat1) {
-  Pose2 u = Pose2(0, 0, 0);
-  Pose2 v = Pose2(1.0, 2.0, 3.1415926 / 6);
-  Pose2 w = Pose2(-0.5, 0.5, -3.1415926 / 18);
-
-  // 2D landmarks
-  vector<Point2> landmarks;
-  landmarks.push_back(Point2(2.0, 0.5));
-  landmarks.push_back(Point2(-0.8, 2.4));
-  landmarks.push_back(Point2(1.9, -0.4));
-  landmarks.push_back(Point2(2.3, 1.0));
-  landmarks.push_back(Point2(-0.4, -0.4));
-  landmarks.push_back(Point2(-3.2, -1.0));
-  landmarks.push_back(Point2(1.5, 2.0));
-
-  // getting bearing measurement from landmarks
-  vector<Rot2> measurement_u, measurement_v, measurement_w;
-  for (unsigned int i = 0; i < landmarks.size(); ++i) {
-    measurement_u.push_back(u.bearing(landmarks[i]));
-    measurement_v.push_back(v.bearing(landmarks[i]));
-    measurement_w.push_back(w.bearing(landmarks[i]));
-  }
-
-  // calculate trifocal tensor
-  TrifocalTensor2 T(measurement_u, measurement_v, measurement_w);
-
-  Matrix2 actual_trifocal_tensor_mat0 = T.mat1();
-
-  Matrix2 exp_trifocal_tensor_mat0;
-  exp_trifocal_tensor_mat0 << -0.27261704, 0.09097327, 0.51699647, 0.0108839;
-
-  EXPECT(assert_equal(actual_trifocal_tensor_mat0, exp_trifocal_tensor_mat0,
-                      1e-2));
+  EXPECT(assert_equal(T.mat0(), expected_tensor_mat0, 1e-2));
+  EXPECT(assert_equal(T.mat1(), expected_tensor_mat1, 1e-2));
 }
 
 int main() {
