d3e0401834419f33c15d908c1eee88bcd1ba1d6f
Varun Agrawal <varagrawal@gmail.com>
2022-02-25 15:29:55 -0500

    add optional support in wrapping for hybrid

--- python/gtsam/tests/test_Hybrid.py
+++ python/gtsam/tests/test_Hybrid.py
@@ -1,6 +1,6 @@
 import gtsam
 import numpy as np
-from gtsam import GaussianHybridFactorGraph
+from gtsam import GaussianHybridFactorGraph, IncrementalHybrid
 from gtsam.utils.test_case import GtsamTestCase
 
 
@@ -11,3 +11,9 @@ class TestHybridElimination(GtsamTestCase):
     def test_elimination(self):
         # Check if constructed correctly
         self.assertIsInstance(self.ghfg, GaussianHybridFactorGraph)
+
+    def test_incremental(self):
+        ordering = gtsam.Ordering()
+        inc = IncrementalHybrid()
+        inc.update(self.ghfg, ordering)
+        # inc.update(self.ghfg, ordering, 4)
