0aeb59695e542bc1513ba63f900afd601dd86525
Fan Jiang <prof.fan@foxmail.com>
2022-03-13 23:15:20 -0400

    add more comments

--- gtsam/hybrid/HybridConditional.cpp
+++ gtsam/hybrid/HybridConditional.cpp
@@ -7,8 +7,25 @@
 
 namespace gtsam {
 void HybridConditional::print(const std::string &s,
-                                     const KeyFormatter &formatter) const {
-  Conditional::print(s, formatter);
+                              const KeyFormatter &formatter) const {
+  std::cout << s << "P(";
+  int index = 0;
+  const size_t N = keys().size();
+  const size_t contN = N - discreteKeys_.size();
+  while (index < N) {
+    if (index > 0) {
+      if (index == nrFrontals_) std::cout << " | "; else std::cout << ", ";
+    }
+    if (index < contN) {
+      std::cout << formatter(keys()[index]);
+    } else {
+      auto &dk = discreteKeys_[index - contN];
+      std::cout << "(" << formatter(dk.first) << ", " << dk.second << ")";
+    }
+    index++;
+  }
+  std::cout << ")\n";
+  if (inner) inner->print("", formatter);
 }
 
 bool HybridConditional::equals(const HybridFactor &other,
