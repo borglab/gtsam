c332c6fac50a65475cc8d21bce3ca88c1e8122eb
Akash Patel <17132214+acxz@users.noreply.github.com>
2022-03-25 17:11:05 -0400

    fix typo
    Co-authored-by: Varun Agrawal <varagrawal@gmail.com>

--- cmake/HandleMetis.cmake
+++ cmake/HandleMetis.cmake
@@ -22,7 +22,7 @@ if(GTSAM_USE_SYSTEM_METIS)
 
     add_library(metis-gtsam-if INTERFACE)
     target_include_directories(metis-gtsam-if BEFORE INTERFACE ${METIS_INCLUDE_DIR}
-      # gtsam_unstable/partition/FindSeparator-inl.h uses internel metislib.h API
+      # gtsam_unstable/partition/FindSeparator-inl.h uses internal metislib.h API
       # via extern "C"
       $<BUILD_INTERFACE:${GTSAM_SOURCE_DIR}/gtsam/3rdparty/metis/libmetis>
       $<BUILD_INTERFACE:${GTSAM_SOURCE_DIR}/gtsam/3rdparty/metis/GKlib>
@@ -37,7 +37,7 @@ else()
   target_include_directories(metis-gtsam BEFORE PUBLIC
     $<INSTALL_INTERFACE:include/gtsam/3rdparty/metis/>
     $<BUILD_INTERFACE:${GTSAM_SOURCE_DIR}/gtsam/3rdparty/metis/include>
-    # gtsam_unstable/partition/FindSeparator-inl.h uses internel metislib.h API
+    # gtsam_unstable/partition/FindSeparator-inl.h uses internal metislib.h API
     # via extern "C"
     $<BUILD_INTERFACE:${GTSAM_SOURCE_DIR}/gtsam/3rdparty/metis/libmetis>
     $<BUILD_INTERFACE:${GTSAM_SOURCE_DIR}/gtsam/3rdparty/metis/GKlib>
