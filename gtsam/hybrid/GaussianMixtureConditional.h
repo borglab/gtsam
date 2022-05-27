90c95d1f43ec43a3f44441534d7d32aa9970fe5f
Varun Agrawal <varagrawal@gmail.com>
2022-05-27 15:12:19 -0400

    Hybrid and Gaussian Mixture conditional docs and some refactor

--- gtsam/hybrid/GaussianMixtureConditional.h
+++ gtsam/hybrid/GaussianMixtureConditional.h
@@ -25,6 +25,14 @@
 #include <gtsam/linear/GaussianConditional.h>
 
 namespace gtsam {
+
+/**
+ * @brief A conditional of gaussian mixtures indexed by discrete variables.
+ *
+ * Represents the conditional density P(X | M, Z) where X is a continuous random
+ * variable, M is the discrete variable and Z is the set of measurements.
+ *
+ */
 class GaussianMixtureConditional
     : public HybridFactor,
       public Conditional<HybridFactor, GaussianMixtureConditional> {
@@ -34,13 +42,28 @@ class GaussianMixtureConditional
   using BaseFactor = HybridFactor;
   using BaseConditional = Conditional<HybridFactor, GaussianMixtureConditional>;
 
+  /// Alias for DecisionTree of GaussianFactorGraphs
+  using Sum = DecisionTree<Key, GaussianFactorGraph>;
+
+  /// typedef for Decision Tree of Gaussian Conditionals
   using Conditionals = DecisionTree<Key, GaussianConditional::shared_ptr>;
 
+ private:
   Conditionals conditionals_;
 
- public:
   /**
-   * @brief Construct a new Gaussian Mixture object
+   * @brief Convert a DecisionTree of factors into a DT of Gaussian FGs.
+   */
+  Sum asGaussianFactorGraphTree() const;
+
+ public:
+  /// @name Constructors
+  /// @{
+
+  /// Defaut constructor, mainly for serialization.
+  GaussianMixtureConditional() = default;
+  /**
+   * @brief Construct a new GaussianMixtureConditional object
    *
    * @param continuousFrontals the continuous frontals.
    * @param continuousParents the continuous parents.
@@ -52,15 +75,6 @@ class GaussianMixtureConditional
                              const DiscreteKeys &discreteParents,
                              const Conditionals &conditionals);
 
-  using Sum = DecisionTree<Key, GaussianFactorGraph>;
-
-  const Conditionals &conditionals();
-
-  /**
-   * @brief Combine Decision Trees
-   */
-  Sum add(const Sum &sum) const;
-
   /**
    * @brief Make a Gaussian Mixture from a list of Gaussian conditionals
    *
@@ -69,10 +83,14 @@ class GaussianMixtureConditional
    * @param discreteParents Discrete parents variables
    * @param conditionals List of conditionals
    */
-  static This FromConditionalList(
+  static This FromConditionals(
       const KeyVector &continuousFrontals, const KeyVector &continuousParents,
       const DiscreteKeys &discreteParents,
       const std::vector<GaussianConditional::shared_ptr> &conditionals);
+
+  /// @}
+  /// @name Testable
+  /// @{
 
   /// Test equality with base HybridFactor
   bool equals(const HybridFactor &lf, double tol = 1e-9) const override;
@@ -82,11 +100,19 @@ class GaussianMixtureConditional
       const std::string &s = "GaussianMixtureConditional\n",
       const KeyFormatter &formatter = DefaultKeyFormatter) const override;
 
- protected:
+  /// @}
+
+  /// Getter for the underlying Conditionals DecisionTree
+  const Conditionals &conditionals();
+
   /**
-   * @brief Convert a DecisionTree of factors into a DT of Gaussian FGs.
+   * @brief Merge the Gaussian Factor Graphs in `this` and `sum` while
+   * maintaining the decision tree structure.
+   *
+   * @param sum Decision Tree of Gaussian Factor Graphs
+   * @return Sum
    */
-  Sum asGaussianFactorGraphTree() const;
+  Sum add(const Sum &sum) const;
 };
 
 }  // namespace gtsam
