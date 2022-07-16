eff9a9ebe5cab3fc07f84cf5e9fe37ca5cb2b5e7
senselessdev1 <johnlambert@hayoungs-mbp.lan>
2022-07-16 00:26:57 -0400

    clean up docstrings

--- python/gtsam/tests/test_DsfTrackGenerator.py
+++ python/gtsam/tests/test_DsfTrackGenerator.py
@@ -16,19 +16,20 @@ class TestDsfTrackGenerator(GtsamTestCase):
     """Tests for DsfTrackGenerator."""
 
     def test_track_generation(self) -> None:
-        """ """
-        kps_i0 = Keypoints(coordinates=np.array([[0,0],[1,1]]))
-        kps_i1 = Keypoints(coordinates=np.array([[2,2],[3,3],[4,4]]))
-        kps_i2 = Keypoints(coordinates=np.array([[5,5],[6,6]]))
+        """Ensures that DSF generates two tracks from measurements in 3 images (H=200,W=400)."""
+        kps_i0 = Keypoints(coordinates=np.array([[10.,20],[30,40]]))
+        kps_i1 = Keypoints(coordinates=np.array([[50.,60],[70,80],[90,100]]))
+        kps_i2 = Keypoints(coordinates=np.array([[110.,120],[130,140]]))
 
         keypoints_list = KeypointsList()
         keypoints_list.append(kps_i0)
         keypoints_list.append(kps_i1)
         keypoints_list.append(kps_i2)
 
+        # For each image pair (i1,i2), we provide a (K,2) matrix of corresponding image indices (k1,k2).
         matches_dict = MatchIndicesMap()
-        matches_dict[(0,0)] = np.array([[0,0],[1,1]])
-        matches_dict[(1,1)] = np.array([[2,2],[3,3],[4,4]])
+        matches_dict[(0,1)] = np.array([[0,0],[1,1]])
+        matches_dict[(1,2)] = np.array([[2,0],[1,1]])
         import pdb; pdb.set_trace()
 
         tracks = DsfTrackGenerator.generate_tracks_from_pairwise_matches(matches_dict, keypoints_list)
