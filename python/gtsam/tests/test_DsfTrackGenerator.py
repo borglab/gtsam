361e3f601c79c3a467a17392db3e40fb1e9db443
senselessdev1 <johnlambert@hayoungs-mbp.lan>
2022-07-16 22:29:01 -0400

    Remove C++17 decompositions, and address comments

--- python/gtsam/tests/test_DsfTrackGenerator.py
+++ python/gtsam/tests/test_DsfTrackGenerator.py
@@ -16,7 +16,7 @@ class TestDsfTrackGenerator(GtsamTestCase):
     """Tests for DsfTrackGenerator."""
 
     def test_track_generation(self) -> None:
-        """Ensures that DSF generates two tracks from measurements in 3 images (H=200,W=400)."""
+        """Ensures that DSF generates three tracks from measurements in 3 images (H=200,W=400)."""
         kps_i0 = Keypoints(coordinates=np.array([[10.0, 20], [30, 40]]))
         kps_i1 = Keypoints(coordinates=np.array([[50.0, 60], [70, 80], [90, 100]]))
         kps_i2 = Keypoints(coordinates=np.array([[110.0, 120], [130, 140]]))
@@ -32,25 +32,27 @@ class TestDsfTrackGenerator(GtsamTestCase):
         matches_dict[(1, 2)] = np.array([[2, 0], [1, 1]])
 
         tracks = DsfTrackGenerator().generate_tracks_from_pairwise_matches(matches_dict, keypoints_list)
+        import pdb
 
+        pdb.set_trace()
         assert len(tracks) == 3
 
         # Verify track 0.
         assert np.allclose(tracks[0].measurements()[0].uv, np.array([10.0, 20.0]))
         assert np.allclose(tracks[0].measurements()[1].uv, np.array([50.0, 60.0]))
-        assert tracks[0].measurements()[0].i, 0
-        assert tracks[0].measurements()[1].i, 1
+        assert tracks[0].measurements()[0].i == 0
+        assert tracks[0].measurements()[1].i == 1
 
         # Verify track 1.
         assert np.allclose(tracks[1].measurements()[0].uv, np.array([30.0, 40.0]))
         assert np.allclose(tracks[1].measurements()[1].uv, np.array([70.0, 80.0]))
         assert np.allclose(tracks[1].measurements()[2].uv, np.array([130.0, 140.0]))
-        assert tracks[1].measurements()[0].i, 0
-        assert tracks[1].measurements()[1].i, 1
-        assert tracks[1].measurements()[2].i, 2
+        assert tracks[1].measurements()[0].i == 0
+        assert tracks[1].measurements()[1].i == 1
+        assert tracks[1].measurements()[2].i == 2
 
         # Verify track 2.
         assert np.allclose(tracks[2].measurements()[0].uv, np.array([90.0, 100.0]))
         assert np.allclose(tracks[2].measurements()[1].uv, np.array([110.0, 120.0]))
-        assert tracks[2].measurements()[0].i, 1
-        assert tracks[2].measurements()[1].i, 2
+        assert tracks[2].measurements()[0].i == 1
+        assert tracks[2].measurements()[1].i == 2
