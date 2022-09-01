c6ebbdc70812715d6f578c6f5fa5d6eb72b33cc4
Varun Agrawal <varagrawal@gmail.com>
2022-09-01 00:06:29 -0400

    add serialization test for GaussianBayesNet

--- gtsam/linear/tests/testGaussianBayesNet.cpp
+++ gtsam/linear/tests/testGaussianBayesNet.cpp
@@ -350,6 +350,37 @@ TEST(GaussianBayesNet, Dot) {
     "}");
 }
 
+#include <gtsam/base/serializationTestHelpers.h>
+using namespace gtsam::serializationTestHelpers;
+
+/* ****************************************************************************/
+// Test GaussianBayesNet serialization.
+TEST(GaussianBayesNet, Serialization) {
+  // Create an arbitrary Bayes Net
+  GaussianBayesNet gbn;
+  gbn += GaussianConditional::shared_ptr(new GaussianConditional(
+      0, Vector2(1.0, 2.0), (Matrix2() << 3.0, 4.0, 0.0, 6.0).finished(), 3,
+      (Matrix2() << 7.0, 8.0, 9.0, 10.0).finished(), 4,
+      (Matrix2() << 11.0, 12.0, 13.0, 14.0).finished()));
+  gbn += GaussianConditional::shared_ptr(new GaussianConditional(
+      1, Vector2(15.0, 16.0), (Matrix2() << 17.0, 18.0, 0.0, 20.0).finished(),
+      2, (Matrix2() << 21.0, 22.0, 23.0, 24.0).finished(), 4,
+      (Matrix2() << 25.0, 26.0, 27.0, 28.0).finished()));
+  gbn += GaussianConditional::shared_ptr(new GaussianConditional(
+      2, Vector2(29.0, 30.0), (Matrix2() << 31.0, 32.0, 0.0, 34.0).finished(),
+      3, (Matrix2() << 35.0, 36.0, 37.0, 38.0).finished()));
+  gbn += GaussianConditional::shared_ptr(new GaussianConditional(
+      3, Vector2(39.0, 40.0), (Matrix2() << 41.0, 42.0, 0.0, 44.0).finished(),
+      4, (Matrix2() << 45.0, 46.0, 47.0, 48.0).finished()));
+  gbn += GaussianConditional::shared_ptr(new GaussianConditional(
+      4, Vector2(49.0, 50.0), (Matrix2() << 51.0, 52.0, 0.0, 54.0).finished()));
+
+  EXPECT(equalsObj<GaussianBayesNet>(gbn));
+  EXPECT(equalsXML<GaussianBayesNet>(gbn));
+  EXPECT(equalsBinary<GaussianBayesNet>(gbn));
+}
+
+
 /* ************************************************************************* */
 int main() {
   TestResult tr;
