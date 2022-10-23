38be12eaf46885d2ae257215582ba616472b5b7c
Frank Dellaert <dellaert@gmail.com>
2022-10-22 18:37:44 -0700

    Use SfmMeasurement and SfmTrack

--- python/gtsam/specializations/sfm.h
+++ python/gtsam/specializations/sfm.h
@@ -18,25 +18,11 @@ py::bind_vector<std::vector<gtsam::BinaryMeasurement<gtsam::Unit3> > >(
 py::bind_vector<std::vector<gtsam::BinaryMeasurement<gtsam::Rot3> > >(
     m_, "BinaryMeasurementsRot3");
 py::bind_map<gtsam::KeyPairDoubleMap>(m_, "KeyPairDoubleMap");
-py::bind_vector<
-    std::vector<gtsam::SfmTrack> >(
-    m_, "SfmTracks");
-py::bind_vector<
-    std::vector<gtsam::SfmCamera> >(
-    m_, "SfmCameras");
-py::bind_vector<
-    std::vector<std::pair<size_t, gtsam::Point2>>>(
-        m_, "SfmMeasurementVector"
-    );
+py::bind_vector<std::vector<gtsam::SfmTrack2d>>(m_, "SfmTrack2dVector");
+py::bind_vector<std::vector<gtsam::SfmTrack>>(m_, "SfmTracks");
+py::bind_vector<std::vector<gtsam::SfmCamera>>(m_, "SfmCameras");
+py::bind_vector<std::vector<std::pair<size_t, gtsam::Point2>>>(
+    m_, "SfmMeasurementVector");
 
-py::bind_vector<
-    std::vector<gtsam::gtsfm::SfmTrack2d> >(
-    m_, "SfmTrack2dVector");
-py::bind_vector<
-    std::vector<gtsam::gtsfm::NamedSfmMeasurement> >(
-    m_, "NamedSfmMeasurementVector");
 py::bind_map<gtsam::gtsfm::MatchIndicesMap>(m_, "MatchIndicesMap");
-
-py::bind_vector<
-    std::vector<gtsam::gtsfm::Keypoints> >(
-    m_, "KeypointsVector");
+py::bind_vector<std::vector<gtsam::gtsfm::Keypoints>>(m_, "KeypointsVector");
--- python/gtsam/tests/test_DsfTrackGenerator.py
+++ python/gtsam/tests/test_DsfTrackGenerator.py
@@ -6,15 +6,9 @@ Authors: John Lambert
 import unittest
 
 import numpy as np
-
-import gtsam
-from gtsam import IndexPair, KeypointsVector, MatchIndicesMap, NamedSfmMeasurementVector
-from gtsam.gtsfm import (
-    DsfTrackGenerator,
-    Keypoints,
-    NamedSfmMeasurement,
-    SfmTrack2d,
-)
+from gtsam import (IndexPair, KeypointsVector, MatchIndicesMap, Point2,
+                   SfmMeasurementVector, SfmTrack2d)
+from gtsam.gtsfm import DsfTrackGenerator, Keypoints
 from gtsam.utils.test_case import GtsamTestCase
 
 
@@ -22,48 +16,55 @@ class TestDsfTrackGenerator(GtsamTestCase):
     """Tests for DsfTrackGenerator."""
 
     def test_track_generation(self) -> None:
-        """Ensures that DSF generates three tracks from measurements in 3 images (H=200,W=400)."""
-        kps_i0 = Keypoints(coordinates=np.array([[10.0, 20], [30, 40]]))
-        kps_i1 = Keypoints(coordinates=np.array([[50.0, 60], [70, 80], [90, 100]]))
-        kps_i2 = Keypoints(coordinates=np.array([[110.0, 120], [130, 140]]))
+        """Ensures that DSF generates three tracks from measurements
+        in 3 images (H=200,W=400)."""
+        kps_i0 = Keypoints(np.array([[10.0, 20], [30, 40]]))
+        kps_i1 = Keypoints(np.array([[50.0, 60], [70, 80], [90, 100]]))
+        kps_i2 = Keypoints(np.array([[110.0, 120], [130, 140]]))
 
         keypoints_list = KeypointsVector()
         keypoints_list.append(kps_i0)
         keypoints_list.append(kps_i1)
         keypoints_list.append(kps_i2)
 
-        # For each image pair (i1,i2), we provide a (K,2) matrix of corresponding image indices (k1,k2).
+        # For each image pair (i1,i2), we provide a (K,2) matrix
+        # of corresponding image indices (k1,k2).
         matches_dict = MatchIndicesMap()
         matches_dict[IndexPair(0, 1)] = np.array([[0, 0], [1, 1]])
         matches_dict[IndexPair(1, 2)] = np.array([[2, 0], [1, 1]])
 
-        tracks = DsfTrackGenerator().generate_tracks_from_pairwise_matches(matches_dict,
-                                                                           keypoints_list,
-                                                                           verbose=True)
+        tracks = DsfTrackGenerator().generate_tracks_from_pairwise_matches(
+            matches_dict,
+            keypoints_list,
+            verbose=False,
+        )
         assert len(tracks) == 3
 
         # Verify track 0.
-        assert np.allclose(tracks[0].measurements()[0].uv, np.array([10.0, 20.0]))
-        assert np.allclose(tracks[0].measurements()[1].uv, np.array([50.0, 60.0]))
-        assert tracks[0].measurements()[0].i == 0
-        assert tracks[0].measurements()[1].i == 1
-        assert tracks[0].numberMeasurements() == 2
+        track0 = tracks[0]
+        np.testing.assert_allclose(track0.measurements[0][1], Point2(10, 20))
+        np.testing.assert_allclose(track0.measurements[1][1], Point2(50, 60))
+        assert track0.measurements[0][0] == 0
+        assert track0.measurements[1][0] == 1
+        assert track0.numberMeasurements() == 2
 
         # Verify track 1.
-        assert np.allclose(tracks[1].measurements()[0].uv, np.array([30.0, 40.0]))
-        assert np.allclose(tracks[1].measurements()[1].uv, np.array([70.0, 80.0]))
-        assert np.allclose(tracks[1].measurements()[2].uv, np.array([130.0, 140.0]))
-        assert tracks[1].measurements()[0].i == 0
-        assert tracks[1].measurements()[1].i == 1
-        assert tracks[1].measurements()[2].i == 2
-        assert tracks[1].numberMeasurements() == 3
+        track1 = tracks[1]
+        np.testing.assert_allclose(track1.measurements[0][1], Point2(30, 40))
+        np.testing.assert_allclose(track1.measurements[1][1], Point2(70, 80))
+        np.testing.assert_allclose(track1.measurements[2][1], Point2(130, 140))
+        assert track1.measurements[0][0] == 0
+        assert track1.measurements[1][0] == 1
+        assert track1.measurements[2][0] == 2
+        assert track1.numberMeasurements() == 3
 
         # Verify track 2.
-        assert np.allclose(tracks[2].measurements()[0].uv, np.array([90.0, 100.0]))
-        assert np.allclose(tracks[2].measurements()[1].uv, np.array([110.0, 120.0]))
-        assert tracks[2].measurements()[0].i == 1
-        assert tracks[2].measurements()[1].i == 2
-        assert tracks[2].numberMeasurements() == 2
+        track2 = tracks[2]
+        np.testing.assert_allclose(track2.measurements[0][1], Point2(90, 100))
+        np.testing.assert_allclose(track2.measurements[1][1], Point2(110, 120))
+        assert track2.measurements[0][0] == 1
+        assert track2.measurements[1][0] == 2
+        assert track2.numberMeasurements() == 2
 
 
 class TestSfmTrack2d(GtsamTestCase):
@@ -71,8 +72,12 @@ class TestSfmTrack2d(GtsamTestCase):
 
     def test_sfm_track_2d_constructor(self) -> None:
         """ """
-        measurements = NamedSfmMeasurementVector()
-        measurements.append(NamedSfmMeasurement(i=0, uv=np.array([10.0, 20.0])))
+        measurements = SfmMeasurementVector()
+        measurements.append((0, Point2(10, 20)))
         track = SfmTrack2d(measurements=measurements)
         track.measurement(0)
         track.numberMeasurements() == 1
+
+
+if __name__ == "__main__":
+    unittest.main()
