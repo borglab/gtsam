9b089d4bbdd9d289ed1ce2761364659120a0ec85
Varun Agrawal <varagrawal@gmail.com>
2022-11-07 18:30:03 -0500

    update HybridSmoother to use the new method

--- gtsam/hybrid/HybridSmoother.cpp
+++ gtsam/hybrid/HybridSmoother.cpp
@@ -32,7 +32,7 @@ void HybridSmoother::update(HybridGaussianFactorGraph graph,
       addConditionals(graph, hybridBayesNet_, ordering);
 
   // Eliminate.
-  auto bayesNetFragment = graph.eliminateSequential(ordering);
+  auto bayesNetFragment = graph.eliminateHybridSequential();
 
   /// Prune
   if (maxNrLeaves) {
