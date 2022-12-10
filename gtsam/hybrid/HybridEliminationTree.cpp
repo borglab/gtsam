0596b2f543a7327d4e89d2999c578883756956ae
Varun Agrawal <varagrawal@gmail.com>
2022-12-10 09:46:26 +0530

    remove unrequired code

--- gtsam/hybrid/HybridEliminationTree.cpp
+++ gtsam/hybrid/HybridEliminationTree.cpp
@@ -27,112 +27,16 @@ template class EliminationTree<HybridBayesNet, HybridGaussianFactorGraph>;
 HybridEliminationTree::HybridEliminationTree(
     const HybridGaussianFactorGraph& factorGraph,
     const VariableIndex& structure, const Ordering& order)
-    : Base(factorGraph, structure, order),
-      graph_(factorGraph),
-      variable_index_(structure) {
-  // Segregate the continuous and the discrete keys
-  std::tie(continuous_ordering_, discrete_ordering_) =
-      graph_.separateContinuousDiscreteOrdering(order);
-}
+    : Base(factorGraph, structure, order) {}
 
 /* ************************************************************************* */
 HybridEliminationTree::HybridEliminationTree(
     const HybridGaussianFactorGraph& factorGraph, const Ordering& order)
-    : Base(factorGraph, order),
-      graph_(factorGraph),
-      variable_index_(VariableIndex(factorGraph)) {}
+    : Base(factorGraph, order) {}
 
 /* ************************************************************************* */
 bool HybridEliminationTree::equals(const This& other, double tol) const {
   return Base::equals(other, tol);
-}
-
-/* ************************************************************************* */
-std::pair<boost::shared_ptr<HybridBayesNet>,
-          boost::shared_ptr<HybridGaussianFactorGraph>>
-HybridEliminationTree::eliminateContinuous(Eliminate function) const {
-  if (continuous_ordering_.size() > 0) {
-    This continuous_etree(graph_, variable_index_, continuous_ordering_);
-    return continuous_etree.Base::eliminate(function);
-
-  } else {
-    HybridBayesNet::shared_ptr bayesNet = boost::make_shared<HybridBayesNet>();
-    HybridGaussianFactorGraph::shared_ptr discreteGraph =
-        boost::make_shared<HybridGaussianFactorGraph>(graph_);
-    return std::make_pair(bayesNet, discreteGraph);
-  }
-}
-
-/* ************************************************************************* */
-boost::shared_ptr<HybridGaussianFactorGraph>
-HybridEliminationTree::addProbPrimes(
-    const HybridBayesNet::shared_ptr& continuousBayesNet,
-    const HybridGaussianFactorGraph::shared_ptr& discreteGraph) const {
-  if (continuous_ordering_.size() > 0 && discrete_ordering_.size() > 0) {
-    // Get the last continuous conditional
-    // which will have all the discrete keys
-    HybridConditional::shared_ptr last_conditional =
-        continuousBayesNet->at(continuousBayesNet->size() - 1);
-    DiscreteKeys discrete_keys = last_conditional->discreteKeys();
-
-    // DecisionTree for P'(X|M, Z) for all mode sequences M
-    const AlgebraicDecisionTree<Key> probPrimeTree =
-        graph_.continuousProbPrimes(discrete_keys, continuousBayesNet);
-
-    // Add the model selection factor P(M|Z)
-    discreteGraph->add(DecisionTreeFactor(discrete_keys, probPrimeTree));
-  }
-  return discreteGraph;
-}
-
-/* ************************************************************************* */
-std::pair<boost::shared_ptr<HybridBayesNet>,
-          boost::shared_ptr<HybridGaussianFactorGraph>>
-HybridEliminationTree::eliminateDiscrete(
-    Eliminate function,
-    const HybridGaussianFactorGraph::shared_ptr& discreteGraph) const {
-  HybridBayesNet::shared_ptr discreteBayesNet;
-  HybridGaussianFactorGraph::shared_ptr finalGraph;
-  if (discrete_ordering_.size() > 0) {
-    This discrete_etree(*discreteGraph, VariableIndex(*discreteGraph),
-                        discrete_ordering_);
-
-    std::tie(discreteBayesNet, finalGraph) =
-        discrete_etree.Base::eliminate(function);
-
-  } else {
-    discreteBayesNet = boost::make_shared<HybridBayesNet>();
-    finalGraph = discreteGraph;
-  }
-
-  return std::make_pair(discreteBayesNet, finalGraph);
-}
-
-/* ************************************************************************* */
-std::pair<boost::shared_ptr<HybridBayesNet>,
-          boost::shared_ptr<HybridGaussianFactorGraph>>
-HybridEliminationTree::eliminate(Eliminate function) const {
-  // Perform continuous elimination
-  HybridBayesNet::shared_ptr bayesNet;
-  HybridGaussianFactorGraph::shared_ptr discreteGraph;
-  std::tie(bayesNet, discreteGraph) = this->eliminateContinuous(function);
-
-  // If we have eliminated continuous variables
-  // and have discrete variables to eliminate,
-  // then compute P(X | M, Z)
-  HybridGaussianFactorGraph::shared_ptr updatedDiscreteGraph =
-      addProbPrimes(bayesNet, discreteGraph);
-
-  // Perform discrete elimination
-  HybridBayesNet::shared_ptr discreteBayesNet;
-  HybridGaussianFactorGraph::shared_ptr finalGraph;
-  std::tie(discreteBayesNet, finalGraph) =
-      eliminateDiscrete(function, updatedDiscreteGraph);
-
-  // Add the discrete conditionals to the hybrid conditionals
-  bayesNet->add(*discreteBayesNet);
-
-  return std::make_pair(bayesNet, finalGraph);
 }
 
 }  // namespace gtsam
--- gtsam/hybrid/HybridEliminationTree.h
+++ gtsam/hybrid/HybridEliminationTree.h
@@ -26,33 +26,12 @@ namespace gtsam {
 /**
  * Elimination Tree type for Hybrid Factor Graphs.
  *
- * The elimination tree helps in elimination by specifying the parents to which
- * the "combined factor" after elimination should be assigned to.
- *
- * In the case of hybrid elimination, we use the elimination tree to store the
- * intermediate results.
- * Concretely, we wish to estimate the unnormalized probability P'(X | M, Z) for
- * each mode assignment M.
- * P'(X | M, Z) can be computed by estimating X* which maximizes the continuous
- * factor graph given the discrete modes, and then computing the unnormalized
- * probability as P(X=X* | M, Z).
- *
- * This is done by eliminating the (continuous) factor graph present at each
- * leaf of the discrete tree formed for the discrete sequence and using the
- * inferred X* to compute the `probPrime`.
- *
  * @ingroup hybrid
  */
 class GTSAM_EXPORT HybridEliminationTree
     : public EliminationTree<HybridBayesNet, HybridGaussianFactorGraph> {
  private:
   friend class ::EliminationTreeTester;
-
-  Ordering continuous_ordering_, discrete_ordering_;
-  /// Used to store the original factor graph to eliminate
-  HybridGaussianFactorGraph graph_;
-  /// Store the provided variable index.
-  VariableIndex variable_index_;
 
  public:
   typedef EliminationTree<HybridBayesNet, HybridGaussianFactorGraph>
@@ -87,75 +66,6 @@ class GTSAM_EXPORT HybridEliminationTree
 
   /** Test whether the tree is equal to another */
   bool equals(const This& other, double tol = 1e-9) const;
-
- protected:
-  /**
-   * @brief Helper method to eliminate continuous variables.
-   *
-   * If no continuous variables exist, return an empty bayes net
-   * and the original graph.
-   *
-   * @param function Elimination function for hybrid elimination.
-   * @return std::pair<boost::shared_ptr<HybridBayesNet>,
-   * boost::shared_ptr<HybridGaussianFactorGraph> >
-   */
-  std::pair<boost::shared_ptr<HybridBayesNet>,
-            boost::shared_ptr<HybridGaussianFactorGraph>>
-  eliminateContinuous(Eliminate function) const;
-
-  /**
-   * @brief Compute the unnormalized probability P'(X | M, Z)
-   * for the factor graph in each leaf of the discrete tree.
-   * The discrete decision tree formed as a result is added to the
-   * `discreteGraph` for discrete elimination.
-   *
-   * @param continuousBayesNet The bayes nets corresponding to
-   * the eliminated continuous variables.
-   * @param discreteGraph Factor graph consisting of factors
-   * on discrete variables only.
-   * @return boost::shared_ptr<HybridGaussianFactorGraph>
-   */
-  boost::shared_ptr<HybridGaussianFactorGraph> addProbPrimes(
-      const HybridBayesNet::shared_ptr& continuousBayesNet,
-      const HybridGaussianFactorGraph::shared_ptr& discreteGraph) const;
-
-  /**
-   * @brief Helper method to eliminate the discrete variables after the
-   * continuous variables have been eliminated.
-   *
-   * If there are no discrete variables, return an empty bayes net and the
-   * discreteGraph which is passed in.
-   *
-   * @param function Hybrid elimination function
-   * @param discreteGraph The factor graph with the factor ϕ(X | M, Z).
-   * @return std::pair<boost::shared_ptr<HybridBayesNet>,
-   * boost::shared_ptr<HybridGaussianFactorGraph> >
-   */
-  std::pair<boost::shared_ptr<HybridBayesNet>,
-            boost::shared_ptr<HybridGaussianFactorGraph>>
-  eliminateDiscrete(
-      Eliminate function,
-      const HybridGaussianFactorGraph::shared_ptr& discreteGraph) const;
-
- public:
-  /**
-   * @brief Override the EliminationTree eliminate method
-   * so we can perform hybrid elimination correctly.
-   *
-   * @param function Hybrid elimination function
-   * @return std::pair<boost::shared_ptr<HybridBayesNet>,
-   * boost::shared_ptr<HybridGaussianFactorGraph> >
-   */
-  std::pair<boost::shared_ptr<HybridBayesNet>,
-            boost::shared_ptr<HybridGaussianFactorGraph>>
-  eliminate(Eliminate function) const;
-
-  Ordering continuousOrdering() const { return continuous_ordering_; }
-  Ordering discreteOrdering() const { return discrete_ordering_; }
-
-  /// Store the provided variable index.
-  VariableIndex variableIndex() const { return variable_index_; }
-  HybridGaussianFactorGraph graph() const { return graph_; }
 };
 
 }  // namespace gtsam
