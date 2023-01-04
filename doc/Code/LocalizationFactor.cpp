311c546b3f4cece4ae53536d4b5bb34864366ca6
Gerry Chen <gerry.chen2015@gmail.com>
2023-01-04 10:49:38 -0500

    update example code file to use NoiseModelFactorN

--- doc/Code/LocalizationFactor.cpp
+++ doc/Code/LocalizationFactor.cpp
@@ -1,9 +1,9 @@
-class UnaryFactor: public NoiseModelFactor1<Pose2> {
+class UnaryFactor: public NoiseModelFactorN<Pose2> {
   double mx_, my_; ///< X and Y measurements
   
 public:
   UnaryFactor(Key j, double x, double y, const SharedNoiseModel& model):
-    NoiseModelFactor1<Pose2>(model, j), mx_(x), my_(y) {}
+    NoiseModelFactorN<Pose2>(model, j), mx_(x), my_(y) {}
 
   Vector evaluateError(const Pose2& q, 
       boost::optional<Matrix&> H = boost::none) const override {
