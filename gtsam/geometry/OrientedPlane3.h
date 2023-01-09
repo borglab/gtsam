5181822b02352bb62d774be9a07e66979efdf712
BrettRD <brettrd@brettrd.com>
2023-01-09 13:40:41 +1100

    wraps more getters in Expressions

--- gtsam/geometry/OrientedPlane3.h
+++ gtsam/geometry/OrientedPlane3.h
@@ -125,12 +125,14 @@ public:
   }
 
   /// Return the normal
-  inline Unit3 normal() const {
+  inline Unit3 normal(OptionalJacobian<2, 3> H = boost::none) const {
+    if (H) *H << I_2x2, Z_2x1;
     return n_;
   }
 
   /// Return the perpendicular distance to the origin
-  inline double distance() const {
+  inline double distance(OptionalJacobian<1, 3> H = boost::none) const {
+    if (H) *H << 0,0,1;
     return d_;
   }
 };
