d05d2af6b5855977ca68a2dd254c0959e34cfe93
kartik arcot <kartik.arcot@verdantrobotics.com>
2023-01-13 10:36:29 -0800

    linear algorithms

--- gtsam/linear/linearAlgorithms-inst.h
+++ gtsam/linear/linearAlgorithms-inst.h
@@ -21,8 +21,9 @@
 #include <gtsam/linear/GaussianConditional.h>
 #include <gtsam/base/treeTraversal-inst.h>
 
-#include <boost/optional.hpp>
 #include <boost/shared_ptr.hpp>
+
+#include <optional>
 
 namespace gtsam
 {
@@ -32,7 +33,7 @@ namespace gtsam
     {
       /* ************************************************************************* */
       struct OptimizeData {
-        boost::optional<OptimizeData&> parentData;
+        OptimizeData* parentData = nullptr;
         FastMap<Key, VectorValues::const_iterator> cliqueResults;
         //VectorValues ancestorResults;
         //VectorValues results;
@@ -55,7 +56,7 @@ namespace gtsam
           OptimizeData& parentData)
         {
           OptimizeData myData;
-          myData.parentData = parentData;
+          myData.parentData = &parentData;
           // Take any ancestor results we'll need
           for(Key parent: clique->conditional_->parents())
             myData.cliqueResults.emplace(parent, myData.parentData->cliqueResults.at(parent));
