179575764a55b08401c83d0d29dc1dfb0b68f261
Frank Dellaert <dellaert@gmail.com>
2023-02-11 15:54:28 -0800

    Reverted ill-advised quick fix for concepts - now no-op.

--- gtsam/base/concepts.h
+++ gtsam/base/concepts.h
@@ -14,21 +14,12 @@
 #include <boost/concept/requires.hpp>
 #include <boost/concept_check.hpp>
 #define GTSAM_CONCEPT_ASSERT(concept) BOOST_CONCEPT_ASSERT((concept))
-#define GTSAM_CONCEPT_ASSERT1(concept) BOOST_CONCEPT_ASSERT((concept))
-#define GTSAM_CONCEPT_ASSERT2(concept) BOOST_CONCEPT_ASSERT((concept))
-#define GTSAM_CONCEPT_ASSERT3(concept) BOOST_CONCEPT_ASSERT((concept))
-#define GTSAM_CONCEPT_ASSERT4(concept) BOOST_CONCEPT_ASSERT((concept))
 #define GTSAM_CONCEPT_REQUIRES(concept, return_type) BOOST_CONCEPT_REQUIRES(((concept)), (return_type))
 #else 
-// These do something sensible:
+// This does something sensible:
 #define BOOST_CONCEPT_USAGE(concept) void check##concept()
-// TODO(dellaert): would be nice if it was a single macro...
-#define GTSAM_CONCEPT_ASSERT(concept) concept checkConcept [[maybe_unused]]
-#define GTSAM_CONCEPT_ASSERT1(concept) concept checkConcept1 [[maybe_unused]]
-#define GTSAM_CONCEPT_ASSERT2(concept) concept checkConcept2 [[maybe_unused]]
-#define GTSAM_CONCEPT_ASSERT3(concept) concept checkConcept3 [[maybe_unused]]
-#define GTSAM_CONCEPT_ASSERT4(concept) concept checkConcept4 [[maybe_unused]]
-// This one just ignores concept for now:
+// These just ignore the concept checking for now:
+#define GTSAM_CONCEPT_ASSERT(concept)
 #define GTSAM_CONCEPT_REQUIRES(concept, return_type) return_type
 #endif
 
