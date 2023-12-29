ba93dec850f0ce45b93271158ab9f778cfe7cc53
Varun Agrawal <varagrawal@gmail.com>
2023-12-29 11:18:58 -0500

    only used built in version of Cephes since there doesn't seem to be an easy packaged version

--- cmake/HandleCephes.cmake
+++ cmake/HandleCephes.cmake
@@ -4,38 +4,13 @@
 # For both system or bundle version, a cmake target "cephes-gtsam-if" is defined
 # (interface library)
 
-option(
-  GTSAM_USE_SYSTEM_CEPHES
-  "Find and use system-installed cephes. If 'off', use the one bundled with GTSAM"
-  OFF)
 
-if(GTSAM_USE_SYSTEM_CEPHES)
-  # # Debian package: libmetis-dev
+add_subdirectory(${GTSAM_SOURCE_DIR}/gtsam/3rdparty/cephes)
 
-  # find_path(METIS_INCLUDE_DIR metis.h REQUIRED) find_library(METIS_LIBRARY
-  # metis REQUIRED)
+list(APPEND GTSAM_EXPORTED_TARGETS cephes-gtsam)
 
-  # if(METIS_INCLUDE_DIR AND METIS_LIBRARY) mark_as_advanced(METIS_INCLUDE_DIR)
-  # mark_as_advanced(METIS_LIBRARY)
-
-  # add_library(cephes-gtsam-if INTERFACE)
-  # target_include_directories(cephes-gtsam-if BEFORE INTERFACE
-  # ${METIS_INCLUDE_DIR} # gtsam_unstable/partition/FindSeparator-inl.h uses
-  # internal metislib.h API # via extern "C"
-  # $<BUILD_INTERFACE:${GTSAM_SOURCE_DIR}/gtsam/3rdparty/metis/libmetis>
-  # $<BUILD_INTERFACE:${GTSAM_SOURCE_DIR}/gtsam/3rdparty/metis/GKlib> )
-  # target_link_libraries(cephes-gtsam-if INTERFACE ${METIS_LIBRARY}) endif()
-
-else()
-  # Bundled version:
-  add_subdirectory(${GTSAM_SOURCE_DIR}/gtsam/3rdparty/cephes)
-
-  list(APPEND GTSAM_EXPORTED_TARGETS cephes-gtsam)
-
-  add_library(cephes-gtsam-if INTERFACE)
-  target_link_libraries(cephes-gtsam-if INTERFACE cephes-gtsam)
-
-endif()
+add_library(cephes-gtsam-if INTERFACE)
+target_link_libraries(cephes-gtsam-if INTERFACE cephes-gtsam)
 
 list(APPEND GTSAM_EXPORTED_TARGETS cephes-gtsam-if)
 install(
