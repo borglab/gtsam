e7cb7b2dcdc730ffd5a7309e69f03121ac41339b
Varun Agrawal <varagrawal@gmail.com>
2024-01-16 15:00:35 -0500

    add constructor for HybridFactorGraph which takes a container of factors

--- gtsam/hybrid/HybridGaussianFactorGraph.h
+++ gtsam/hybrid/HybridGaussianFactorGraph.h
@@ -126,6 +126,11 @@ class GTSAM_EXPORT HybridGaussianFactorGraph
   /// @brief Default constructor.
   HybridGaussianFactorGraph() = default;
 
+  /** Construct from container of factors (shared_ptr or plain objects) */
+  template <class CONTAINER>
+  explicit HybridGaussianFactorGraph(const CONTAINER& factors)
+      : Base(factors) {}
+
   /**
    * Implicit copy/downcast constructor to override explicit template container
    * constructor. In BayesTree this is used for:
