95098e0cade22f672f3b426099feb91c2a1863d7
Varun Agrawal <varagrawal@gmail.com>
2024-09-26 18:51:30 -0400

    add missing preprocessor guard

--- gtsam/sfm/TranslationFactor.h
+++ gtsam/sfm/TranslationFactor.h
@@ -149,11 +149,13 @@ class BilinearAngleTranslationFactor
   }
 
  private:
+#ifdef GTSAM_ENABLE_BOOST_SERIALIZATION
   friend class boost::serialization::access;
   template <class ARCHIVE>
   void serialize(ARCHIVE& ar, const unsigned int /*version*/) {
     ar& boost::serialization::make_nvp(
         "Base", boost::serialization::base_object<Base>(*this));
   }
+#endif
 };  // \ BilinearAngleTranslationFactor
 }  // namespace gtsam
