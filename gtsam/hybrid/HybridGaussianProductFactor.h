f4bf280047b352bf49ae939f8195f6aca65eebc4
Varun Agrawal <varagrawal@gmail.com>
2024-10-09 13:08:42 -0400

    implement constructor

--- gtsam/hybrid/HybridGaussianProductFactor.h
+++ gtsam/hybrid/HybridGaussianProductFactor.h
@@ -58,7 +58,10 @@ class GTSAM_EXPORT HybridGaussianProductFactor
   /// Deleted constructor since we don't have istream operator for
   /// GaussianFactorGraphValuePair
   HybridGaussianProductFactor(const std::vector<DiscreteKey>& labelCs,
-                              const std::string& table) = delete;
+                              const std::string& table) {
+    throw std::runtime_error(
+        "HybridGaussianProductFactor: No way to construct.");
+  }
 
   ///@}
 
