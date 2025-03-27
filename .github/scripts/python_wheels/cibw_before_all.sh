6fd467d0d7c887d56b3ee9833f7683cd1199d7d1
Yashas Ambati <yashas.amb@gmail.com>
2025-03-27 13:13:50 -0400

    Generate doxygen XML and enable docstrings

--- .github/scripts/python_wheels/cibw_before_all.sh
+++ .github/scripts/python_wheels/cibw_before_all.sh
@@ -14,7 +14,7 @@ export PYTHON="python${PYTHON_VERSION}"
 
 if [ "$(uname)" == "Linux" ]; then
     # manylinux2014 is based on CentOS 7, so use yum to install dependencies
-    yum install -y wget
+    yum install -y wget doxygen
 
     # Install Boost from source
     wget https://archives.boost.io/release/1.87.0/source/boost_1_87_0.tar.gz --quiet
@@ -50,12 +50,14 @@ cmake $PROJECT_DIR \
     -DGTSAM_PYTHON_VERSION=$PYTHON_VERSION \
     -DPYTHON_EXECUTABLE:FILEPATH=$(which $PYTHON) \
     -DGTSAM_ALLOW_DEPRECATED_SINCE_V43=OFF \
-    -DCMAKE_INSTALL_PREFIX=$PROJECT_DIR/gtsam_install
+    -DCMAKE_INSTALL_PREFIX=$PROJECT_DIR/gtsam_install \
+    -DGTSAM_GENERATE_DOC_XML=1
 
-cd $PROJECT_DIR/build/python
-echo | gcc -E -dM - | grep __GNUC__
+# Generate Doxygen XML documentation
+doxygen build/doc/Doxyfile
 
 # Install the Python wrapper module and generate Python stubs
+cd $PROJECT_DIR/build/python
 if [ "$(uname)" == "Linux" ]; then
     make -j $(nproc) install
     make -j $(nproc) python-stubs
