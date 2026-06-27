3f107c4c31b0e30cdc72f64f9565f50feaaa9084
Frank Dellaert <dellaert@gmail.com>
2026-06-27 17:59:23 +0200

    Pass optional Jacobians without local init in LieGroupEKF predictMean

--- gtsam/navigation/LieGroupEKF.h
+++ gtsam/navigation/LieGroupEKF.h
@@ -187,25 +187,26 @@ class LieGroupEKF : public ManifoldEKF<G> {
             typename = enable_if_dynamics<Dynamics>>
   G predictMean(Dynamics&& f, double dt,
                 OptionalJacobian<Dim, Dim> Phi = {}) const {
-    Jacobian Df;
-    Jacobian Dexp;
     if (Phi) {
-      if constexpr (Dim == Eigen::Dynamic) {
-        Df.resize(this->n_, this->n_);
-        Dexp.resize(this->n_, this->n_);
+      Jacobian Df;
+      TangentVector xi = f(this->X_, &Df);
+      if constexpr (std::is_same_v<G, Matrix>) {
+        *Phi = expm(Df * dt, K);
+        return traits<G>::Retract(this->X_, xi * dt);
+      } else {
+        Jacobian Dexp;
+        G U = traits<G>::Expmap(xi * dt, &Dexp);
+        *Phi = transitionMatrix<K>(xi, Df, dt, U, Dexp);
+        return traits<G>::Compose(this->X_, U);
       }
-      Df.setZero();
-      Dexp.setZero();
-    }
-    if constexpr (std::is_same_v<G, Matrix>) {
-      TangentVector xi = f(this->X_, Phi ? &Df : nullptr);
-      if (Phi) *Phi = expm(Df * dt, K);
-      return traits<G>::Retract(this->X_, xi * dt);
     } else {
-      TangentVector xi = f(this->X_, Phi ? &Df : nullptr);
-      G U = traits<G>::Expmap(xi * dt, Phi ? &Dexp : nullptr);
-      if (Phi) *Phi = transitionMatrix<K>(xi, Df, dt, U, Dexp);
-      return traits<G>::Compose(this->X_, U);
+      TangentVector xi = f(this->X_, nullptr);
+      if constexpr (std::is_same_v<G, Matrix>) {
+        return traits<G>::Retract(this->X_, xi * dt);
+      } else {
+        G U = traits<G>::Expmap(xi * dt);
+        return traits<G>::Compose(this->X_, U);
+      }
     }
   }
 
