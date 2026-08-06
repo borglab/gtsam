817093c214c15c85cd7cfac410449a33efd9378a
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-08-06 17:24:25 +0300

    Use public METIS API for edge partitioning

--- cmake/HandleMetis.cmake
+++ cmake/HandleMetis.cmake
@@ -28,12 +28,7 @@ if(GTSAM_USE_SYSTEM_METIS)
     mark_as_advanced(METIS_INCLUDE_DIR)
     mark_as_advanced(METIS_LIBRARY)
     add_library(metis-gtsam-if INTERFACE)
-    target_include_directories(metis-gtsam-if BEFORE INTERFACE ${METIS_INCLUDE_DIR}
-      # gtsam_unstable/partition/FindSeparator-inl.h uses internal metislib.h API
-      # via extern "C"
-      $<BUILD_INTERFACE:${GTSAM_SOURCE_DIR}/gtsam/3rdparty/metis/libmetis>
-      $<BUILD_INTERFACE:${GTSAM_SOURCE_DIR}/gtsam/3rdparty/metis/GKlib>
-    )
+    target_include_directories(metis-gtsam-if BEFORE INTERFACE ${METIS_INCLUDE_DIR})
     target_link_libraries(metis-gtsam-if INTERFACE ${GTSAM_METIS_LINK_TARGET})
   endif()
 else()
@@ -44,10 +39,6 @@ else()
   target_include_directories(metis-gtsam BEFORE PUBLIC
     $<INSTALL_INTERFACE:include/gtsam/3rdparty/metis/>
     $<BUILD_INTERFACE:${GTSAM_SOURCE_DIR}/gtsam/3rdparty/metis/include>
-    # gtsam_unstable/partition/FindSeparator-inl.h uses internal metislib.h API
-    # via extern "C"
-    $<BUILD_INTERFACE:${GTSAM_SOURCE_DIR}/gtsam/3rdparty/metis/libmetis>
-    $<BUILD_INTERFACE:${GTSAM_SOURCE_DIR}/gtsam/3rdparty/metis/GKlib>
   )
 
   add_library(metis-gtsam-if INTERFACE)
