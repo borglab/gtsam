0444e6405c220e0c26e4684ad8322fe654df4e7d
Frank Dellaert <dellaert@gmail.com>
2026-08-11 10:27:59 -0400

    Remove vestigal BETWEENFACTOR_VISIBILITY

--- gtsam/slam/BetweenFactor.h
+++ gtsam/slam/BetweenFactor.h
@@ -22,14 +22,6 @@
 #include <gtsam/nonlinear/NonlinearFactor.h>
 #include <gtsam/nonlinear/NoiseModelFactorN.h>
 
-#ifdef _WIN32
-#define BETWEENFACTOR_VISIBILITY
-#else
-// This will trigger a LNKxxxx on MSVC, so disable for MSVC build
-// Please refer to https://github.com/borglab/gtsam/blob/develop/Using-GTSAM-EXPORT.md
-#define BETWEENFACTOR_VISIBILITY GTSAM_EXPORT
-#endif
-
 namespace gtsam {
 
   /**
